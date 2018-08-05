b11590bede215a0a5acfa75177d4a782382a462d
Noah Gilmore <noah.w.gilmore@gmail.com>
2018-08-05 14:42:29 -0700

    Add a hack to get around the gross django 1.6 limitation with postgres 10.3+/psycopg2 2.7.5+

--- berkeleytime/berkeleytime/management/commands/playlist.py
+++ berkeleytime/berkeleytime/management/commands/playlist.py
@@ -1,4 +1,12 @@
 """Playlist management command."""
+
+# This import is a hack since with Django 1.6 there's an odd circular import
+# error ('cannot import name actions') when django tries to import these modules
+# like four levels down. This error only happens when running management
+# commands. When we upgrade to Django 1.7+, we might be able to remove this.
+# See https://stackoverflow.com/questions/24186814/importerror-cannot-import-name-actions
+from django.contrib.admin import ModelAdmin, actions # noqa
+
 from django.core.management.base import BaseCommand
 from mondaine.service.playlist import playlist_service
 from mondaine.service.enumeration.category import PlaylistCategory
