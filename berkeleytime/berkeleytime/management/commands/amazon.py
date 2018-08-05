bc8c87f273c9c93985bdd26043ff53eb42d11855
Noah Gilmore <noah.w.gilmore@gmail.com>
2018-08-05 14:42:29 -0700

    Add a hack to get around the gross django 1.6 limitation with postgres 10.3+/psycopg2 2.7.5+

--- berkeleytime/berkeleytime/management/commands/amazon.py
+++ berkeleytime/berkeleytime/management/commands/amazon.py
@@ -6,6 +6,13 @@ from django.conf import settings
 
 from marketplace.service.amazon import amazon_service
 
+# This import is a hack since with Django 1.6 there's an odd circular import
+# error ('cannot import name actions') when django tries to import these modules
+# like four levels down. This error only happens when running management
+# commands. When we upgrade to Django 1.7+, we might be able to remove this.
+# See https://stackoverflow.com/questions/24186814/importerror-cannot-import-name-actions
+from django.contrib.admin import ModelAdmin, actions # noqa
+
 
 class Command(BaseCommand):
     """Prints json from amazon about a given textbook isbn.
--- berkeleytime/berkeleytime/management/commands/enrollment.py
+++ berkeleytime/berkeleytime/management/commands/enrollment.py
@@ -2,6 +2,13 @@ from django.core.management.base import BaseCommand
 from catalog.models import *
 import csv
 
+# This import is a hack since with Django 1.6 there's an odd circular import
+# error ('cannot import name actions') when django tries to import these modules
+# like four levels down. This error only happens when running management
+# commands. When we upgrade to Django 1.7+, we might be able to remove this.
+# See https://stackoverflow.com/questions/24186814/importerror-cannot-import-name-actions
+from django.contrib.admin import ModelAdmin, actions # noqa
+
 #
 # Running This Script
 # $ python manage.py enrollment <SEMESTER> <YEAR>
