5b608ad47fd03a1e583442c2213e80ea5ac106eb
Michael Li <michael.li@berkeley.edu>
2019-01-27 14:29:14 -0800

    change frontend configuration

--- berkeleytime/frontend/src/index.js
+++ berkeleytime/frontend/src/index.js
@@ -1,1 +1,24 @@
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  HashRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import BT from './components/BerkeleyTime/index.jsx';
+
+import './assets/css/bootstrap.min.css';
+import './assets/css/animate.min.css';
+import './assets/sass/light-bootstrap-dashboard.css';
+import './assets/css/demo.css';
+import './assets/css/pe-icon-7-stroke.css';
+
+ReactDOM.render((
+  <HashRouter>
+    <Switch>
+      <Route path="/" name="Home" component={BT} />
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'));
