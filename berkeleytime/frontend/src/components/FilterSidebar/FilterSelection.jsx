252a653054438d862618426874a504c7950ab243
Michael Li <michael.li@berkeley.edu>
2019-02-21 15:54:03 -0800

    catalog: allow filtered courses to be selected and course card to be populated

--- berkeleytime/frontend/src/components/FilterSidebar/FilterSelection.jsx
+++ berkeleytime/frontend/src/components/FilterSidebar/FilterSelection.jsx
@@ -1,53 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function FilterSelection({
-  courseAbbreviation, courseTitle, percentageEnrolled,
-  units, waitlisted, averageGrade, borderColors,
-  gradeColors, id,
-}) {
-  return (
-    <div className={`filter-selection ${borderColors[id % 4]}`}>
-      <div className="filter-selection-content">
-        <h4 className="filter-selection-heading">{courseAbbreviation}</h4>
-        <p className="filter-selection-description">{courseTitle}</p>
-        <div className="filter-selection-enrollment-data">
-          <div className="dataBlock">
-            <i className="fa fa-circle first" />
-            <p>{` ${percentageEnrolled}% enrolled`}</p>
+class FilterSelection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  getFormattedEnrollment(enrollmentConstant) {
+    return Math.floor(enrollmentConstant * 100, 100);
+  }
+
+  getCorrectDisplayConstant() {
+
+  }
+
+  getCorrectDisplayColor() {
+
+  }
+
+  clickHandler() {
+    let course = {
+      id: this.props.id,
+      abbreviation: this.props.courseAbbreviation,
+      courseNumber: this.props.courseNumber
+    }
+    this.props.onClick(course);
+  }
+
+  render() {
+    const {courseAbbreviation, courseNumber, courseTitle, percentageEnrolled,
+      units, waitlisted, averageGrade, borderColors,
+      gradeColors, id} = this.props
+
+    return (
+        <button className={`filter-selection ${borderColors[id % 4]}`} onClick={this.clickHandler}>
+          <div className="filter-selection-content">
+            <h4 className="filter-selection-heading">{courseAbbreviation} {courseNumber}</h4>
+            <p className="filter-selection-description">{courseTitle}</p>
+            <div className="filter-selection-enrollment-data">
+              <div className="dataBlock">
+                <i className="fa fa-circle first" />
+                <p>{` ${this.getFormattedEnrollment(percentageEnrolled)}% enrolled`}</p>
+              </div>
+              <div className="dataBlock">
+                <i className="fa fa-circle second" />
+                <p>{` ${waitlisted} waitlisted`}</p>
+              </div>
+            </div>
           </div>
-          <div className="dataBlock">
-            <i className="fa fa-circle second" />
-            <p>{` ${waitlisted} waitlisted`}</p>
+          <div className="filter-selection-aside">
+            <div className="filter-selection-units">
+              <h6>{`${units} Units`}</h6>
+            </div>
+            {averageGrade &&
+              <div className="filter-selection-average-grade ">
+                <h5>Average Grade</h5>
+                <p className={'bt-red-text'}>{averageGrade}</p>
+              </div>
+            }
           </div>
-        </div>
-      </div>
-      <div className="filter-selection-aside">
-        <div className="filter-selection-units">
-          <h6>{`${units} Units`}</h6>
-        </div>
-        <div className="filter-selection-average-grade ">
-          <h5>Average Grade</h5>
-          <p className={'bt-red-text'}>{averageGrade}</p>
-        </div>
-      </div>
-      <div className="vertical-line" />
-    </div>
-  );
+          <div className="vertical-line" />
+        </button>
+    );
+  }
 }
 
 FilterSelection.defaultProps = {
-  courseAbbreviation: 'CS 61A',
-  courseTitle: 'The Structure and Interpretation of Computer Programs',
-  percentageEnrolled: 100,
-  units: 4,
-  waitlisted: 5,
-  averageGrade: 'B',
   borderColors: ['bt-blue-border', 'bt-green-border', 'bt-pink-border', 'bt-yellow-border'],
   gradeColors: {
     A: 'bt-green-text',
     B: 'bt-orange-text',
   },
-  id: 0,
 };
 
 export default FilterSelection;
