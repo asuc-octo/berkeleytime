19600d9892ccd15c8f65546763c221f132ae337c
Hantao (WIll) Wang <hantaowang97@gmail.com>
2019-03-04 13:35:33 -0800

    fix footer links and change banner in legacy site

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,16 +7,16 @@ function Footer() {
       <div className="columns is-mobile">
         <div className="column is-1-fullhd is-2-tablet is-4-mobile">
           <p className="btfooter-column"><b>Get Started</b></p>
-          <Link to="/dashboard" className="">Sign Up</Link>
           <Link to="/catalog" className="">Catalog</Link>
           <Link to="/enrollment" className="">Enrollment</Link>
+          <Link to="/grades" className="">Grades</Link>
         </div>
 
         <div className="column is-1-fullhd is-2-tablet is-4-mobile">
           <p className=""><b>Support</b></p>
-          <Link to="/" className="">Updates</Link>
-          <Link to="/">Report a Bug</Link>
-          <Link to="/" className="">Contact</Link>
+          <a href="http://old.berkeleytime-internal.com/" className="">Legacy Site</a>
+          <a href="https://goo.gl/forms/HDQ10XBDHJ0aCjhf1">Report a Bug</a>
+          <a href="mailto:octo.berkeleytime@asuc.org" className="">Contact</a>
         </div>
 
         <div className="column is-1-fullhd is-2-tablet is-4-mobile">
