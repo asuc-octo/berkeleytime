426141f8725353ff26dbef3c0b6d617d43d567fd
Michael Li <michael.li@berkeley.edu>
2019-03-10 15:57:59 -0700

    finish course selection card for enrollment

--- frontend/src/components/ClassCards/ClassCard.jsx
+++ frontend/src/components/ClassCards/ClassCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function ClassCard (props) {
-  const { course, title, semester, faculty } = props;
+  const { id, course, title, semester, faculty, removeCourse } = props;
 
   return (
     <div className="class-card card">
@@ -18,7 +18,7 @@ function ClassCard (props) {
           <div className="class-card classTitle">
             {title}
           </div>
-          <button type="button" className="delete" />
+          <button type="button" className="delete" onClick={() => removeCourse(id)} />
         </div>
       </div>
     </div>
