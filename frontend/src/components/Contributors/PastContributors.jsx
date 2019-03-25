4707670e8735228bbb3213cec3665a106194fb61
Hantao (WIll) Wang <hantaowang97@gmail.com>
2019-03-25 03:02:38 -0700

    artwork credit and join link

--- frontend/src/components/Contributors/PastContributors.jsx
+++ frontend/src/components/Contributors/PastContributors.jsx
@@ -27,6 +27,11 @@ function PastContributors({ founders, pastContributors }) {
                 ))} 
                     </div>
             </div>
+            <div className="graphic-contributors has-text-centered container is-fluid">
+                Landing page artwork by <a href="http://victorialynwu.design">Victoria Wu</a>
+                <br></br>
+                Detective bear artwork by <a href="http://victorialynwu.design">Someone Else</a>
+            </div>
         </div>
     </section>
 );
