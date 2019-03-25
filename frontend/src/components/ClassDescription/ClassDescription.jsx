c0ed2ee38fedd0a5c3b37d526bfbeded95b23baa
Michael Li <michael.li@berkeley.edu>
2019-03-25 11:34:07 -0700

    Fix catalog class description rendering

--- frontend/src/components/ClassDescription/ClassDescription.jsx
+++ frontend/src/components/ClassDescription/ClassDescription.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
+import { BarLoader } from 'react-spinners';
 
 import axios from 'axios';
 
@@ -16,6 +17,7 @@ class ClassDescription extends Component {
     this.state = {
       tab: 0,
       courseData: {},
+      loading: true,
     }
     this.details = this.details.bind(this)
     this.sections = this.sections.bind(this)
@@ -33,32 +35,23 @@ class ClassDescription extends Component {
     this.updateCourse(this.props.course);
   }
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if (Object.keys(this.state.courseData).length == 0) {
-  //     return true;
-  //   } else if (this.state.tab !== nextState.tab) {
-  //     return true;
-  //   }
-
-  //   console.log(this.props);
-  //   console.log(nextProps);
-
-  //   return this.state.courseData.course.id !== nextProps.course.id;
-  // }
-
   componentDidUpdate(prevProps, prevState) {
+    if(this.state.loading) {
+      return
+    }
+
     if(Object.keys(prevState.courseData).length == 0) {
       this.updateCourse(this.props.course);
     }
-    else if(this.props.course.id !== prevState.courseData.course.id) {
+    else if(this.props.course.id !== this.state.courseData.course.id) {
       this.updateCourse(this.props.course);
     }
   }
 
   updateCourse(course) {
     let courseID = course.id;
-    let courseAbbreviation = course.abbreviation;
-    let courseNumber = course.courseNumber;
+    this.setState({loading: true});
+
     axios.get(`/api/catalog_json/course_box/`, {
       params: {
         course_id: courseID,
@@ -68,23 +61,17 @@ class ClassDescription extends Component {
       console.log(res);
       this.setState({
         courseData: res.data,
+        loading: false,
       })
     })
     .catch((err) => {
-      if (err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-      }
-      console.log(err.config);
+      console.log(err)
     });
   }
 
   render() {
     const { tab, courseData } = this.state;
     let { course, sections } = courseData;
-
-    console.log(courseData);
 
     let gradeTo = {
       pathname: '/grades',
@@ -96,49 +83,60 @@ class ClassDescription extends Component {
       courseID: 112,
     }
     return (
-      <div>
-      {course &&
-        <div className="card filter-description">
-          <div className="filter-description-header">
-            <h3>{course.abbreviation} {course.course_number}</h3>
-            <p>{`${course.units} Unit${course.units !== '1.0' ? 's' : ''}`.replace(/.0/g, "").replace(/-/g, " - ")}</p>
-          </div>
-          <p className="filter-description-title">{course.title}</p>
-          <div className="filter-description-stats">
-            <FontAwesome className={`filter-description-stats-icon`} name={'bar-chart'}/>
-            <div className="filter-description-stats-avg">
-              <p>Course Average: {course.letter_average || 'N/A'}
-                &nbsp;(<Link to={gradeTo}>See grade distributions</Link>)
-              </p>
+      <div className="filter-description-container">
+      {this.state.loading ? (
+        <div className="filter-description-loading">
+            <BarLoader
+            sizeUnit={"px"}
+            size={150}
+            color={'#123abc'}
+            loading={true}
+          />
+       </div>
+      ) : (
+        Object.entries(course).length !== 0 &&
+          <div className="card filter-description">
+            <div className="filter-description-header">
+              <h3>{course.abbreviation} {course.course_number}</h3>
+              <p>{`${course.units} Unit${course.units !== '1.0' ? 's' : ''}`.replace(/.0/g, "").replace(/-/g, " - ")}</p>
+            </div>
+            <p className="filter-description-title">{course.title}</p>
+            <div className="filter-description-stats">
+              <FontAwesome className={`filter-description-stats-icon`} name={'bar-chart'}/>
+              <div className="filter-description-stats-avg">
+                <p>Course Average: {course.letter_average || 'N/A'}
+                  &nbsp;(<Link to={gradeTo}>See grade distributions</Link>)
+                </p>
 
+              </div>
+              <FontAwesome className={`filter-description-stats-icon`} name={'user-o'}/>
+              <div className="filter-description-stats-enroll">
+                <p>Enrollment: {course.enrolled}/{course.enrolled_max}
+                  &nbsp;(<Link to={enrollmentTo}>See enrollment history</Link>)
+                </p>
+              </div>
             </div>
-            <FontAwesome className={`filter-description-stats-icon`} name={'user-o'}/>
-            <div className="filter-description-stats-enroll">
-              <p>Enrollment: {course.enrolled}/{course.enrolled_max}
-                &nbsp;(<Link to={enrollmentTo}>See enrollment history</Link>)
-              </p>
+            {/* <p className="filter-description-instructors">Instructor(s): {info.instructors}</p> */}
+            <div className="filter-description-tabs">
+              <div className="tabs">
+                <ul>
+                  <li className={tab == 0 ? 'is-active' : ''}><a onClick={this.details}>Course Details</a></li>
+                  <li className={tab == 1 ? 'is-active' : ''}><a onClick={this.sections}>Sections</a></li>
+                </ul>
+              </div>
+              {tab == 0 ? (
+                <ClassDetails
+                  description={course.description}
+                  prerequisites={course.prerequisites}
+                />
+              ) : (
+                <ClassSections
+                  sections={sections}
+                />
+              )}
             </div>
           </div>
-          {/* <p className="filter-description-instructors">Instructor(s): {info.instructors}</p> */}
-          <div className="filter-description-tabs">
-            <div className="tabs">
-              <ul>
-                <li className={tab == 0 ? 'is-active' : ''}><a onClick={this.details}>Course Details</a></li>
-                <li className={tab == 1 ? 'is-active' : ''}><a onClick={this.sections}>Sections</a></li>
-              </ul>
-            </div>
-            {tab == 0 ? (
-              <ClassDetails
-                description={course.description}
-                prerequisites={course.prerequisites}
-              />
-            ) : (
-              <ClassSections
-                sections={sections}
-              />
-            )}
-          </div>
-        </div>
+      )
       }
       </div>
     );
