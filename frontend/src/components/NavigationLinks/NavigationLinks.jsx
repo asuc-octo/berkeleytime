5b6dad089f0dd690d77f7e777299d7b78093a24d
Hantao (WIll) Wang <hantaowang97@gmail.com>
2019-03-27 23:44:12 -0700

    work with jemma on style fixes and bugs

--- frontend/src/components/NavigationLinks/NavigationLinks.jsx
+++ frontend/src/components/NavigationLinks/NavigationLinks.jsx
@@ -8,13 +8,13 @@ function NavigationLinks() {
         <Link to="/catalog">Catalog</Link>
       </div>
       <div className="navbar-item">
-        <Link to="/scheduler">Scheduler</Link>
-      </div>
-      <div className="navbar-item">
         <Link to="/grades">Grades</Link>
       </div>
       <div className="navbar-item">
         <Link to="/enrollment">Enrollment</Link>
+      </div>
+      <div className="navbar-item">
+        <Link to="/scheduler">Scheduler</Link>
       </div>
       <div className="navbar-item">
         <Link to="/about">About</Link>
--- frontend/src/views/Error/Error.jsx
+++ frontend/src/views/Error/Error.jsx
@@ -13,7 +13,7 @@ function Error() {
                 <p>Looks like the page you were looking for couldn't be found.</p>
                 <p>Here are a couple things you can do.</p>
                 <div className="error-buttons">
-                <button type="button" className="button is-info error-classes"><Link to="/catalog"><b>Back to Classes</b></Link></button>
+                <button type="button" className="button error-info"><Link to="/catalog"><b>Back to Classes</b></Link></button>
                 <button type="button" className="button error-bugs"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdP9GySJRnQmmYFxdoxmcw1Ar_uw-cPXqRdXpJg-Lg2SPksaw/viewform"><b>Report a Bug</b></a></button>
                 </div>
               </div>
