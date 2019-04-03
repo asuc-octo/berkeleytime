73f37b2d1dbff98dc98315140c992d2bbdde78d3
Hantao (WIll) Wang <hantaowang97@gmail.com>
2019-04-03 15:45:17 -0700

    update script

--- berkeleytime/update.sh
+++ berkeleytime/update.sh
@@ -5,5 +5,5 @@ export SIS_CLASS_APP_KEY=***REMOVED***
 export SIS_COURSE_APP_ID=***REMOVED***
 export SIS_COURSE_APP_KEY=***REMOVED***
 python manage.py course
-python manage.py playlist
 python manage.py schedule
+python manage.py playlist
