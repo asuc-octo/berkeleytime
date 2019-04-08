d6507ef870df19478b73993e4424a119c16042db
Christopher Liu <chrisliu@berkeley.edu>
2019-04-07 18:42:39 -0700

    lint fixes; style fixes;

--- frontend/src/components/GradesInfoCard/GradesInfoCard.jsx
+++ frontend/src/components/GradesInfoCard/GradesInfoCard.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
-function formatPercentage(num) {
-  return (num * 100).toFixed(0);
+/** Accepts a percentile between 0 and 1, converts it to a string. */
+function percentileToString(percentile) {
+  if (percentile === 1) {
+    return '100th';
+  }
+  if (percentile === 0) {
+    return '0th';
+  }
+  var str = `${percentile}`.padEnd(4, '0').slice(2);
+  if (str[0] === '0') {
+    if (str[1] === '1') {
+      return str[1] + 'th';
+    } else if (str[1] === '2') {
+      return str[1] + 'nd';
+    } else if (str[1] === '3') {
+      return str[1] + 'rd';
+    } else {
+      return str[1] + 'th';
+    }
+  } else {
+    if (str[1] === '1' && str[0] !== '1') {
+      return str + 'th';
+    } else if (str[1] === '2' && str[0] !== '1') {
+      return str + 'nd';
+    } else if (str[1] === '3' && str[0] !== '1') {
+      return str + 'rd';
+    } else {
+      return str + 'th';
+    }
+  }
 }
 
 function getGradeColor(grade) {
@@ -90,8 +118,7 @@ export default function GradesInfoCard({
           <Col xs={6} className="class-avgs">
             <div className="class-stats-name">
               {selectedGrade !== undefined && selectedGrade !== null &&
-                `${formatPercentage(selectedGrade.percentile_low)}th
-                - ${formatPercentage(selectedGrade.percentile_high)}th Percentile`}
+                `${percentileToString(selectedGrade.percentile_low)} - ${percentileToString(selectedGrade.percentile_high)} Percentile`}
             </div>
           </Col>
           <Col xs={6} className="class-avgs">
@@ -111,7 +138,7 @@ export default function GradesInfoCard({
           <Col xs={6} className="class-avgs">
             <div className="class-percent">
               {selectedGrade !== undefined && selectedGrade !== null &&
-                `${formatPercentage(selectedGrade.percent)}%`}
+                `${Number.parseFloat(selectedGrade.numerator/denominator * 100).toFixed(2)} %`}
             </div>
           </Col>
         </Row>
