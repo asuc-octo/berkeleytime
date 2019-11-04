0de301e5aadd26eed550cc44cacd1e6825a1ea0f
Christopher Liu <chrisliu@berkeley.edu>
2019-11-03 17:03:21 -0800

    catalog class desc table

--- frontend/src/components/ClassDescription/ClassDescription.jsx
+++ frontend/src/components/ClassDescription/ClassDescription.jsx
@@ -144,41 +144,41 @@ class ClassDescription extends Component {
               {course.description}
             </p>
             <h5>Class Times</h5>
-            <div>
-            <Table className="table">
-              <thead>
-                <tr>
-                  <th style={{width: '75px'}}><abbr title="Lecture/Discussion/Lab">Type</abbr></th>
-                  <th style={{width: '50px'}}><abbr title="Course Capture Number">CCN</abbr></th>
-                  <th style={{width: '100px'}}>Instructor</th>
-                  <th style={{width: '85px'}}>Time</th>
-                  <th style={{width: '85px'}}>Location </th>
-                  <th style={{width: '75px'}}>Enrolled </th>
-                  <th style={{width: '75px'}}>Waitlist </th>
-                </tr>
-              </thead>
-              <tbody>
-                {sections.map(section => {
-                  let startDate = new Date(section.start_time + "Z");
-                  let endDate = new Date(section.end_time + "Z");
-                  return (
-                    <tr>
-                      <td>{section.kind}</td>
-                      <td>{section.ccn}</td>
-                      <td>{section.instructor}</td>
-                      {!isNaN(startDate) && !isNaN(endDate) ? (
-                      <td>{section.word_days} {ClassDescription.formatDate(startDate)} - {ClassDescription.formatDate(endDate)}</td>
-                      ) : (
-                        <td></td>
-                      )}
-                      <td>{section.location_name}</td>
-                      <td>{section.enrolled}/{section.enrolled_max}</td>
-                      <td>{section.waitlisted}</td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </Table>
+            <div className="table-container">
+              <Table className="table">
+                <thead>
+                  <tr>
+                    <th style={{width: '75px'}}><abbr title="Lecture/Discussion/Lab">Type</abbr></th>
+                    <th style={{width: '50px'}}><abbr title="Course Capture Number">CCN</abbr></th>
+                    <th style={{width: '100px'}}>Instructor</th>
+                    <th style={{width: '85px'}}>Time</th>
+                    <th style={{width: '85px'}}>Location </th>
+                    <th style={{width: '75px'}}>Enrolled </th>
+                    <th style={{width: '75px'}}>Waitlist </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sections.map(section => {
+                    let startDate = new Date(section.start_time + "Z");
+                    let endDate = new Date(section.end_time + "Z");
+                    return (
+                      <tr>
+                        <td>{section.kind}</td>
+                        <td>{section.ccn}</td>
+                        <td>{section.instructor}</td>
+                        {!isNaN(startDate) && !isNaN(endDate) ? (
+                        <td>{section.word_days} {ClassDescription.formatDate(startDate)} - {ClassDescription.formatDate(endDate)}</td>
+                        ) : (
+                          <td></td>
+                        )}
+                        <td>{section.location_name}</td>
+                        <td>{section.enrolled}/{section.enrolled_max}</td>
+                        <td>{section.waitlisted}</td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </Table>
             </div>
           </div>
         </div>
