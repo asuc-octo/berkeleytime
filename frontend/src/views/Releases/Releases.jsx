7fa51f53685b0a75daf5a148824203fc33d010ac
christinas24 <christina.shao@berkeley.edu>
2019-11-17 01:39:48 -0800

    Added new 'Releases' page

--- frontend/src/views/Releases/Releases.jsx
+++ frontend/src/views/Releases/Releases.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  ButtonToolbar
+} from 'react-bootstrap';
+
+import Log from '../../components/Releases/Log';
 
 class Releases extends Component {
   render() {
     return (
       <div className="releases">
-        <div className="releases-container">
-            <div>Berkeleytime Releases</div>
-            {/* {Releases.logs.map(item => <Log {...item} />)} */}
-        </div>
+        <Container>
+          <Row>
+            <Col lg={2}></Col>
+              <Col lg={8}>
+                <div className="releases-heading">
+                  <h2>Berkeleytime Releases</h2>
+                  <h3>Keep up-to-date with our releases and bug fixes.</h3>
+                  <ButtonToolbar className="releases-heading-button">
+                    <a className="btn btn-bt-red-inverted btn-bt-md" href="https://goo.gl/forms/HDQ10XBDHJ0aCjhf1" role="button">Report a Bug</a>
+                  </ButtonToolbar>
+                </div>
+              </Col>
+              <Col lg={2}></Col>
+          </Row>
+          <Row>
+            <Col lg={2}></Col>
+            <Col lg={8}>
+              {Releases.logs.map(item => <Log {...item} />)}
+            </Col>
+            <Col lg={2}></Col>
+          </Row>
+        </Container>
       </div>
     );
   }
@@ -16,13 +42,13 @@ class Releases extends Component {
 Releases.logs = [
   {
     date: 'September 5th, 2019',
-    new: [],
-    fixes: []
+    whatsNew: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", "Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore"],
+    fixes: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", "Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."]
   },
   {
     date: 'September 5th, 2019',
-    new: [],
-    fixes: []
+    whatsNew: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", "Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore"],
+    fixes: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", "Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."]
   },
 ];
 
