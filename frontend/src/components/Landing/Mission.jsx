01de444ea5808e73a09520172d40b4f803619bdc
Grace Luo <graceluo@berkeley.edu>
2019-11-17 18:20:58 -0800

    finished landing mobile views

--- frontend/src/components/Landing/Mission.jsx
+++ frontend/src/components/Landing/Mission.jsx
@@ -12,14 +12,14 @@ function Mission() {
     <div className="landing-mission">
       <Container>
         <Row>
-          <Col lg={6}>
+          <Col xs={6} s={6} md={6} lg={6}>
             <div className="landing-mission-column">
               <h3>Our Mission</h3>
               <p className="has-text-centered">Berkeleytime is an official organization under the ASUC Office of the Chief Technology Officer. We are dedicated to designing free, accessible software for students.</p>
               <a href="http://asucocto.org/">ASUC OCTO <FontAwesome name="long-arrow-right" /></a>
             </div>
           </Col>
-          <Col lg={6}>
+          <Col xs={6} s={6} md={6} lg={6}>
             <div className="landing-mission-column">
               <h3>Who We Are</h3>
               <p className="has-text-centered">We are a group of dedicated developers and designers helping simplify your course selection and planning experience as seamless as possible. Designed for students, by students.</p>
