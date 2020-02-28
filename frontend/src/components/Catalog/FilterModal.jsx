61b2bc827bd9cf1a335f7da6643622665d2a8501
Grace Luo <graceluo@berkeley.edu>
2020-02-28 12:45:19 -0800

    almost done with modal

--- frontend/src/components/Catalog/FilterModal.jsx
+++ frontend/src/components/Catalog/FilterModal.jsx
@@ -4,20 +4,30 @@ import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
 import {Modal, Form} from "react-bootstrap";
 
 function CheckboxGroup(props) {
-  console.log(props.nestedOptions)
+
   return (
     <Form>
       {props.nestedOptions.map(item => (
           <div>
-            <div className="filter-modal-label">{item.label}</div>
-            {Object.values(item.options).map(option => (
+            {item.options != null ?
+            <div className="filter-form-label">{item.label}</div> :
+            null}
+
+            {item.options != null ?
+            Object.values(item.options).map(option => (
               <Form.Check
                 type="checkbox"
                 id={option.value}
                 label={option.label}
                 onClick={props.handler}
               />
-              ))}
+              )) : 
+              <Form.Check
+                type="checkbox"
+                id={item.value}
+                label={item.label}
+                onClick={props.handler}
+              />}
           </div>
       ))}
     </Form>
@@ -29,16 +39,20 @@ export class FilterModal extends Component {
   render() {
     return (
       <Modal show={this.props.showFilters}>
-        <div className="filter">
           <div className="filter-modal">
-            <CheckboxGroup
-              nestedOptions={this.props.options}
-              handler={this.props.handleCheckbox}
-            />
-            <button className="btn-bt-primary-inverted" onClick={this.props.hideModal}> 
-            Cancel </button>
+            <div className="filter-form">
+              <CheckboxGroup
+                nestedOptions={this.props.options}
+                handler={this.props.handleCheckbox}
+              />
+            </div>
+            <div className="filter-button-bar">
+              <button className="btn-bt-primary-inverted" onClick={this.props.hideModal}> 
+              Cancel </button>
+              <button className="btn-bt-primary" onClick={this.props.hideModal}> 
+              Save </button>
+            </div>
           </div>
-        </div>
       </Modal>
     );
   }
