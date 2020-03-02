14a2bd654e539feac3b86035d2578f8460e0ef2e
Grace Luo <graceluo@berkeley.edu>
2020-03-01 17:16:10 -0800

    changed landing image

---
+++ frontend/src/components/Catalog/FilterModal.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
+import {Modal, Form} from "react-bootstrap";
+
+function CheckboxGroup(props) {
+
+  return (
+    <Form>
+      {props.nestedOptions.map(item => (
+          <div>
+            {item.options != null ?
+            <div className="filter-form-label">{item.label}</div> :
+            null}
+
+            {item.options != null ?
+      s      Object.values(item.options).map(option => (
+              <div className="custom-checkbox">
+                <Form.Check
+                  type="checkbox"
+                  id={option.value}
+                  name={option.label}
+                  label={option.label}
+                  onClick={props.handler.bind(this)}
+                />
+              </div>
+              )) : 
+              <div className="custom-checkbox">
+                <Form.Check
+                  type="checkbox"
+                  id={item.value}
+                  name={item.label}
+                  label={item.label}
+                  onClick={props.handler.bind(this)}
+               />
+              </div>}
+          </div>
+      ))}
+    </Form>
+  )
+}
+
+export class FilterModal extends Component {
+
+  render() {
+    return (
+      <Modal show={this.props.showFilters}>
+          <div className="filter-modal">
+            <div className="filter-form">
+              <CheckboxGroup
+                nestedOptions={this.props.options}
+                handler={this.props.storeSelection}
+              />
+            </div>
+            <div className="filter-button-bar">
+              <button className="btn-bt-primary-inverted" onClick={this.props.hideModal}> 
+              Cancel </button>
+              <button className="btn-bt-primary" onClick={this.props.saveModal}> 
+              Save </button>
+            </div>
+          </div>
+      </Modal>
+    );
+  }
+}
+
+export default FilterModal;
