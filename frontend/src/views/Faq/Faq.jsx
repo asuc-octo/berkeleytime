2672a4d26e2f9b33bd16160b477a87155880cff7
Ruochen <ruochen99@berkeley.edu>
2020-03-15 12:56:15 -0700

    faq

--- frontend/src/views/Faq/Faq.jsx
+++ frontend/src/views/Faq/Faq.jsx
@@ -31,7 +31,7 @@ class Faq extends Component {
                 <h2>Frequently Asked Questions</h2>
                 <h3>Answering your most commonly asked questions.</h3>
                 <ButtonToolbar className="faq-heading-button">
-                  <a className="btn btn-bt-blue-inverted btn-bt-md" role="button">Contact Us</a>
+                  <a className="btn btn-bt-blue-inverted btn-bt-md" role="button" href = "mailto: octo.berkeley@asuc.org">Contact Us</a>
                 </ButtonToolbar>
               </div>
             </Col>
@@ -53,19 +53,39 @@ class Faq extends Component {
 Faq.questions = [
   {
     question: 'Where does Berkeleytime get its data? Is it accurate?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore'
+    answer: 'BerkeleyTime sources our historic course data directly from Berkeley Student Information System’s Course and Enrollment APIs. We source grade data from CalAnswers. '
   },
   {
     question: 'How can I contact Berkeleytime?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore'
+    answer: 'Any questions or concerns that you have can be directed to octo.berkeley@asuc.org.'
   },
   {
     question: 'When are grades/classes/enrollment data released?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore'
+    answer: 'All of our data is made available to our users as soon as it is published by the school. For class information, this typically occurs 2 weeks before the start of Phase I. Grades data is typically published 6 weeks into the start of the following semester. Enrollment data is refreshed nightly as students enroll and drop classes.'
   },
   {
     question: 'Is anyone actively working on Berkeleytime?',
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore'
+    answer: 'We’re here! Our team here at UC Berkeley is continually working to deliver you all the best experience in selecting the courses you need to exceed. You can learn more about us here.'
+  },
+  {
+    question: 'How do I apply to join the team?',
+    answer: 'BerkeleyTime typically recruits students to work with our team at the start of every fall semester. Keep an eye out during the first couple of weeks of fall semester for any announcements about our recruitment.'
+  },
+  {
+    question: 'Berkeleytime stopped working. Now what?',
+    answer: 'Remain calm! If you find an issue with the site or data, please let us know by submitting a bug report.'
+  },
+  {
+    question: 'Can I access your API for a project I’m working on?',
+    answer: 'Yes! Our backend API is open and available for anyone to use. You can read the documentation here.'
+  },
+  {
+    question: 'How do I give the Berkeleytime team feedback?',
+    answer: 'Email us at octo.berkeley@asuc.org!'
+  },
+  {
+    question: 'I’m a student. How can I help the team out?',
+    answer: 'The best way for you to help is to report any bugs that you see on our platform so that we can continue to provide everyone with the most accurate and up to date data.'
   },
 ];
 
