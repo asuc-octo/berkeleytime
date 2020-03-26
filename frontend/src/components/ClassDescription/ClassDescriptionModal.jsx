6d6c1e3be6cdddc0adcc12b2f06e72f15af27673
Grace Luo <graceluo@berkeley.edu>
2020-03-25 17:48:59 -0700

    remove react-multiselect-checbox and fix mission

--- frontend/src/components/ClassDescription/ClassDescriptionModal.jsx
+++ frontend/src/components/ClassDescription/ClassDescriptionModal.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
 import {Modal, Form} from "react-bootstrap";
 import ClassDescription from './ClassDescription';
 
