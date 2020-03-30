313a89ae914ebf46e9caadd468f9637ee5664f39
Grace Luo <graceluo@berkeley.edu>
2020-03-29 20:38:36 -0700

    finished tooltip for grades

--- frontend/src/components/Graphs/GradesGraph.jsx
+++ frontend/src/components/Graphs/GradesGraph.jsx
@@ -9,11 +9,37 @@ import {
   Legend,
   ResponsiveContainer,
 } from 'recharts';
+import {
+  percentileToString
+} from '../../utils/utils';
 
 import vars from '../../variables/Variables';
 
+const MobileTooltip = props => {
+  const active = props.active;
+  const payload = props.payload;
+  const label = props.label;
+  if (active) {
+    const denominator = props.denominator;
+    const numerator = denominator * (props.payload[0].value / 100);
+    let percentile = props.selectedPercentiles;
+    let percentileLow = percentile ? percentileToString(percentile.percentile_low) : 0;
+    let percentileHigh = percentile ? percentileToString(percentile.percentile_high): 0;
+    let courseName = payload[0].name.split('/')[0];
+    return (
+      <div className="grades-tooltip">
+        <h6> Grade: {label} </h6>
+        <p style={{ color: props.color }}> {courseName} </p>
+        <p> {percentileLow} - {percentileHigh} percentile </p>
+        <p>{numerator}/{denominator}</p>
+      </div>
+    );
+  }
+  return null;
+};
+
 export default function GradesGraph({
-  graphData, gradesData, updateBarHover, updateGraphHover, isMobile
+  graphData, gradesData, updateBarHover, updateGraphHover, selectedPercentiles, denominator, color, isMobile
 }) {
 
   return (
@@ -41,7 +67,13 @@ export default function GradesGraph({
           <XAxis type="number" unit="%" />
           <YAxis dataKey="name" type="category" />
           <Tooltip
-            formatter={(value, name) => [`${Math.round(value * 10) / 10}%`, name]}
+            content={
+              <MobileTooltip 
+                selectedPercentiles={selectedPercentiles}
+                color={color}
+                denominator={denominator}
+              />
+            }
           />
           {gradesData.map((item, i) => (
             <Bar
