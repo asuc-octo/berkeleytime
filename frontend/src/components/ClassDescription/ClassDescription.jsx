39949c355bddf702de7b2516350c6834ca77be75
Will Wang <hwang97@berkeley.edu>
2020-04-12 03:36:59 -0700

    remove abbreviation tooltips

--- frontend/src/components/ClassDescription/ClassDescription.jsx
+++ frontend/src/components/ClassDescription/ClassDescription.jsx
@@ -145,13 +145,13 @@ class ClassDescription extends Component {
               <Table className="table">
                 <thead>
                   <tr>
-                    <th style={{width: '75px'}}><abbr title="Lecture/Discussion/Lab">Type</abbr></th>
-                    <th style={{width: '50px'}}><abbr title="Course Capture Number">CCN</abbr></th>
+                    <th style={{width: '75px'}}>Type</th>
+                    <th style={{width: '50px'}}>CCN</th>
                     <th style={{width: '100px'}}>Instructor</th>
                     <th style={{width: '85px'}}>Time</th>
-                    <th style={{width: '85px'}}>Location </th>
-                    <th style={{width: '75px'}}>Enrolled </th>
-                    <th style={{width: '75px'}}>Waitlist </th>
+                    <th style={{width: '85px'}}>Location</th>
+                    <th style={{width: '75px'}}>Enrolled</th>
+                    <th style={{width: '75px'}}>Waitlist</th>
                   </tr>
                 </thead>
                 <tbody>
