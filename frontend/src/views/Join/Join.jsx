317672fbcc1728e49432d58dd760472420502c6a
Jemma Kwak <yeogyoungkwak@gmail.com>
2020-04-18 17:57:49 -0700

    last of join

--- frontend/src/views/Join/Join.jsx
+++ frontend/src/views/Join/Join.jsx
@@ -1,23 +1,33 @@
 import React, { PureComponent } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, ButtonToolbar } from 'react-bootstrap';
+import retreat_silly from '../../assets/img/images/about/group/retreat_silly.png';
+import janet_jemma from '../../assets/img/images/about/group/janet_jemma.jpg';
 
-import doe from '../../assets/img/images/about/group/doe.jpg';
 
 class Join extends PureComponent {
   render() {
     const { values } = this.props;
 
     return (
-      <div className="join">
-        <div className="join-us">
-          <h5>Join our Team!</h5>
+      <div className="about">
+        <div className="about-us">
+          <h5>Join the BT Team! &#9996; </h5>
           <p>
-            We're a small group of student volunteers at UC Berkeley, dedicated to
-            simplifying the course discovery experience. We actively build, improve
-            and maintain Berkeleytime.
+            We'll be recruiting for new team members in the fall! Sign up to hear about recruitment updates.
           </p>
         </div>
+        <ButtonToolbar className="releases-heading-button join">
+        <input placeholder="Your email address"></input>
+          <a href="/join" role="button">
+            <button className="btn btn-bt-primary btn-bt-sm">
+              Sign up for Updates
+            </button>
+          </a>
+        </ButtonToolbar>
+        <img className="join-pic" src={janet_jemma} />
+
       </div>
+
     );
   }
 }
