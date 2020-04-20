9c7463f467573eee6af1ab720de6684498452475
Anson Tsai <yuancheng.tsai@berkeley.edu>
2020-04-19 22:38:05 -0700

    Add api endpoint and function to accept form responses

--- berkeleytime/berkeleytime/urls.py
+++ berkeleytime/berkeleytime/urls.py
@@ -84,7 +84,8 @@ urlpatterns = patterns('',
     url(r'scheduler/view_schedules/$', TemplateView.as_view(template_name="scheduler/view_schedules.html")),
 
     # Forms
-    url(r'^forms/config/(?P<config_name>[\w\d]+)/', 'forms.views.get_config'),
+    url(r'^forms/config/(?P<config_name>[\w\d]+)/$', 'forms.views.get_config'),
+    url(r'^forms/submit/(?P<config_name>[\w\d]+)/(?P<form_response>[^\/]+)/$', 'forms.views.record_response')
 
     # Direct to Template
     (r'^legal/terms/$', TemplateView.as_view(template_name="legal/terms.html")),
--- berkeleytime/forms/views.py
+++ berkeleytime/forms/views.py
@@ -6,6 +6,8 @@ from django.core.cache import cache
 from django.db.models import Avg, Sum
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
+from googleapi import *
 from yaml import load, dump, CLoader as Loader, CDumper as Dumper
 
 # Returns YAML file (in JSON format) with name
@@ -20,3 +22,8 @@ def get_config(request, config_name):
 	except:
 		print("Unexpected error within get_config. Raised error:")
 		raise
+
+# Calls on the function that uploads response to google drive
+# Form_response would be (<example>,<blah>),(<example1>,<blah1>),...
+def record_response(request, config_name, form_response):
+	check_yaml_response(config_name + ".yaml", list(ast.literal_eval(form_response)))
