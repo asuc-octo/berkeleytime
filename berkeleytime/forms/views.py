5ed782156d837c88fe17f5b37730c6ad560b8cf1
Will Wang <hwang97@berkeley.edu>
2020-04-20 00:30:34 -0700

    the last of the form changes

--- berkeleytime/forms/views.py
+++ berkeleytime/forms/views.py
@@ -1,27 +1,23 @@
-from __future__ import division
-import re, os, sys, datetime
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from berkeleytime.utils.requests import render_to_json
 
-from django.core.cache import cache
-from django.db.models import Avg, Sum
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-from googleapi import *
-from yaml import load, dump, CLoader as Loader, CDumper as Dumper
+from googleapi import check_yaml_response
+try:
+    from yaml import load, dump, CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import load, dump, Loader, Dumper
 
 # Returns YAML file (in JSON format) with name
 def get_config(request, config_name):
-# YAML_PATH is the filepath to the yaml file
+
 	try:
-		file = open(config_name + ".yaml")
-		loaded_yaml = load(file, Loader=Loader)
+		with open("forms/configs/{}.yaml".format(config_name), "r") as f:
+			loaded_yaml = load(f, Loader=Loader)
 		return render_to_json(loaded_yaml)
 	except FileNotFoundError:
 		print("Error when trying to read file:", config_name + ".yaml", "ERROR: FileNoteFoundError")
-	except:
+	except Exception:
 		print("Unexpected error within get_config. Raised error:")
-		raise
+
 
 # Calls on the function that uploads response to google drive
 # Form_response would be (<example>,<blah>),(<example1>,<blah1>),...
