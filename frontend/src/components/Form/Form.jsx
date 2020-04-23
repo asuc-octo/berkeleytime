9c16ba3a1d846c6793e8ba519f73e74fcf13e988
Will Wang <hwang97@berkeley.edu>
2020-04-22 17:15:30 -0700

    add ability to read files

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -10,11 +10,14 @@ class BTForm extends Component {
       responses: {},
       validated: false,
       validation: {},
+      filesUploaded: {},
     };
+
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleCheck = this.handleCheck.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFileUpload = this.handleFileUpload.bind(this);
+    this.removeFile = this.removeFile.bind(this);
   }
 
   componentDidMount() {
@@ -26,7 +29,10 @@ class BTForm extends Component {
         let responses = {};
         form.questions.map(function(question) {
           if (question.type === "file") {
-            responses[question.unique_name] = React.createRef();
+            responses[question.unique_name] = {
+              ref: React.createRef(),
+              files: [],
+            };
           }
         });
         this.setState({
@@ -104,8 +110,76 @@ class BTForm extends Component {
   }
 
   handleFileUpload(event) {
+    const target = event.target;
+    const { form, responses } = this.state;
+    const files = [...responses[target.name].files];
+    const fileNames = responses[target.name].files.map(file => file.name);
+    for (let i = 0; i < target.files.length; i++) {
+      if (!fileNames.includes(target.files[i].name)) {
+        files.push(target.files[i]);
+        fileNames.push(target.files[i].name);
+      }
+    }
+
+    let validation = {};
+    this.validateAll(form, {
+          ...responses,
+          [target.name]: {
+            ...responses[target.name],
+            files: files
+          }
+        }, validation);
+    for (let i = 0; i < target.files.length; i++) {
+      let reader = new FileReader();
+      const file = target.files[i];
+      reader.onload = (event) => {
+        this.setState(prevState => ({
+          filesUploaded: {
+            ...prevState.filesUploaded,
+            [target.name]: {
+              ...prevState.filesUploaded[target.name] ? {...prevState.filesUploaded[target.name]} : {...{}},
+              [file.name]: event.target.result,
+            }
+          }
+        }))
+      };
+      reader.readAsBinaryString(file);
+    }
+
+    this.setState(prevState => ({
+        responses: {
+          ...prevState.responses,
+          [target.name]: {
+            ...prevState.responses[target.name],
+            files: files
+          }
+        },
+        validation: prevState.validated ? validation : {},
+      }));
     event.preventDefault();
-    this.forceUpdate();
+  }
+
+  removeFile(question, removedFile) {
+    const { form, responses, filesUploaded } = this.state;
+    const files = responses[question].files.filter(file => file.name !== removedFile);
+    let validation = {};
+    this.validateAll(form, {
+          ...responses,
+          [question]: {
+            ...responses[question],
+            files: files
+          }
+        }, validation);
+    this.setState(prevState => ({
+        responses: {
+          ...prevState.responses,
+          [question]: {
+            ...prevState.responses[question],
+            files: files
+          }
+        },
+        validation: prevState.validated ? validation : {},
+      }));
   }
 
   setValidation(question, message, valid, validation) {
@@ -133,8 +207,17 @@ class BTForm extends Component {
 
   validateRequired(form, responses, validation) {
     for (let question of form.questions) {
-      if (question.required && !responses[question.unique_name]) {
-        this.setValidation(question, "This question is required", false, validation);
+      if (question.required) {
+        if (question.type === "file" && responses[question.unique_name].files.length === 0) {
+          this.setValidation(question, "This question is required", false, validation);
+        } else if (question.type === "multiple_select") {
+          if (!responses[question.unique_name] || responses[question.unique_name].length === 0) {
+            this.setValidation(question, "This question is required", false, validation);
+          }
+        }
+        else if (!responses[question.unique_name] || responses[question.unique_name].length === 0) {
+          this.setValidation(question, "This question is required", false, validation);
+        }
       }
     }
   }
@@ -292,18 +375,16 @@ class BTForm extends Component {
     let valid = validation[question.unique_name] ? validation[question.unique_name].valid : false;
     let invalid = validation[question.unique_name] ? !validation[question.unique_name].valid : false;
     let fileList = [];
-    if (responses[question.unique_name] && responses[question.unique_name].current && responses[question.unique_name].current.files) {
-      for (var i = 0; i < responses[question.unique_name].current.files.length; i++) {
-        fileList.push(responses[question.unique_name].current.files[i].name);
+    if (responses[question.unique_name] && responses[question.unique_name].files) {
+      for (var i = 0; i < responses[question.unique_name].files.length; i++) {
+        fileList.push(responses[question.unique_name].files[i].name);
       }
     }
     return (
       <div>
         <Form.File
-          label={ fileList.length === 0
-            ? question.placeholder
-            : fileList.join(", ")
-          }
+          label={ question.placeholder }
+          name={ question.unique_name }
           accept={ question.accept ? question.accept : ""}
           ref={ responses[question.unique_name] }
           onChange = { this.handleFileUpload }
@@ -313,12 +394,26 @@ class BTForm extends Component {
           multiple
           custom
         />
+        <ListGroup>
+          { fileList.map(file =>
+              <ListGroup.Item>{ file }
+              <span className="uploaded-file-remove"
+                onClick={ ()=> { this.removeFile(question.unique_name, file) }}
+              >
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M6.00001 19C6.00001 20.1 6.90001 21 8 21H16C17.1 21 18 20.1 18 19V7H6.00001V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="#FC7676"/>
+                </svg>
+              </span>
+              </ListGroup.Item>
+            )
+          }
+        </ListGroup>
       </div>
     )
   }
 
   render() {
-    const { form, responses, validated, validation } = this.state;
+    const { form, responses, filesUploaded, validation } = this.state;
 
     if (form === null) {
       return null;
