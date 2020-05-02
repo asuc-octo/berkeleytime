7d0f63982d0fb3348db439995b08f74b4c1a8dde
Will Wang <hwang97@berkeley.edu>
2020-05-02 03:18:00 -0700

    bring back banner for general public

--- frontend/src/Berkeleytime.jsx
+++ frontend/src/Berkeleytime.jsx
@@ -25,7 +25,7 @@ class Berkeleytime extends Component {
   constructor(props) {
     super(props);
 
-    const key = 'apr2020_staging_test';
+    const key = 'bt-release-may-2020';
     if (!localStorage[key]) {
       localStorage[key] = true;
       const { dispatch } = this.props;
@@ -74,7 +74,7 @@ class Berkeleytime extends Component {
                  !...:!TVBBBRPFT||||||||||!!^^""'   ||||
                  !.......:!?|||||!!^^""'            ||||
                  !.........||||                     ||||
-                 !.........||||  ##                 ||||
+                 !.........||||  $                  ||||
                  !.........||||                     ||||
                  !.........||||                     ||||
                  !.........||||                     ||||
