4ee116e5168f0153dc6b7e4e8b8c166ba8d8a8cf
gundamMC <gundammc@gundammc.com>
2020-10-07 00:37:03 -0700

    Add user app and BerkeleytimeUser model

--- backend/berkeleytime/settings.py
+++ backend/berkeleytime/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'berkeleytime',
+    'user',
 ]
 
 # Middlewares
---
+++ backend/user/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from user.models import BerkeleytimeUser
+
+# Register your models here.
+admin.site.register(BerkeleytimeUser)
