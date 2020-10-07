2b0b9bb4c042ceb6dfa030d426f8ee17fb1cbcb1
gundamMC <gundammc@gundammc.com>
2020-10-07 00:37:03 -0700

    Add user app and BerkeleytimeUser model

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BerkeleytimeUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='berkeleytimeuser')
+    major = models.CharField(max_length=100)
+    # potentially many to many with a major model
+    # saved_classes = many to many to Course
+    # saved_schedules = one to many to Schedule
+    # and a bunch of BooleanFields for email preferences
