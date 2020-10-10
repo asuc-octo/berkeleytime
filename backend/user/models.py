769914ae224b00f20ccdd139f853254f35330777
gundamMC <gundammc@gundammc.com>
2020-10-09 17:48:13 -0700

    Extend user model

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from catalog.models import Course
 
 # Create your models here.
 
@@ -7,6 +8,11 @@ class BerkeleytimeUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='berkeleytimeuser')
     major = models.CharField(max_length=100)
     # potentially many to many with a major model
-    # saved_classes = many to many to Course
+    saved_classes = models.ManyToManyField(Course)
     # saved_schedules = one to many to Schedule
-    # and a bunch of BooleanFields for email preferences
+
+    # email preferences
+    email_class_update = models.BooleanField(default=False)
+    email_grade_update = models.BooleanField(default=False)
+    email_enrollment_opening = models.BooleanField(default=False)
+    email_berkeleytime_update = models.BooleanField(default=False)
