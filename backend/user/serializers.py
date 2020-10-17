a959eed1260feb0579416e364fc61f84da04c443
gundamMC <gundammc@gundammc.com>
2020-10-17 13:17:03 -0700

    Add user creation and update

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from user.models import BerkeleytimeUser
+
+UserModel = get_user_model()
 
 class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -11,3 +14,20 @@ class BerkeleytimeUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = BerkeleytimeUser
         fields = '__all__'
+
+    def create(self, validated_data):
+        # create django user
+        user = UserModel.objects.create(
+            email=validated_data['user']['email'],
+            first_name=validated_data['user']['first_name'],
+            last_name=validated_data['user']['last_name']
+            )
+        user.save()
+        
+        # create berkeleytimeuser
+        btUser = BerkeleytimeUser.objects.create(
+            user = user,
+            # other fields are left at default upon registration
+        )
+
+        return btUser
