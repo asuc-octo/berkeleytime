308fb1b1327169564d67f618bce96d2336efd6da
jonathanpan777 <jonathanpan@berkeley.edu>
2020-10-28 20:05:42 -0700

    schemas

--- backend/berkeleytime/schema.py
+++ backend/berkeleytime/schema.py
@@ -1,5 +1,10 @@
 import graphene
 
+from catalog.schema import Query as CatalogQuery
+from enrollment.schema import Query as EnrollmentQuery
+from forms.schema import Query as FormsQuery
+from grades.schema import Query as GradesQuery
+from playlist.schema import Query as PlaylistQuery
 from user.schema import Query as UserQuery
 from user.schema import Mutation as UserMutation
 
