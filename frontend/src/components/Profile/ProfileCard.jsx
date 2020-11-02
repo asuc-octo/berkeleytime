4a2a0824ace06f6172b509f49c85c1089dbb1260
Austin George <ageorge8000@gmail.com>
2020-11-01 23:05:06 -0500

    Imported Utils & Removed Mobile Styling

--- frontend/src/components/Profile/ProfileCard.jsx
+++ frontend/src/components/Profile/ProfileCard.jsx
@@ -1,32 +1,7 @@
 import React, { PureComponent } from 'react';
+import { formatUnits, formatPercentage, applyIndicatorPercent, applyIndicatorGrade } from '../../utils/utils'
 
 class ProfileCard extends PureComponent {
-  static formatEnrollmentPercentage(percentage) {
-    return `${Math.floor(percentage * 100, 100)}% enrolled`;
-  }
-
-  static formatUnits(units) {
-    return `${units} Unit${units === '1.0' || units === '1' ? '' : 's'}`.replace(/.0/g, '').replace(/ - /, '-').replace(/ or /g, '-');
-  }
-
-  static colorEnrollment(percentage) {
-    const pct = percentage * 100;
-    if (pct < 33) {
-      return 'enrollment-first-third';
-    } else if (pct < 67) {
-      return 'enrollment-second-third';
-    } else {
-      return 'enrollment-last-third';
-    }
-  }
-
-  static colorGrade(grade) {
-    if (grade === '') {
-      console.error('colorGrade: no grade provided!');
-      return '';
-    }
-    return `grade-${grade[0]}`;
-  }
 
   render() {
     const {
@@ -47,14 +22,14 @@ class ProfileCard extends PureComponent {
             <div className="profile-card-info-stats">
               { enrolled_percentage === -1
                 ? null
-                : <p className={ProfileCard.colorEnrollment(enrolled_percentage)}>{ProfileCard.formatEnrollmentPercentage(enrolled_percentage)}</p>
+                : applyIndicatorPercent(formatPercentage(enrolled_percentage),enrolled_percentage)
               }
 
-              <p>&nbsp;•&nbsp;{ProfileCard.formatUnits(units)}</p>
+              <p>&nbsp;•&nbsp;{formatUnits(units)}</p>
             </div>
           </div>
           <div className="profile-card-sort profile-card-grade">
-            <h6 className={ProfileCard.colorGrade(letter_average)}>{letter_average}</h6>
+            {applyIndicatorGrade(letter_average,letter_average)}
           </div>
         </div>
       </div>
