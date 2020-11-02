631b5702e51b8c1445e9f90a643994cd66365b04
Vihan <vihan+github@vihan.org>
2020-11-01 22:59:25 -0800

    Add frontend graphql config
    Signed-off-by: Vihan <vihan+github@vihan.org>

---
+++ frontend/relay.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // ...
+    // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
+    src: "./src",
+    schema: "./schema.graphql",
+    exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+    language: "typescript",
+    extensions: ["js", "jsx", "ts", "tsx"]
+};
