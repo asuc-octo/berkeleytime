677e56c81aa22aa36bc3a40e679cb7df5ee46ee3
Austin George <ageorge8000@gmail.com>
2020-11-06 22:13:56 -0500

    Added Edit Buttons & Major Dropdown

--- frontend/src/components/Profile/AccountSubview.jsx
+++ frontend/src/components/Profile/AccountSubview.jsx
@@ -1,9 +1,18 @@
 import React, { PureComponent } from 'react';
 import Property from './Property';
 import ProfileCard from './ProfileCard';
+import EditPencil from '../../assets/svg/profile/edit.svg';
 
 class AccountSubview extends PureComponent {
   render() {
+  
+    //Information has been hardcoded for testing purposes only
+    const options = [
+      { value: "Psychology", label: "Psychology" },
+      { value: "Physics", label: "Physics" },
+      { value: "Philosophy", label: "Philosophy" },
+      { value: "Rhetoric", label: "Rhetoric" },
+    ];
 
     return (
       <div className="profile-subview">
@@ -11,11 +20,14 @@ class AccountSubview extends PureComponent {
         <h2>Personal Information</h2>
 
         {/* Information has been hardcoded for testing purposes only */}
-        <Property attribute="Full Name" value="Oski Bear"/>
-        <Property attribute="bConnected Email" value="oski@berkeley.edu"/>
-        <Property attribute="Major(s)" value="Psychology"/>
+        <Property options = {options} attribute="Full Name" value="Oski Bear"/>
+        <Property options = {options} attribute="bConnected Email" value="oski@berkeley.edu"/>
+        <Property options = {options} attribute="Major(s)" value="Psychology"/>
         
+        <div className="profile-title">
         <h2>Saved Classes</h2>
+        <button className="edit-button"><img className="edit-pencil" src={EditPencil} alt="Edit Classes"/></button>
+        </div>
 
         {/* Information has been hardcoded for testing purposes only */}
         <ProfileCard abbreviation="ASAMST" course_number="143AC" title="Asian American Health" letter_average="A" enrolled_percentage=".5" units="3"/>
