fa12413d18844420b6390e6cfcf975e7fbd6b675
Leon Ming <leon.ming@berkeley.edu>
2020-11-08 20:42:19 -0800

    added grade and enrollment filters for course query

--- backend/catalog/schema.py
+++ backend/catalog/schema.py
@@ -1,8 +1,7 @@
+import django_filters
 import graphene
-from graphene import Node
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from graphql import GraphQLError
 
 from catalog.models import Course, Section
 
@@ -11,19 +10,45 @@ class CourseType(DjangoObjectType):
     class Meta:
         model = Course
         filter_fields = '__all__'
-        interfaces = (Node, )
+        interfaces = (graphene.Node, )
+
+
+class CourseFilter(django_filters.FilterSet):
+    has_grades = django_filters.BooleanFilter(method='filter_has_grades')
+    # overrides Course.has_enrollment. TODO: Remove Course.has_enrollment soon
+    has_enrollment = django_filters.BooleanFilter(method='filter_has_enrollment')
+
+    def filter_has_grades(self, queryset, name, value):
+        return queryset.exclude(grade__isnull=value)
+
+    def filter_has_enrollment(self, queryset, name, value):
+        return queryset.exclude(section__isnull=value)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
 
 
 class SectionType(DjangoObjectType):
     class Meta:
         model = Section
         filter_fields = '__all__'
-        interfaces = (Node, )
+        interfaces = (graphene.Node, )
+
+    @property
+    def qs(self):
+        return super().qs.filter(disabled=False)
+
+
+class SectionFilter(django_filters.FilterSet):
+    class Meta:
+        model = Section
+        fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    course = Node.Field(CourseType)
-    all_courses = DjangoFilterConnectionField(CourseType)
+    course = graphene.Node.Field(CourseType)
+    all_courses = DjangoFilterConnectionField(CourseType, filterset_class=CourseFilter)
 
-    section = Node.Field(SectionType)
-    all_sections = DjangoFilterConnectionField(SectionType)
+    section = graphene.Node.Field(SectionType)
+    all_sections = DjangoFilterConnectionField(SectionType, filterset_class=SectionFilter)
--- backend/enrollment/schema.py
+++ backend/enrollment/schema.py
@@ -1,5 +1,4 @@
 import graphene
-from graphene import Node
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphql import GraphQLError
@@ -11,8 +10,8 @@ class EnrollmentType(DjangoObjectType):
     class Meta:
         model = Enrollment
         filter_fields = '__all__'
-        interfaces = (Node, )
+        interfaces = (graphene.Node, )
 
 class Query(graphene.ObjectType):
     all_enrollments = DjangoFilterConnectionField(EnrollmentType)
-    enrollment = Node.Field(EnrollmentType)
+    enrollment = graphene.Node.Field(EnrollmentType)
