7a47f12a6136ce963f48a8d7b4c5e30be9bf88f5
Leon Ming <leon.ming@berkeley.edu>
2020-11-10 00:06:03 -0800

    don't return l&s playlists before current semester

--- backend/playlist/schema.py
+++ backend/playlist/schema.py
@@ -1,7 +1,9 @@
 import graphene
+from django.db.models import Q
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
+from berkeleytime.settings import CURRENT_SEMESTER, CURRENT_YEAR
 from playlist.models import Playlist
 
 
@@ -13,7 +15,8 @@ class PlaylistType(DjangoObjectType):
 
     @classmethod
     def get_queryset(cls, queryset, info):
-        return queryset.exclude(category='custom')
+        return (queryset.exclude(Q(category='custom') | Q(category='ls')) |
+               queryset.filter(category='ls', semester=CURRENT_SEMESTER, year=CURRENT_YEAR))
 
 class Query(graphene.ObjectType):
     all_playlists = DjangoFilterConnectionField(PlaylistType)
