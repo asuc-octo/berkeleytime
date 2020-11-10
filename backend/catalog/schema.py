bfe3e52888a9eeab2db7f012f013a2526cfa7814
jonathanpan777 <jonathanpan@berkeley.edu>
2020-11-09 21:23:52 -0800

    filter in playlists

--- backend/catalog/schema.py
+++ backend/catalog/schema.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 import django_filters
 import graphene
 from graphene_django import DjangoObjectType
@@ -14,15 +16,32 @@ class CourseType(DjangoObjectType):
 
 
 class CourseFilter(django_filters.FilterSet):
+    class PlaylistFilter(django_filters.UUIDFilter, django_filters.BaseInFilter):
+        pass
+
     has_grades = django_filters.BooleanFilter(method='filter_has_grades')
     # overrides Course.has_enrollment. TODO: Remove Course.has_enrollment soon
     has_enrollment = django_filters.BooleanFilter(method='filter_has_enrollment')
 
+    in_playlists = PlaylistFilter(method='filter_in_playlists')
+    
     def filter_has_grades(self, queryset, name, value):
         return queryset.exclude(grade__isnull=value)
 
     def filter_has_enrollment(self, queryset, name, value):
         return queryset.exclude(section__isnull=value)
+    
+    def filter_in_playlists(self, queryset, name, value):
+        categories = Playlist.objects.filter(id__in=value).distinct('category').values_list('category', flat=True)
+        all_reduce = [queryset]
+        for category in categories: 
+            playlists = Playlist.objects.filter(id__in=filter_ids, category=category)
+            intersected = [category.courses.all() for playlist in playlists]
+            if category in ('university', 'ls', 'engineering', 'haas'):
+                all_reduce.append(reduce(lambda x, y: x & y, intersected))
+            else:
+                all_reduce.append(reduce(lambda x, y: x | y, intersected))
+        return reduce(lambda  x, y: x & y, all_reduce).distinct()
 
     class Meta:
         model = Course
