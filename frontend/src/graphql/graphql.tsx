ff23f7a8dc0f15b24338654951bc8554ade99a26
Vihan <vihan+github@vihan.org>
2020-11-13 19:34:56 -0800

    Login mutations with graphql backend
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- frontend/src/graphql/graphql.tsx
+++ frontend/src/graphql/graphql.tsx
@@ -633,6 +633,32 @@ export interface Verify {
   payload: Scalars['GenericScalar'];
 }
 
+export type UserFragment = (
+  { __typename?: 'UserType' }
+  & Pick<UserType, 'id' | 'firstName' | 'lastName' | 'email' | 'username'>
+);
+
+export type LoginMutationVariables = Exact<{
+  token: Scalars['String'];
+}>;
+
+
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { login?: Maybe<(
+    { __typename?: 'ObtainJSONWebToken' }
+    & Pick<ObtainJsonWebToken, 'newUser'>
+    & { user?: Maybe<(
+      { __typename?: 'BerkeleytimeUserType' }
+      & Pick<BerkeleytimeUserType, 'id' | 'major'>
+      & { user: (
+        { __typename?: 'UserType' }
+        & UserFragment
+      ) }
+    )> }
+  )> }
+);
+
 export type GetFiltersQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -667,7 +693,54 @@ export type StubQuery = (
   )> }
 );
 
+export const UserFragmentDoc = gql`
+    fragment User on UserType {
+  id
+  firstName
+  lastName
+  email
+  username
+}
+    `;
+export const LoginDocument = gql`
+    mutation Login($token: String!) {
+  login(tokenId: $token) {
+    newUser
+    user {
+      id
+      major
+      user {
+        ...User
+      }
+    }
+  }
+}
+    ${UserFragmentDoc}`;
+export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
 
+/**
+ * __useLoginMutation__
+ *
+ * To run a mutation, you first call `useLoginMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLoginMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [loginMutation, { data, loading, error }] = useLoginMutation({
+ *   variables: {
+ *      token: // value for 'token'
+ *   },
+ * });
+ */
+export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, baseOptions);
+      }
+export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
+export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
+export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
 export const GetFiltersDocument = gql`
     query GetFilters {
   allPlaylists {
