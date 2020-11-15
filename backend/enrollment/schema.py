52ff556d95fca15ebc3acdcec608899ae556a302
gundamMC <gundammc@gundammc.com>
2020-11-14 23:34:23 -0800

    Add demo endpoint for aggregate enrollment

--- backend/enrollment/schema.py
+++ backend/enrollment/schema.py
@@ -3,8 +3,12 @@ from graphene import Node
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphql import GraphQLError
+from graphql_relay.node.node import from_global_id
+from django.db.models import Sum
 
 from enrollment.models import Enrollment
+from catalog.models import Course
+from catalog.schema import CourseType, SectionType
 
 
 class EnrollmentType(DjangoObjectType):
@@ -13,6 +17,66 @@ class EnrollmentType(DjangoObjectType):
         filter_fields = '__all__'
         interfaces = (Node, )
 
+class EnrollmentData(graphene.ObjectType):
+    date_created = graphene.Date()
+    enrolled = graphene.Int()
+    enrolled_max = graphene.Int()
+
+
+class EnrollmentInfo(graphene.ObjectType):
+    course = graphene.Field(CourseType)
+    section = graphene.Field(SectionType)
+    aggregate = graphene.Boolean()
+    data = graphene.List(EnrollmentData)
+    enrolled_max = graphene.Int()
+    enrolled_percent_max = graphene.Float()
+    enrolled_scale_max = graphene.Int()
+    waitlisted_max = graphene.Int()
+    waitlisted_percent_max = graphene.Float()
+    waitlisted_scale_max = graphene.Int()
+
+
 class Query(graphene.ObjectType):
     all_enrollments = DjangoFilterConnectionField(EnrollmentType)
     enrollment = Node.Field(EnrollmentType)
+    aggregated_enrollment_by_semester = graphene.Field(
+        EnrollmentInfo,
+        course_id = graphene.ID(),
+        semester = graphene.String(),
+        year = graphene.Int()
+    )
+
+    def resolve_aggregated_enrollment_by_semester(root, info, course_id, semester, year):
+        course = Course.objects.get(pk = from_global_id(course_id)[1])
+        sections = course.section_set.all().filter(semester = semester, year = year, disabled = False, is_primary = True)
+
+        enrollments = Enrollment.objects.filter(section__in = sections).values('date_created').annotate(enrolled=Sum('enrolled'), enrolled_max=Sum('enrolled_max')).order_by('date_created')
+
+        models = []
+        for data in enrollments:
+            e = EnrollmentData(**data)
+            models.append(e)
+
+        enrolled_max = models[-1].enrolled_max
+
+        return EnrollmentInfo(
+            course = course,
+            aggregate = True,
+            data = models,
+            enrolled_max = enrolled_max
+        )
+
+        # stuff from the original implementation:
+        # last_date = sections[0].enrollment_set.all().latest('date_created').date_created
+        # enrolled_max = Enrollment.objects.filter(section__in = sections, date_created = last_date).aggregate(Sum('enrolled_max'))['enrolled_max__sum']
+        # waitlisted_max = Enrollment.objects.filter(section__in = sections, date_created = last_date).aggregate(Sum('waitlisted_max'))['waitlisted_max__sum']
+
+        # dates = {d: [0, 0] for d in sections[0].enrollment_set.all().values_list('date_created', flat = True)}
+
+
+        # enrolled_outliers = [d['enrolled_percent'] for d in rtn['data'] if d['enrolled_percent'] >= 1.0]
+        # enrolled_percent_max = max(enrolled_outliers) * 1.10 if enrolled_outliers  else 1.10
+        # waitlisted_outliers = [d['waitlisted_percent'] for d in rtn['data'] if d['waitlisted_percent'] >= 1.0]
+        # waitlisted_percent_max = max(waitlisted_outliers) * 1.10 if waitlisted_outliers else 1.10
+        # enrolled_scale_max = int(rtn['enrolled_percent_max'] * rtn['enrolled_max'])
+        # waitlisted_scale_max = int(rtn['waitlisted_percent_max'] * rtn['waitlisted_max'])
