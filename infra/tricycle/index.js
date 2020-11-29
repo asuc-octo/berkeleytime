51f3087c05ad0361647d0d7b966cec1ac100eb8c
uberkelium <22272118+uberkelium@users.noreply.github.com>
2020-11-29 01:34:56 -0800

    Fix vulnerability in tricycle deployment rotation, separate Redis staging and prod passwords, merge system files (#375)

--- infra/tricycle/index.js
+++ infra/tricycle/index.js
@@ -17,13 +17,25 @@ const {
   CI_COMMIT_BRANCH,
   FILEPATH_DEPLOY_BACKEND,
   FILEPATH_DEPLOY_FRONTEND,
+  FILEPATH_DEPLOY_INGRESS,
   SECRET_KUBERNETES_CREDENTIALS,
   TRICYCLE_MAX_NUMBER_OF_LIVE_DEV_BRANCHES,
 } = process.env;
-
 const BASE_NAME_DEPLOYMENT_BACKEND = `bt-backend-dev-`;
-const FILEPATH_DEPLOY_INGRESS = `infra/k8s/default/bt-ingress-tricycle.yaml`;
-
+const ILLEGAL_BRANCH_NAMES = ["gcp", "ocf", "staging", "www"];
+const SUBDOMAIN_PATTERN = /^[A-Za-z0-9](?:[A-Za-z0-9\-]{0,61}[A-Za-z0-9])?$/; // RFC-1123 subdomain name compliance
+if (ILLEGAL_BRANCH_NAMES.includes(CI_COMMIT_BRANCH)) {
+  throw Error(
+    `'${CI_COMMIT_BRANCH}' is an illegal branch name: '${ILLEGAL_BRANCH_NAMES.map(
+      (branch) => branch + ".berkeleytime.com"
+    ).join(", ")}' are special reserved URLs`
+  );
+}
+if (!SUBDOMAIN_PATTERN.test(CI_COMMIT_BRANCH)) {
+  throw Error(
+    `'${CI_COMMIT_BRANCH}' is an illegal branch name: it does not conform to RFC-1123 domain name standards`
+  );
+}
 await tee(
   `export CI_ENVIRONMENT_NAME=dev-${CI_COMMIT_BRANCH}; envsubst < ${FILEPATH_DEPLOY_BACKEND} | kubectl apply -f - --kubeconfig ${SECRET_KUBERNETES_CREDENTIALS}`
 );
@@ -42,18 +54,21 @@ const backendDeployments = (
   .split("\n")
   .filter((name) => name.includes(BASE_NAME_DEPLOYMENT_BACKEND));
 if (backendDeployments.length > TRICYCLE_MAX_NUMBER_OF_LIVE_DEV_BRANCHES) {
-  const prunedBranch = backendDeployments[0]
-    .split("/")[1]
-    .split("-")
-    .slice(3)
-    .join("-");
-  await tee(
-    `export CI_ENVIRONMENT_NAME=dev-${prunedBranch}; envsubst < ${FILEPATH_DEPLOY_BACKEND} | kubectl delete -f -`
-  );
-  await tee(
-    `export CI_ENVIRONMENT_NAME=dev-${prunedBranch}; envsubst < ${FILEPATH_DEPLOY_FRONTEND} | kubectl delete -f -`
-  );
-  await tee(
-    `export CI_ENVIRONMENT_NAME=${prunedBranch}; envsubst < ${FILEPATH_DEPLOY_INGRESS} | kubectl delete -f -`
-  );
+  const branchesToPrune = backendDeployments
+    .slice(
+      0,
+      backendDeployments.length - TRICYCLE_MAX_NUMBER_OF_LIVE_DEV_BRANCHES
+    ) // ^ Integer arithmetic with integer-like strings (env var) in JS is fine
+    .map((line) => line.split("/")[1].split("-").slice(3).join("-"));
+  for (let branch of branchesToPrune) {
+    await tee(
+      `export CI_ENVIRONMENT_NAME=dev-${branch}; envsubst < ${FILEPATH_DEPLOY_BACKEND} | kubectl delete -f -`
+    );
+    await tee(
+      `export CI_ENVIRONMENT_NAME=dev-${branch}; envsubst < ${FILEPATH_DEPLOY_FRONTEND} | kubectl delete -f -`
+    );
+    await tee(
+      `export CI_ENVIRONMENT_NAME=${branch}; envsubst < ${FILEPATH_DEPLOY_INGRESS} | kubectl delete -f -`
+    );
+  }
 }
