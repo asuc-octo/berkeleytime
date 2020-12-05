ded78e4f11fca110b70f1dacbac3d37b2b0eba44
Grace Luo <graceluo@berkeley.edu>
2020-12-05 00:36:17 -0800

    Add login modal

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -28,7 +28,6 @@ const routes: Array<RouteProps> = [
   { path: '/enrollment',        component: Enrollment,     exact: false },
   { path: '/about',             component: About           },
   { path: '/dashboard',         component: Dashboard       },
-  // { path: '/login',             component: Login           },
   { path: '/releases',          component: Releases        },
   { path: '/faq',               component: Faq             },
   { path: '/apidocs',           component: ApiDocs         },
