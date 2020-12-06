5440d8a87e0df7c799864fcbc897bd7f086bb995
Grace Luo <graceluo@berkeley.edu>
2020-12-05 20:35:12 -0800

    More styling fixes to login modal

--- frontend/src/components/Login/LoginModal.jsx
+++ frontend/src/components/Login/LoginModal.jsx
@@ -3,16 +3,14 @@ import { Modal } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import LoginButton from './LoginButton';
 import { H3, P } from 'bt/custom';
+import close from 'assets/svg/common/close.svg';
 
 export class LoginModal extends Component {
   render() {
     return (
       <Modal show={this.props.showLogin} onHide={this.props.hideLogin} className="login">
         <button className="link-btn back-btn" onClick={this.props.hideLogin}>
-          <svg width="25" height="25" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16" cy="16" r="16" fill="#C4C4C4"/>
-            <path d="M24 15H11.83L17.42 9.41L16 8L8 16L16 24L17.41 22.59L11.83 17H24V15Z" fill="white"/>
-          </svg>
+          <img src={close} />
         </button>
         <H3 bold className="mb-2">Welcome to Berkeleytime</H3>
         <P className="login-text">
