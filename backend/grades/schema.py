14cac2f7a52775b52ae258728d8042f272d19af9
Leon Ming <leon.ming@berkeley.edu>
2020-12-06 15:38:24 -0800

    minor changes

--- backend/grades/schema.py
+++ backend/grades/schema.py
@@ -6,10 +6,10 @@ from graphene_django.filter import DjangoFilterConnectionField
 from grades.models import Grade
 from grades.utils import add_up_grades, gpa_to_letter_grade
 
-STANDARD_GRADES = [("a1", "A+"), ("a2", "A"), ("a3", "A-"),
-                   ("b1", "B+"), ("b2", "B"), ("b3", "B-"),
-                   ("c1", "C+"), ("c2", "C"), ("c3", "C-"),
-                   ("d", "D"), ("f", "F"), ("p", "P"), ("np", "NP")]
+STANDARD_GRADES = [('a1', 'A+'), ('a2', 'A'), ('a3', 'A-'),
+                   ('b1', 'B+'), ('b2', 'B'), ('b3', 'B-'),
+                   ('c1', 'C+'), ('c2', 'C'), ('c3', 'C-'),
+                   ('d', 'D'), ('f', 'F'), ('p', 'P'), ('np', 'NP')]
 
 ALL_GRADES = (
     'a1', 'a2', 'a3',
@@ -50,9 +50,9 @@ class GradeType(DjangoObjectType):
         percentile_ceiling = 0
         for grade, display in STANDARD_GRADES:
             if grade == 'd':
-                numerator = sum([getattr(self, d) for d in ('d1', 'd2', 'd3')]) or 0.0
+                numerator = sum([getattr(self, d, 0.0) for d in ('d1', 'd2', 'd3')])
             else:
-                numerator = getattr(self, grade) or 0.0
+                numerator = getattr(self, grade, 0.0)
             percent = round(numerator / self.graded_total if self.graded_total else 0.0, 2)
             if grade == 'p' or grade == 'np':
                 percentile_high = 0
@@ -78,7 +78,6 @@ class GradeType(DjangoObjectType):
             return round(float(sum(counter.values())) / total, 3)
 
     def resolve_section_letter(self, info):
-        # horribly inefficient but how to not calculate gpa twice?
         counter, total = add_up_grades([self])
         if total == 0:
             return 'N/A'
@@ -86,13 +85,10 @@ class GradeType(DjangoObjectType):
             return gpa_to_letter_grade(round(float(sum(counter.values())) / total, 3))
 
     def resolve_denominator(self, info):
-        denom = 0
-        for grade in ALL_GRADES:
-            denom += getattr(self, grade) or 0.0
-        return denom
+        return sum(getattr(self, grade, 0.0) for grade in ALL_GRADES)
 
 
 
 class Query(graphene.ObjectType):
-    all_grades = DjangoFilterConnectionField(GradeType, filterset_class=GradeFilter)
+    all_grades = DjangoFilterConnectionField(GradeType)
     grade = graphene.Node.Field(GradeType)
