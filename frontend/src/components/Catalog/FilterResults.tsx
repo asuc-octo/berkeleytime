2e1dfed9a89a643f5e844a150a06706b65a44a57
Vihan <vihan+github@vihan.org>
2020-12-06 19:17:25 -0800

    Show the BTLoader for catalog cols instantly
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- frontend/src/components/Catalog/FilterResults.tsx
+++ frontend/src/components/Catalog/FilterResults.tsx
@@ -69,7 +69,7 @@ const FilterResults = ({
           <div>A critical error occured.</div>
         </div>
       ) : loading ? (
-        <BTLoader />
+        <BTLoader showInstantly />
       ) : sortedCourses.length === 0 && !showEmptyState ? (
         <div className="filter-results-loading">
           <div className="filter-results-empty">
