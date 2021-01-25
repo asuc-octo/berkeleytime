d56676b2823442706e87644d49a50143fa8391cc
Grace Luo <graceluo@berkeley.edu>
2021-01-24 19:31:45 -0800

    Change login button back to Nav.Link

--- frontend/src/components/Login/LoginButton.tsx
+++ frontend/src/components/Login/LoginButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import btn_google_signin from 'assets/svg/profile/btn_google_signin.svg';
-import { Button } from 'bt/custom';
 import { ReactComponent as GoogleIcon } from '../../assets/svg/profile/google.svg';
 
 type Props = {
@@ -12,9 +11,13 @@ const LoginButton = ({ hideLogin }: Props) => {
 
   // TODO: potentially add loading state for this button?
   return (
-    <Button className="login-btn bt-bold" href={{ as_link: '/api/login/' }}>
+    <Nav.Link
+      className="login-btn bt-bold bt-btn-primary btn-bt-md"
+      eventKey={6}
+      onClick={() => { window.location.href = "/api/login/"; }}
+    >
       <GoogleIcon className="login-img" /> Sign in with Google
-    </Button>
+    </Nav.Link>
   );
 };
 
