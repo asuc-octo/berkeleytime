2ad7441810321e56d8953d2297cb13f5ae5de567
Leon Ming <leon.ming@berkeley.edu>
2021-01-27 18:03:04 -0800

    emails working

--- backend/berkeleytime/management/commands/email.py
+++ backend/berkeleytime/management/commands/email.py
@@ -1,7 +1,7 @@
-"""Email jobs based on preferences 
+"""Email jobs based on preferences
 
-Send emails based on occasions from database. 
-BerkeleytimeUser model has boolean fields to determine if emails are desired. 
+Send emails based on occasions from database.
+BerkeleytimeUser model has boolean fields to determine if emails are desired.
 """
 from django.core.management.base import BaseCommand
 from django.core.mail.message import EmailMultiAlternatives
@@ -44,20 +44,17 @@ class Command(BaseCommand):
             for subject, message, html_message, recipient in datatuple
         ]
         return connection.send_messages(messages)
-    
+
     def handle(self, *args, **options):
         if 'subject' not in options or 'template' not in options or 'occasion' not in options:
             print('Please enter all required parameters.')
             return
         subject, template, occasion = options['subject'], options['template'], options['occasion']
 
-        # dir = os.path.dirname(os.path.dirname(__file__))
-        # temp_path = os.path.join(dir, template)
-        bt_users = BerkeleytimeUser.objects.filter(**{ occasion: True }).select_related('auth_user__email')
+        bt_users = BerkeleytimeUser.objects.filter(**{ occasion: True }).select_related('user')
         html_message = render_to_string(template)
         plain_message = strip_tags(html_message)
-        
+
         email_datatuple = [(subject, plain_message, html_message, bt_user.user.email) for bt_user in bt_users]
-        # email_datatuple = [(subject, plain_message, html_message, bt_user) for bt_user in ['jonathanpan@berkeley.edu', 'leon.ming@berkeley.edu']]
-        
+
         self.send_mass_html_mail(email_datatuple)
