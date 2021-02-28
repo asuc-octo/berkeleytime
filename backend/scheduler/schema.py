2b67196664ef6e62da13bc6cf3108476e792d099
Shuming Xu <smxu@berkeley.edu>
2021-02-28 02:28:44 -0800

    Improve scheduler schema to use SectionSelection

--- backend/scheduler/schema.py
+++ backend/scheduler/schema.py
@@ -9,7 +9,7 @@ from berkeleytime.settings import CURRENT_SEMESTER, CURRENT_YEAR
 
 # Django models
 from django.contrib.auth.models import User
-from scheduler.models import Schedule, TimeBlock
+from scheduler.models import Schedule, TimeBlock, SectionSelection
 from catalog.models import Course, Section
 
 class TimeBlockType(DjangoObjectType):
@@ -17,50 +17,27 @@ class TimeBlockType(DjangoObjectType):
         model = TimeBlock
         interfaces = (graphene.Node, )
 
+class SectionSelectionType(DjangoObjectType):
+    class Meta:
+        model = SectionSelection
+        interfaces = (graphene.Node, )
+
 class ScheduleType(DjangoObjectType):
     class Meta:
         model = Schedule
         interfaces = (graphene.Node, )
 
-class CreateSchedule(graphene.Mutation):
-    class Arguments:
-        name = graphene.String(required=False)
-        year = graphene.String(required=False)
-        semester = graphene.String(required=False)
-        timezone = graphene.String()
-        classes = graphene.List(graphene.ID)
+class SectionSelectionInput(graphene.InputObjectType):
+    course = graphene.ID(required=True)
+    primary = graphene.ID(required=False)
+    secondary = graphene.List(graphene.ID, required=False)
 
-    # output
-    schedule = graphene.Field(ScheduleType)
+class TimeBlockInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    start_time = graphene.Time(required=True)
+    end_time = graphene.Time(required=True)
+    days = graphene.String(required=True)
 
-    # @login_required
-    def mutate(self, info, timezone, classes, name=None, year=CURRENT_YEAR, semester=CURRENT_SEMESTER):
-        user = User.objects.get(email='smxu@berkeley.edu').berkeleytimeuser
-        # user = info.context.user.berkeleytimeuser
-        
-        # fill in with default values
-        if not name:
-            # default name = "Schedule #"
-            count = user.schedules.all().count()
-            name = "Schedule " + str(count + 1)  # 1-index
-
-        schedule = Schedule.objects.create(
-            user = user,
-            timezone = timezone,
-            name = name,
-            year = year,
-            semester = semester
-        )
-
-        try:
-            for class_id in classes:
-                course = Course.objects.get(pk=from_global_id(class_id)[1])
-                schedule.classes.add(course)
-        except Course.DoesNotExist:
-            return GraphQLError('Invalid Class ID')
-
-        schedule.save()
-        return CreateSchedule(schedule=schedule)
 
 def forceInt(value):
     """
@@ -72,23 +49,136 @@ def forceInt(value):
     except ValueError:
         return int(float(value))
 
+
+def set_selected_sections(schedule, selected_sections):
+    """
+    Update schedule.selected_sections with selected_sections. Selections not in
+    selected_sections will be removed. Returns the total unit count.
+
+    Args:
+        schedule: scheduler.Schedule object
+        selected_sections: List of SectionSelectionInput from mutation
+
+    Returns:
+        Total unit count (Integer)
+    """
+    old_selections = set(schedule.selected_sections.all())
+    units = 0
+    for selection_input in selected_sections:
+        # get course
+        course = Course.objects.get(pk=from_global_id(selection_input.course)[1])
+        # only one selection for each course-schedule pair
+        selection, _ = schedule.selected_sections.get_or_create(
+            schedule = schedule,
+            course = course
+        )
+        units += forceInt(course.units)
+
+        # get primary
+        if selection_input.primary:
+            primary = Section.objects.get(pk=from_global_id(selection_input.primary)[1])
+            selection.primary = primary
+        else:
+            # clear primary selection
+            selection.primary = None
+
+        # update list of secondary sections
+        if selection_input.secondary:
+            # collect secondary sections
+            secondary_sections = []
+            for secondary_id in selection_input.secondary:
+                secondary = Section.objects.get(pk=from_global_id(secondary_id)[1])
+                secondary_sections.append(secondary)
+            selection.secondary.set(secondary_sections)
+
+        selection.save()
+        old_selections.remove(selection)
+
+    # remove not added selections
+    for selection in old_selections:
+        selection.delete()
+
+    return units
+
+
+class CreateSchedule(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=False)
+        year = graphene.String(required=False)
+        semester = graphene.String(required=False)
+        selected_sections = graphene.List(SectionSelectionInput, required=False)
+        timeblocks = graphene.List(TimeBlockInput, required=False)
+
+    # output
+    schedule = graphene.Field(ScheduleType)
+
+    @login_required
+    def mutate(self, info, name=None, year=CURRENT_YEAR, semester=CURRENT_SEMESTER,
+    selected_sections=None, timeblocks=None):
+        user = info.context.user.berkeleytimeuser
+
+        # fill in with default values
+        if not name:
+            # default name = "Schedule #"
+            count = user.schedules.all().count()
+            name = "Schedule " + str(count + 1)  # 1-index
+
+        schedule = Schedule.objects.create(
+            user = user,
+            name = name,
+            year = year,
+            semester = semester
+        )
+
+        # update sections
+        if selected_sections is not None:
+            # use is not None to allow empty lists for clearing selections
+            try:
+                # generate section selections
+                units = set_selected_sections(schedule, selected_sections)
+                # update units
+                schedule.total_units = units
+            except Course.DoesNotExist:
+                return GraphQLError('Invalid Course ID')
+            except Section.DoesNotExist:
+                return GraphQLError('Invalid Section ID')
+        
+        # create timeblocks
+        if timeblocks:
+            for timeblock_input in timeblocks:
+                # unique schedule-name pair
+                timeblock = schedule.timeblocks.create(
+                    schedule = schedule,
+                    name = timeblock_input.name,
+                    start_time = timeblock_input.start_time,
+                    end_time = timeblock_input.end_time,
+                    days = timeblock_input.days,
+                )
+                timeblock.save()
+
+        # save objects
+        schedule.save()
+
+        return CreateSchedule(schedule=schedule)
+
+
 class UpdateSchedule(graphene.Mutation):
     class Arguments:
         schedule_id = graphene.ID()
         # year and semester can't be edited
         name = graphene.String(required=False)
-        classes = graphene.List(graphene.ID, required=False)
-        selected_sections = graphene.List(graphene.ID, required=False)
+        selected_sections = graphene.List(SectionSelectionInput, required=False)
+        timeblocks = graphene.List(TimeBlockInput, required=False)
 
     schedule = graphene.Field(ScheduleType)
 
-    # @login_required
-    def mutate(self, info, schedule_id, name, classes, selected_sections):
+    @login_required
+    def mutate(self, info, schedule_id, name=None, selected_sections=None, timeblocks=None):
         schedule = None
         try:
             schedule = Schedule.objects.get(pk=from_global_id(schedule_id)[1])
-        except TimeBlock.DoesNotExist:
-            return GraphQLError('Invalid Time Block ID')
+        except Schedule.DoesNotExist:
+            return GraphQLError('Invalid Schedule ID')
         
         # ensure that schedule belongs to the current user
         if info.context.user.berkeleytimeuser != schedule.user:
@@ -97,104 +187,52 @@ class UpdateSchedule(graphene.Mutation):
         # update name
         if name:
             schedule.name = name
-
-        # update classes
-        if classes:
-            unit = 0
-            try:
-                for class_id in classes:
-                    course = Course.objects.get(pk=from_global_id(class_id)[1])
-                    unit += forceInt(course.units)
-                    schedule.classes.add(course)
-            except Course.DoesNotExist:
-                return GraphQLError('Invalid Class ID')
-            schedule.total_units = unit
         
         # update sections
-        if selected_sections:
+        if selected_sections is not None:
+            # use is not None to allow empty lists for clearing selections
             try:
-                for section_id in selected_sections:
-                    section = Section.objects.get(pk=from_global_id(section_id)[1])
-                    schedule.selected_sections.add(section)
+                # generate section selections
+                units = set_selected_sections(schedule, selected_sections)
+                # update units
+                schedule.total_units = units
             except Course.DoesNotExist:
+                return GraphQLError('Invalid Course ID')
+            except Section.DoesNotExist:
                 return GraphQLError('Invalid Section ID')
 
+        # update timeblocks
+        if timeblocks is not None:
+            saved_timeblocks = set()
+            for timeblock_input in timeblocks:
+                # unique schedule-name pair
+                timeblock, new_timeblock = schedule.timeblocks.get_or_create(
+                    schedule = schedule,
+                    name = timeblock_input.name,
+                    defaults = {
+                        'start_time': timeblock_input.start_time,
+                        'end_time': timeblock_input.end_time,
+                        'days': timeblock_input.days,
+                    }
+                )
+                
+                # manually override values if old 
+                if not new_timeblock:
+                    timeblock.start_time = timeblock_input.start_time
+                    timeblock.end_time = timeblock_input.end_time
+                    timeblock.days = timeblock_input.days
+                
+                timeblock.save()
+                saved_timeblocks.add(timeblock)
+            
+            # remove not added timeblocks
+            removed_timeblocks = set(schedule.timeblocks.all()) - saved_timeblocks
+            for timeblock in removed_timeblocks:
+                timeblock.delete()
+
         schedule.save()
-        return CreateTimeBlock(schedule = schedule)
+        return UpdateSchedule(schedule = schedule)
 
-class CreateTimeBlock(graphene.Mutation):
-    class Arguments:
-        schedule_id = graphene.ID()
-        name = graphene.String()
-        start_time = graphene.Time()
-        end_time = graphene.Time()
-        days = graphene.String()
-
-    time_block = graphene.Field(TimeBlockType)
-    schedule = graphene.Field(ScheduleType)
-
-    # @login_required
-    def mutate(self, info, schedule_id, name, start_time, end_time, days):
-        schedule = None
-        try:
-            schedule = Schedule.objects.get(pk=from_global_id(schedule_id)[1])
-        except Schedule.DoesNotExist:
-            return GraphQLError('Invalid Schedule ID')
-        
-        # ensure that the schedule belongs to the current user
-        # if info.context.user.berkeleytimeuser != schedule.user:
-        user = User.objects.get(email='smxu@berkeley.edu').berkeleytimeuser
-        if user != schedule.user:
-            return GraphQLError('No permission')
-
-        timeblock = TimeBlock.objects.create(
-            schedule = schedule,
-            name = name,
-            start_time = start_time,
-            end_time = end_time,
-            days = days
-        )
-        timeblock.save()
-
-        return CreateTimeBlock(time_block = timeblock, schedule = schedule)
-
-class UpdateTimeBlock(graphene.Mutation):
-    class Arguments:
-        time_block_id = graphene.ID()
-        name = graphene.String(required=False)
-        start_time = graphene.Time(required=False)
-        end_time = graphene.Time(required=False)
-        days = graphene.String(required=False)
-
-    time_block = graphene.Field(TimeBlockType)
-
-    # avaliable fields
-    _time_block_fields = (
-        'name',
-        'start_time',
-        'end_time',
-        'days'
-    )
-
-    # @login_required
-    def mutate(self, info, time_block_id, **kwargs):
-        timeblock = None
-        try:
-            timeblock = TimeBlock.objects.get(pk=from_global_id(time_block_id)[1])
-        except TimeBlock.DoesNotExist:
-            return GraphQLError('Invalid Time Block ID')
-        
-        # ensure that time blog belongs to the current user
-        if info.context.user.berkeleytimeuser != timeblock.schedule.user:
-            return GraphQLError('No permission')
-
-        # update attributes
-        for key in kwargs:
-            if key in UpdateTimeBlock._time_block_fields:
-                setattr(timeblock, key, kwargs[key])
-        timeblock.save()
-
-        return CreateTimeBlock(time_block = timeblock)
 
 class Query(graphene.ObjectType):
     schedules = graphene.List(ScheduleType)
@@ -209,5 +247,3 @@ class Query(graphene.ObjectType):
 class Mutation(graphene.ObjectType):
     create_schedule = CreateSchedule.Field()
     update_schedule = UpdateSchedule.Field()
-    create_time_block = CreateTimeBlock.Field()
-    update_time_block = UpdateTimeBlock.Field()
