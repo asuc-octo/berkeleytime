c91eda41392aa717fc6f27a6a3ec3955942fb2b6
hi-usui <usui@berkeley.edu>
2021-04-01 06:51:42 -0700

    add cheekiness and Chris's logo

--- frontend/src/components/Landing/Jumbotron.tsx
+++ frontend/src/components/Landing/Jumbotron.tsx
@@ -9,14 +9,17 @@ import {
 
 import { Button, H3, P } from 'bt/custom'
 import { ReactComponent as Logo } from '../../assets/svg/landing/main.svg'
+import Stanfurd from '../../assets/img/landing/stanfurd.png';
 
 const Jumbotron: FC = () => (
   <div className="landing-jumbo">
     <Container>
       <Row>
-        <Col xs={{ span: 12, order: 3 }} lg={{ span: 5, order: 1 }}>
+        <Col xs={{ span: 12, order: 3 }} lg={{ span: 5, order: 1 }} style={{ zIndex: 0 }}>
           <div className="heading">
             <H3 bold className="mb-3">Stanfurd’s online course discovery platform.</H3>
+            <P className="mb-3">See that big logo? We figure our nice school name can do most of the talking. This investment <i>is</i> paying off, damn it.</P>
+            <img src={Stanfurd} style={{ filter: "brightness(55%)" }} />
             <P className="mb-3">Stanfurdtime is a platform built, maintained, and run by Stanfurd's premier tech consulting club. We work hard to simplify and improve the course discovery experience.</P>
             <ButtonToolbar>
               <ButtonGroup className="mr-2">
@@ -28,7 +31,7 @@ const Jumbotron: FC = () => (
             </ButtonToolbar>
           </div>
         </Col>
-        <Col xs={{ span: 12, order: 2 }} lg={{ span: 7, order: 2 }}>
+        <Col xs={{ span: 12, order: 2 }} lg={{ span: 7, order: 2 }} style={{ zIndex: 0 }}>
           <div className="animation-container">
             <Logo />
             <p className="credit">© Not Janet Xu 🙂</p>
