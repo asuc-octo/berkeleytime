cce6ca05e418228cbb10cf70eadfebd95bb03252
Grace Luo <graceluo@berkeley.edu>
2021-04-04 18:48:49 -0700

    Add ASUC elections links

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -27,6 +27,7 @@ const Logout = lazy(() => import('./views/Profile/Logout'));
 const PrivacyPolicy = lazy(() => import('./views/Policies/PrivacyPolicy'));
 const TermsOfService = lazy(() => import('./views/Policies/TermsOfService'));
 const UserTestingForm = lazy(() => import('./views/Forms/UserTestingForm'));
+const STFGuide = lazy(() => import('./views/STFGuide'));
 
 const routes: Array<RouteProps> = [
   { path: '/landing', component: Landing },
@@ -53,6 +54,7 @@ const routes: Array<RouteProps> = [
   { path: '/error', component: Error },
   { path: '/legal/privacy', component: PrivacyPolicy },
   { path: '/legal/terms', component: TermsOfService },
+  { path: '/stf-guide', component: STFGuide },
 ];
 
 const Routes: React.FC = () => (
