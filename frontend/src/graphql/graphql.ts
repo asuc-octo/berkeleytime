55b19b455c6c7e006a964a63dd01f901d75faa07
Vihan <vihan+github@vihan.org>
2021-04-04 17:27:16 -0700

    Uncomment stuff
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- frontend/src/graphql/graphql.ts
+++ frontend/src/graphql/graphql.ts
@@ -29,7 +29,7 @@ export interface Scalars {
   GenericScalar: any;
   /**
    * Allows use of a JSON String for input / output from the GraphQL schema.
-   * 
+   *
    * Use of this type is *not recommended* as you lose the benefits of having a defined, static
    * schema (one of the key benefits of GraphQL).
    */
