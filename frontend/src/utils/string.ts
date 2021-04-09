9964756ea1e1b49245098a3f55c95d34d05bd353
Vihan <vihan+github@vihan.org>
2021-04-09 01:41:35 -0700

    Basic card hover previews
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- frontend/src/utils/string.ts
+++ frontend/src/utils/string.ts
@@ -7,7 +7,7 @@ export function hash(str: string): number {
     hval = 0x811c9dc5;
 
   for (i = 0, l = str.length; i < l; i++) {
-    hval ^= (str.charCodeAt(i) * 57) % 256;
+    hval ^= str.charCodeAt(i);
     hval +=
       (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
   }
