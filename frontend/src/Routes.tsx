f1e0bd44f033e789532e1c372826f7eb4810ac84
Grace Luo <graceluo@berkeley.edu>
2021-04-12 10:49:27 -0700

    Create universal RedirectLink object

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -27,6 +27,7 @@ const Logout = lazy(() => import('./views/Profile/Logout'));
 const PrivacyPolicy = lazy(() => import('./views/Policies/PrivacyPolicy'));
 const TermsOfService = lazy(() => import('./views/Policies/TermsOfService'));
 const UserTestingForm = lazy(() => import('./views/Forms/UserTestingForm'));
+const RedirectLink = lazy(() => import('./views/RedirectLink'));
 
 const routes: Array<RouteProps> = [
   { path: '/landing', component: Landing },
@@ -53,6 +54,7 @@ const routes: Array<RouteProps> = [
   { path: '/error', component: Error },
   { path: '/legal/privacy', component: PrivacyPolicy },
   { path: '/legal/terms', component: TermsOfService },
+  { path: '/redirect', component: RedirectLink, exact: false },
 ];
 
 const Routes: React.FC = () => (
