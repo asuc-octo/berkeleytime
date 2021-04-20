0b533f484927af91288ee1d9060f2d4f9eec714f
Vihan <vihan+github@vihan.org>
2021-04-19 22:59:51 -0700

    Add support for weekend classes
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- frontend/src/components/Scheduler/Calendar/CourseCard.tsx
+++ frontend/src/components/Scheduler/Calendar/CourseCard.tsx
@@ -1,9 +1,11 @@
 import TrashButton from 'components/Common/TrashButton';
 import { CourseOverviewFragment } from 'graphql/graphql';
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 import { courseToColor, courseToName } from 'utils/courses/course';
 import {
+  getColorForSection,
+  getCourseForSchedule,
   removeSection,
   Schedule,
   SchedulerSectionType,
@@ -11,17 +13,38 @@ import {
 import { formatLocation, formatSectionTime } from 'utils/sections/section';
 import cx from 'classnames';
 import CalendarCard from './CalendarCard';
-import Color from 'color';
 
 type Props = {
-  course: CourseOverviewFragment | null;
+  /**
+   * The course associated with the section. You
+   * don't need to pass this if a `schedule` is
+   * provided.
+   */
+  course?: CourseOverviewFragment | null;
   section: SchedulerSectionType;
-  schedule: Schedule;
+
+  /**
+   * If this course card represents a section from
+   * a schedule, pass the schedule here.
+   */
+  schedule?: Schedule;
+
+  /**
+   * Allows the user to modify the schedule from
+   * this card.
+   */
   setSchedule?: (newSchedule: Schedule) => void;
 
   /**
+   * CSS hex color of the card, otherwise defaults
+   * to a color determined from the course's title.
+   */
+  color?: string;
+
+  /**
    * Defaults to `false`. Overrides and disables noPopover.
-   * Also disables the dropdown
+   * Also disables the dropdown. Use this if the card is
+   * a temporary preview and not a permanent calendar item.
    */
   isPreview?: boolean;
 
@@ -32,10 +55,13 @@ type Props = {
 };
 
 const CourseCard = ({
-  course,
   section,
   schedule,
+  course = schedule ? getCourseForSchedule(schedule, section) : null,
   setSchedule,
+  color = schedule
+    ? getColorForSection(schedule, section)
+    : courseToColor(section.courseId),
   isPreview = false,
   noPopover = false,
 }: Props) => {
@@ -47,13 +73,11 @@ const CourseCard = ({
     .filter(Boolean)
     .join(', ');
 
-  const cardColor = courseToColor(section.courseId);
-
   const card = (
     <CalendarCard
       title={cardTitle}
       description={cardDescription}
-      color={cardColor}
+      color={color}
       className={cx('calendar-course-card', {
         'calendar-course-card--preview': isPreview,
       })}
@@ -70,10 +94,10 @@ const CourseCard = ({
         <div>
           <div
             className="course-card-popover-square"
-            style={{ background: cardColor }}
+            style={{ background: color }}
           />
           <h4>{cardTitle}</h4>
-          {setSchedule && (
+          {schedule && setSchedule && (
             <TrashButton
               onClick={() => {
                 setSchedule(removeSection(schedule, section.id));
