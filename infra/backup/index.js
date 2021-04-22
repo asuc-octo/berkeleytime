8ddbcabd6da880c0ccbd95fbe7f623842f002c43
hi-usui <22272118+hi-usui@users.noreply.github.com>
2021-04-22 16:43:07 -0700

    infra sync (#456), other branches must merge for builds to pass

--- infra/backup/index.js
+++ infra/backup/index.js
@@ -1,13 +1,20 @@
 import moment from "moment";
 import { spawn } from "promisify-child-process";
 
+/**
+ * TO-DO: Convert this from a regular cron to k8s Cronjob, but slightly tricky 
+ * since it requires gcloud auth and rook-ceph-tools pod
+ */
+
+const NUMBER_OF_BACKUPS_TO_STORE = 30;
+
 const tee = async (...args) => {
   const child = spawn(...args, {
     env: process.env,
     encoding: "utf8",
     shell: true,
     stdio: "pipe",
-    signal: null,
+    // signal: null,
   });
   child.stdin.pipe(process.stdin);
   child.stdout.pipe(process.stdout);
@@ -31,6 +38,22 @@ const upload = async (obj) => {
   await tee(
     `kubectl -n rook-ceph exec deploy/rook-ceph-tools -- rbd export rook-cephrbd/csi-vol-${volumeHandle} - | gzip | gsutil cp - gs://berkeleytime-218606/${label}/${volumeSnapshotCompressed}`
   );
+  const backups = (
+    await tee(
+      `gsutil ls -l gs://berkeleytime-218606/${label} | sort --key 2 --reverse`
+    )
+  ).stdout
+    .trim()
+    .split("\n");
+  const deletes = [];
+  for (const [index, line] of backups.entries()) {
+    if (index > NUMBER_OF_BACKUPS_TO_STORE) {
+      deletes.push(line.split(/\s+/).slice(-1)[0]);
+    }
+  }
+  if (deletes.length) {
+    await tee(`gsutil rm ${deletes.join(" ")}`);
+  }
 };
 await upload({
   label: "bt-psql-prod",
