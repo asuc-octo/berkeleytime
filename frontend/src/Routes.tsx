6a5e91fea087f1b89a4e9d0202bd3f76d96f3d55
Grace Luo <graceluo@berkeley.edu>
2021-04-29 15:04:04 -0700

    Clean up routes and remove api docs

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,19 +4,18 @@ import BTLoader from 'components/Common/BTLoader';
 
 import Landing from './views/Landing';
 import Catalog from './views/Catalog/Catalog';
-// import Dashboard from './views/Dashboard/Dashboard';
+import Error from './views/Error/Error';
+// const Grades = lazy(() => new Promise(() => {}));
 // import DesignApp from './views/Forms/DesignApp';
 // import EngineeringApp from './views/Forms/EngineeringApp';
 // import { Positions } from './views/Apply/DescriptionPages';
-import Error from './views/Error/Error';
+// const ApiDocs = lazy(() => import('./views/Api/Api'));
 
-// const Grades = lazy(() => new Promise(() => {}));
 const Grades = lazy(() => import('./views/Grades/Grades'));
 const Enrollment = lazy(() => import('./views/Enrollment/Enrollment'));
 const About = lazy(() => import('./views/About'));
 const Releases = lazy(() => import('./views/Releases/Releases'));
 const Faq = lazy(() => import('./views/Faq'));
-const ApiDocs = lazy(() => import('./views/Api/Api'));
 const TestForm = lazy(() => import('./views/Forms/TestForm'));
 const BugsForm = lazy(() => import('./views/Forms/BugsForm'));
 const Apply = lazy(() => import('./views/Apply/Apply'));
@@ -35,10 +34,8 @@ const routes: Array<RouteProps> = [
   { path: '/grades', component: Grades, exact: false },
   { path: '/enrollment', component: Enrollment, exact: false },
   { path: '/about', component: About },
-  // { path: '/login',             component: Login           },
   { path: '/releases', component: Releases },
   { path: '/faq', component: Faq },
-  { path: '/apidocs', component: ApiDocs },
   { path: '/testform', component: TestForm },
   { path: '/bugs', component: BugsForm },
   { path: '/usertesting', component: UserTestingForm },
@@ -47,10 +44,6 @@ const routes: Array<RouteProps> = [
   { path: '/oauth2callback', component: Login },
   { path: '/logout', component: Logout },
   // { path: '/scheduler', component: Scheduler },
-  // { path: '/apply/positions',   component: Positions       },
-  // { path: '/apply/engineering', component: EngineeringApp  },
-  // { path: '/apply/design',      component: DesignApp       },
-  // { path: '/apply/embed',       component: Apply           },
   { path: '/error', component: Error },
   { path: '/legal/privacy', component: PrivacyPolicy },
   { path: '/legal/terms', component: TermsOfService },
