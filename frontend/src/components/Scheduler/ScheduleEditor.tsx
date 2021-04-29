eadc91306e508b565ef72104064afb38991db4b4
Vihan <vihan+github@vihan.org>
2021-04-29 15:59:14 -0700

    Scheduler refactor for less-spaghetti logic
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- frontend/src/components/Scheduler/ScheduleEditor.tsx
+++ frontend/src/components/Scheduler/ScheduleEditor.tsx
@@ -1,168 +1,76 @@
-import React, {
-  ChangeEvent,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
-import { Button, Col, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
+import React, { ChangeEvent, ReactNode, useState } from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
 import CourseSelector from 'components/Scheduler/CourseSelector';
 
-import {
-  CourseOverviewFragment,
-  useGetScheduleForIdLazyQuery,
-  useUpdateScheduleMutation,
-} from '../../graphql/graphql';
+import { useGetCoursesForFilterQuery } from '../../graphql/graphql';
 import BTLoader from 'components/Common/BTLoader';
 import {
-  DEFAULT_SCHEDULE,
-  deserializeSchedule,
   Schedule,
   SchedulerSectionType,
   scheduleToICal,
-  serializeSchedule,
 } from 'utils/scheduler/scheduler';
 import SchedulerCalendar from 'components/Scheduler/Calendar/SchedulerCalendar';
-import { Semester } from 'utils/playlists/semesters';
-import { debounce } from 'lodash';
-import Callout from './Callout';
-import { useUser } from 'graphql/hooks/user';
-import { useCreateSchedule } from 'graphql/hooks/schedule';
 import AccessControl from './AccessControl';
 import { AccessStatus } from 'utils/scheduler/accessStatus';
-
-// This is NOT an interval. Rather it combines all
-// changes within this time interval into one
-// update event. This means there will be at MOST
-// one event sent in each of the specfied interval.
-const SCHEDULER_AUTOSAVE_INTERVAL = 500;
+import { useSemester } from 'graphql/hooks/semester';
+import { getNodes } from 'utils/graphql';
+import { Semester } from 'utils/playlists/semesters';
 
 type Props = {
-  allCourses: CourseOverviewFragment[];
-  semester: Semester;
-  scheduleId: string | null;
-  setScheduleId: (newScheduleId: string) => void;
+  /**
+   * The semester being edited. If not provided, will
+   * assume the latest semester.
+   */
+  semester?: Semester;
+
+  /**
+   * The users schedule.
+   */
+  schedule: Schedule;
+
+  /**
+   * Called when the schedule should be updated.
+   */
+  setSchedule: (newSchedule: Schedule) => void;
+
+  /**
+   * This is the widget to show next to the title.
+   * It typically represents the schedule's save state.
+   */
+  saveWidget?: ReactNode;
+
+  /**
+   * If provided, shows access control with the
+   * specified ID.
+   */
+  accessControl?: string;
 };
 
 const ScheduleEditor = ({
-  allCourses,
+  schedule,
   semester,
-  scheduleId,
-  setScheduleId,
+  setSchedule,
+  saveWidget,
+  accessControl,
 }: Props) => {
-  const [
-    getScheduleForId,
-    { loading: isFetchingRemoteSchedule },
-  ] = useGetScheduleForIdLazyQuery({
-    onError: (error) => {
-      alert(`Couldn't load schedule: ${error.message}`);
-    },
-    onCompleted: (data) => {
-      if (!data.schedule) {
-        alert(`Couldn't find the given schedule.`);
-        return;
-      }
+  const { semester: latestSemester, error: semesterError } = useSemester(
+    semester
+  );
 
-      const schedule = deserializeSchedule(data.schedule);
-      setRawSchedule(schedule);
+  // Only load the list of filters once we have the latest semester. If we
+  // didn't wait, we'd load all semesters' classes which is way to many.
+  const { data, error: coursesError } = useGetCoursesForFilterQuery({
+    variables: {
+      playlists: latestSemester?.playlistId!,
     },
+    skip: !latestSemester,
   });
-
-  useEffect(() => {
-    // Clear the update queue
-    currentlyPendingUpdate.current = null;
-
-    if (scheduleId !== null) {
-      getScheduleForId({
-        variables: {
-          id: scheduleId,
-        },
-      });
-    } else {
-      setRawSchedule(DEFAULT_SCHEDULE);
-    }
-  }, [scheduleId, getScheduleForId]);
-
-  const [schedule, setRawSchedule] = useState<Schedule>(DEFAULT_SCHEDULE);
-  const isRemoteSaved = scheduleId !== null;
 
   // If the user is hovering over a section. This will store that section
   const [
     previewSection,
     setPreviewSection,
   ] = useState<SchedulerSectionType | null>(null);
-
-  // Whether or not we say 'is saving...' may be different from whether
-  // or not there is an ongoing network requests. The main constraint
-  // is we do NOT want to say the schedule is saved if the schedule as
-  // currently shown is not what's on the server.
-  const [isVisualSaving, setIsVisualSaving] = useState(false);
-  const [
-    createScheduleMutation,
-    { error: creationError },
-  ] = useCreateSchedule();
-
-  const [
-    updateScheduleMutation,
-    { error: saveError },
-  ] = useUpdateScheduleMutation();
-
-  // Stores the currently pending autosave mutation query.
-  const currentlyPendingUpdate = useRef<Promise<any> | null>(null);
-
-  // Call this to save a schedule. This gets renewed when the
-  // scheduleId changes (to clear the save queue).
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const saveSchedule = useCallback(
-    debounce(async (schedule: Schedule, semester: Semester, id: string) => {
-      setIsVisualSaving(true);
-
-      const result = updateScheduleMutation({
-        variables: {
-          scheduleId: id,
-          ...serializeSchedule(schedule, semester),
-        },
-      });
-
-      currentlyPendingUpdate.current = result;
-      try {
-        await currentlyPendingUpdate.current;
-      } finally {
-        // If there was a autosave error, the hook
-        // handles that so we don't need to worry.
-        setIsVisualSaving(false);
-      }
-    }, SCHEDULER_AUTOSAVE_INTERVAL),
-    [scheduleId]
-  );
-
-  async function setSchedule(newSchedule: Schedule) {
-    setRawSchedule(newSchedule);
-
-    if (scheduleId) {
-      // Wait for previous update to finish before queuing next one
-      // In effect, this will result in sequential updates being
-      // combined due to saveSchedule being 'debounced'. This is
-      // also done to avoid data races.
-      await currentlyPendingUpdate.current;
-      setIsVisualSaving(true);
-      saveSchedule(newSchedule, semester, scheduleId);
-    }
-  }
-
-  async function createSchedule() {
-    setIsVisualSaving(true);
-
-    try {
-      const result = await createScheduleMutation(schedule, semester);
-
-      if (result.data?.createSchedule?.schedule) {
-        setScheduleId(result.data.createSchedule.schedule.id);
-      }
-    } finally {
-      setIsVisualSaving(false);
-    }
-  }
 
   const setScheduleName = (event: ChangeEvent<HTMLInputElement>) =>
     setSchedule({
@@ -176,8 +84,22 @@ const ScheduleEditor = ({
       access: newAccess,
     });
 
+  if (!data || !semester) {
+    return (
+      <div className="scheduler viewport-app">
+        <div className="scheduler__status">
+          {semesterError || coursesError ? (
+            'An error occured loading scheduler information. Please try again later.'
+          ) : (
+            <BTLoader />
+          )}
+        </div>
+      </div>
+    );
+  }
+
   function exportToCalendar() {
-    const icsData = scheduleToICal(schedule, semester);
+    const icsData = scheduleToICal(schedule, semester!);
     const icsURI =
       `data:text/calendar;charset=utf8,` + encodeURIComponent(icsData);
 
@@ -187,23 +109,7 @@ const ScheduleEditor = ({
     link.click();
   }
 
-  const { isLoggedIn, loading: loadingUser } = useUser();
-
-  if (isFetchingRemoteSchedule) {
-    return <BTLoader />;
-  }
-
-  const saveButton = (
-    <Button
-      className="bt-btn-primary px-3"
-      size="sm"
-      onClick={createSchedule}
-      disabled={!isLoggedIn}
-      style={{ pointerEvents: !isLoggedIn ? 'none' : undefined }}
-    >
-      Save
-    </Button>
-  );
+  const allCourses = getNodes(data.allCourses!);
 
   return (
     <Row noGutters>
@@ -226,45 +132,14 @@ const ScheduleEditor = ({
               placeholder="Schedule Name"
               className="scheduler-name-input mr-3"
             />
-            {isVisualSaving ? (
-              <span>Saving schedule...</span>
-            ) : creationError ? (
-              <Callout
-                type="warning"
-                state="error"
-                message="Could not save schedule."
-              />
-            ) : saveError ? (
-              <Callout
-                type="warning"
-                state="error"
-                message="Error autosaving."
-              />
-            ) : isRemoteSaved ? (
-              <span>Schedule saved.</span>
-            ) : isLoggedIn ? (
-              saveButton
-            ) : (
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id="schedule-save-popover">
-                    {loadingUser
-                      ? 'Loading account...'
-                      : 'You must be logged in to save.'}
-                  </Tooltip>
-                }
-                placement="bottom"
-              >
-                <span className="d-inline-block">{saveButton}</span>
-              </OverlayTrigger>
-            )}
+            {saveWidget}
           </div>
           <div>
-            {isRemoteSaved && (
+            {accessControl && (
               <AccessControl
                 visibility={schedule.access}
                 setVisibility={setScheduleVisibility}
-                scheduleId={scheduleId!}
+                scheduleId={accessControl}
               />
             )}
             <Button
--- frontend/src/graphql/hooks/graphql.ts
+++ frontend/src/graphql/hooks/graphql.ts
@@ -1,9 +1,12 @@
 import {
   FetchResult,
+  LazyQueryHookOptions,
+  LazyQueryResult,
   MutationFunction,
   MutationHookOptions,
   MutationResult,
   MutationTuple,
+  QueryTuple,
   useMutation,
 } from '@apollo/client';
 import { useCallback } from 'react';
@@ -35,4 +38,33 @@ export const wrapMutation = <
     [m]
   );
   return [mutationRunner, fetchResult];
+};
+
+/**
+ * Wraps mutation in a custom hook
+ */
+export const wrapLazyQuery = <
+  LazyQuery extends unknown,
+  LazyQueryVariables extends unknown,
+  HookParameters extends any[]
+>(
+  lazyQuery: (
+    options?: LazyQueryHookOptions<LazyQuery, LazyQueryVariables>
+  ) => QueryTuple<LazyQuery, LazyQueryVariables>,
+  callback: (
+    ...params: HookParameters
+  ) => LazyQueryHookOptions<LazyQuery, LazyQueryVariables>
+) => (
+  options?: LazyQueryHookOptions<LazyQuery, LazyQueryVariables>
+): [
+  (...params: HookParameters) => void,
+  LazyQueryResult<LazyQuery, LazyQueryVariables>
+] => {
+  const [m, fetchResult] = lazyQuery(options);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const queryRunner = useCallback(
+    (...params: HookParameters) => m(callback(...params)),
+    [m]
+  );
+  return [queryRunner, fetchResult];
 };
