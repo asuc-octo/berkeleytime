7259b47b9d311e6260797c975ac94319544edd79
Kevin Wang <kev@kevwang.dev>
2021-06-06 22:52:41 -0700

    Convert code to typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import { ApolloServer } from "apollo-server-express"
+import { Server as IoServer } from "socket.io"
+import { Server as HttpServer } from "http"
+import "express-async-errors"
+
+import "./env"
+
+const app = express()
+const apollo = new ApolloServer({})
+const http = new HttpServer(app)
+
+apollo.applyMiddleware({ app: app, path: "/graphql" })
+
+// Uncomment when you are ready to play with websockets.
+// const io = new IoServer(http)
+// const sockets = {}
+// io.on("connection", (socket) => {
+//     sockets[socket.id] = socket
+//     socket.on("disconnect", (reason) => {
+//         delete sockets[socket.id]
+//     })
+// })
+
+// Generic error handler for uncaught exceptions, from express-async-errors
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err)
+    return res.status(500).json({ error: err.stack })
+})
+
+http.listen(process.env.PORT_EXPRESS, () => {
+    console.log(`Server now listening on port ${process.env.PORT_EXPRESS}`)
+})
