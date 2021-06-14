ffe61a01f413e10f32734c51a6ab4d5264fb27e4
hi-usui <usui@berkeley.edu>
2021-06-13 23:40:51 -0700

    implement happy path for '/api/users/login'

---
+++ backend/src/services/passport.ts
@@ -0,0 +1,28 @@
+import passport from "passport"
+import { ExtractJwt, Strategy as JwtStrategy } from "passport-jwt"
+
+import { KEY_BERKELEYTIME } from "#src/config"
+import { User } from "#src/models/_index"
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      passReqToCallback: true,
+      secretOrKey: KEY_BERKELEYTIME,
+    },
+    async (req, jwtPayload, done) => {
+      const { id } = jwtPayload
+      if (id) {
+        const user = await User.findById(id)
+        if (user) {
+          done(null, user)
+        } else {
+          done(null, false)
+        }
+      } else {
+        done(null, false, { message: "authentication failed" })
+      }
+    }
+  )
+)
