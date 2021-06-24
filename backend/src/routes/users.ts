fae4186c0226650c0690914d8a4bfc950359ff26
hi-usui <usui@berkeley.edu>
2021-06-24 13:44:14 -0700

    add validation for client requests

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,12 +2,13 @@ import express from "express"
 import passport from "passport"
 
 import { Users } from "#src/controllers/users"
+import { AjvMiddleware, usersValidator } from "#src/validation/_index"
 
 const router = express.Router()
 const authenticate = passport.authenticate("jwt", { session: false })
 
-router.post("/login", Users.login)
-router.post("/register", Users.register)
+router.post("/login", AjvMiddleware(usersValidator.login), Users.login)
+router.post("/register", AjvMiddleware(usersValidator.register), Users.register)
 router.get("/exists", authenticate, Users.exists)
 router.get("/current", authenticate, Users.current)
 
---
+++ backend/src/validation/users.ts
@@ -0,0 +1,66 @@
+import Ajv from "ajv"
+import ajvErrors from "ajv-errors"
+import addFormats from "ajv-formats"
+
+// interface Login {
+//   email: string
+//   password: string
+// }
+
+// interface Register {
+//   email: string
+//   name: string
+//   password: string
+//   password_confirm: string
+// }
+
+const ajv = new Ajv({ allErrors: true, $data: true })
+addFormats(ajv)
+ajvErrors(ajv)
+
+// const SchemaLogin: JSONSchemaType<Login> = {
+const SchemaLogin = {
+  type: "object",
+  required: ["email", "password"],
+  properties: {
+    email: {
+      type: "string",
+      format: "email",
+    },
+    password: {
+      type: "string",
+    },
+  },
+}
+
+const SchemaRegister = {
+  type: "object",
+  required: ["email", "name", "password", "password_confirm"],
+  properties: {
+    email: {
+      type: "string",
+      format: "email",
+    },
+    name: {
+      errorMessage: "must provide a name",
+      type: "string",
+      minLength: 1,
+    },
+    password: {
+      minLength: 5,
+    },
+    password_confirm: {
+      type: "string",
+      errorMessage: "must be equal to constant",
+      const: {
+        $data:
+          "1/password" /* This field must have the exact same value as password */,
+      },
+    },
+  },
+}
+
+export const usersValidator = {
+  login: ajv.compile(SchemaLogin),
+  register: ajv.compile(SchemaRegister),
+}
