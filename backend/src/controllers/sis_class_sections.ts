1c9d1de94424ab9d458c89d66cead3ebed689f54
hi-usui <usui@berkeley.edu>
2022-02-22 21:48:49 -0800

    move businessLogic fn and try-catch block

--- backend/src/controllers/sis_class_sections.ts
+++ backend/src/controllers/sis_class_sections.ts
@@ -57,6 +57,65 @@ export const SIS_Class_Sections = new (class Controller {
     const shared = {
       sisClassSectionCount: 1,
     };
+    const businessLogic = async ({ courseId, termId }) => {
+      try {
+        const sisClassSections = await this.requestClassSectionData({
+          courseId,
+          termId,
+        });
+
+        for (const sisClassSection of sisClassSections) {
+          const original = await SIS_Class_Section.findOne({
+            "class.session.term.id": termId,
+            id: sisClassSection.id,
+          });
+
+          const foundClassSection = original?.toJSON();
+          omitter(foundClassSection);
+
+          if (_.isEqual(foundClassSection, sisClassSection)) {
+            if (!foundClassSection) {
+              // prettier-ignore
+              console.error(`WARNING! ONE OF THE IDENTIFIERS HAS A FATAL ERROR: ${JSON.stringify(sisClassSection)}`.red);
+            }
+            // prettier-ignore
+            console.info(`${ts()} SIS CLASS SECTION COUNT: ${shared.sisClassSectionCount}`.padEnd(55, " ") + `no changes: (${original?._id}) cs-course-id '${courseId}' '${foundClassSection['id']}' '${sisClassSection["displayName"]}'`.green);
+          } else {
+            const result = await SIS_Class_Section.findOneAndUpdate(
+              {
+                "class.session.term.id": termId,
+                id: sisClassSection.id,
+              },
+              sisClassSection,
+              {
+                lean: true,
+                new: true,
+                strict: false,
+                upsert: true,
+                rawResult: true,
+              }
+            );
+            console.info(
+              `${ts()} SIS CLASS SECTION COUNT: ${
+                shared.sisClassSectionCount
+              }`.padEnd(55, " ") +
+                `${
+                  result.lastErrorObject.updatedExisting
+                    ? //@ts-ignore
+                      //prettier-ignore
+                      `updated (${result.value._id}) cs-course-id '${courseId}' '${sisClassSection.displayName} ${JSON.stringify((await SIS_Class_Section.history.find({ collectionId: result.value._id }).sort({ updatedAt: "desc" }).limit(1))[0]) + "\n"}'`.yellow
+                    : `created (${result.lastErrorObject.upserted}) cs-course-id '${courseId}' '${result.value["displayName"]}'`
+                        .yellow
+                }`
+            );
+          }
+          shared.sisClassSectionCount++;
+        }
+      } catch (e) {
+        console.error(JSON.stringify(e));
+      }
+    };
+
     for await (const sisCourse of SIS_Course.find({
       "status.code": "ACTIVE",
       identifiers: { $ne: { type: "cs-course-id", id: "" } },
@@ -73,66 +132,8 @@ export const SIS_Class_Sections = new (class Controller {
         ),
       }).distinct("session.term.id");
       for (const termId of terms.reverse()) {
-        try {
-          const businessLogic = async () => {
-            const sisClassSections = await this.requestClassSectionData({
-              courseId,
-              termId,
-            });
-
-            for (const sisClassSection of sisClassSections) {
-              const original = await SIS_Class_Section.findOne({
-                "class.session.term.id": termId,
-                id: sisClassSection.id,
-              });
-
-              const foundClassSection = original?.toJSON();
-              omitter(foundClassSection);
-
-              if (_.isEqual(foundClassSection, sisClassSection)) {
-                if (!foundClassSection) {
-                  // prettier-ignore
-                  console.error(`WARNING! ONE OF THE IDENTIFIERS HAS A FATAL ERROR: ${JSON.stringify(sisClassSection)}`.red);
-                }
-                // prettier-ignore
-                console.info(`${ts()} SIS CLASS SECTION COUNT: ${shared.sisClassSectionCount}`.padEnd(55, " ") + `no changes: (${original?._id}) cs-course-id '${courseId}' '${foundClassSection['id']}' '${sisClassSection["displayName"]}'`.green);
-              } else {
-                const result = await SIS_Class_Section.findOneAndUpdate(
-                  {
-                    "class.session.term.id": termId,
-                    id: sisClassSection.id,
-                  },
-                  sisClassSection,
-                  {
-                    lean: true,
-                    new: true,
-                    strict: false,
-                    upsert: true,
-                    rawResult: true,
-                  }
-                );
-                console.info(
-                  `${ts()} SIS CLASS SECTION COUNT: ${
-                    shared.sisClassSectionCount
-                  }`.padEnd(55, " ") +
-                    `${
-                      result.lastErrorObject.updatedExisting
-                        ? //@ts-ignore
-                          //prettier-ignore
-                          `updated (${result.value._id}) cs-course-id '${courseId}' '${sisClassSection.displayName} ${JSON.stringify((await SIS_Class_Section.history.find({collectionId:result.value._id}).sort({updatedAt:"desc"}).limit(1))[0])+"\n"}'`.yellow
-                        : `created (${result.lastErrorObject.upserted}) cs-course-id '${courseId}' '${result.value["displayName"]}'`
-                            .yellow
-                    }`
-                );
-              }
-              shared.sisClassSectionCount++;
-            }
-          };
-          await queue.onSizeLessThan(1000);
-          queue.add(() => businessLogic());
-        } catch (e) {
-          console.error(JSON.stringify(e));
-        }
+        await queue.onSizeLessThan(1000);
+        queue.add(() => businessLogic({ courseId, termId }));
       }
     }
     await queue.onEmpty();
