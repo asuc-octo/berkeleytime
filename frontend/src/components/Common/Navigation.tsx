c2b4e4e76ad14d60762e15720ea43babe4260495
Zachary Zollman <zacharyzollman@berkeley.edu>
2022-05-03 16:00:13 -0700

    Revert "try adjusting button width"
    This reverts commit 9bf54c5a6975d7ce41bd9274a3387090475e3ad6.

--- frontend/src/components/Common/Navigation.tsx
+++ frontend/src/components/Common/Navigation.tsx
@@ -75,7 +75,7 @@ const Navigation: FC<Props> = (props) => {
           {isLoggedIn ? (
             <>
               <NavigationLink to="/profile">Profile</NavigationLink>
-              <Button style="width: fit-content;" href="/logout">Log out</Button>
+              <Button href="/logout">Log out</Button>
             </>
           ) : (
             <Button onClick={() => setShowLogin(true)}>
