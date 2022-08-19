f950e6d956ee0b19646fdd73183b51ccf4c9b988
Max Vogel <mehvix@protonmail.com>
2022-08-19 00:15:12 -0500

    upd start/end dates to match 2022 Fall semester (#501)
    http://guide.berkeley.edu/academic-calendar/

--- frontend/src/utils/scheduler/scheduler.ts
+++ frontend/src/utils/scheduler/scheduler.ts
@@ -208,8 +208,8 @@ export const serializeSchedule = (
  * Generates iCal file string.
  */
 export function scheduleToICal(schedule: Schedule, semester: Semester): string {
-  const SEMESTER_START = new Date(2022, 0, 18);
-  const LAST_COURSE_DAY = new Date(2022, 4, 6);
+  const SEMESTER_START = new Date(2022, 7, 24);
+  const LAST_COURSE_DAY = new Date(2022, 11, 2 );
 
   const dateToICal = (date: Date) =>
     date.toISOString().replace(/[-:Z]|\.\d+/g, '');
