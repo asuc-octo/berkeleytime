0f6d130329da366e2cf5d480d88ebaca809376e6
Gabe Mitnick <gabe.mitnick@gmail.com>
2022-09-23 19:06:09 -0700

    add back in redirect function in Banner

--- frontend/src/components/Common/Banner.tsx
+++ frontend/src/components/Common/Banner.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 // import { Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 import { Button } from 'bt/custom';
 import { ReduxState } from '../../redux/store'
 import { closeBanner } from '../../redux/common/actions'
@@ -10,6 +11,11 @@ import close from '../../assets/svg/common/close.svg'
 interface Props extends PropsFromRedux {}
 
 const Banner: FC<Props> = (props) => {
+  const history = useHistory();
+  function redirect(site: string) {
+    history.push("/redirect?site=" + site)
+  }
+
   return props.banner ? (
     <div className="banner">
       <div className="content">
