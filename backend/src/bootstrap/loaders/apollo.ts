1ddbf2bef0bd7a7738e4bcd5fe31b405f653c5c1
AlexXi19 <alex2001314jjj@gmail.com>
2022-10-21 19:07:13 -0700

    cleanup

--- backend/src/bootstrap/loaders/apollo.ts
+++ backend/src/bootstrap/loaders/apollo.ts
@@ -5,10 +5,7 @@ import { buildSchema } from "../../utils/buildSchema";
 
 export default async () => {
   const schema = buildSchema();
-  // console.log(typeDefs);
 
-  // const mergedTypeDefs = mergeTypeDefs(typeDefs);
-  // console.log(mergedTypeDefs);
   return new ApolloServer({
     schema,
     playground: config.isDev,
