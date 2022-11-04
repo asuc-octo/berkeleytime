6612c314e6b1acb7c27c55655ee8b5b9b80dcee9
Ethan Ikegami <ikeg225@berkeley.edu>
2022-11-04 13:31:25 -0700

    query by all semesters

--- backend/src/modules/grade/controller.ts
+++ backend/src/modules/grade/controller.ts
@@ -4,7 +4,14 @@ import { GradeModule } from "./generated-types/module-types";
 
 export async function grades(CourseControlNumber: number, Year: number, Semester: string): Promise<GradeModule.Grade> {
   // DATA C100: 26384 2021 "Fall"
-  const grades = await GradeModel.find({CourseControlNbr: CourseControlNumber, "term.year": Year, "term.semester": Semester});
+  let grades;
+  if (Year === -1 && Semester === "") {
+    grades = await GradeModel.find({CourseControlNbr: CourseControlNumber});
+  } else if (Year >= 1868 && ["Fall", "Spring", "Summer"].includes(Semester)) {
+    grades = await GradeModel.find({CourseControlNbr: CourseControlNumber, "term.year": Year, "term.semester": Semester});
+  } else {
+    throw new Error("Invalid query parameters");
+  }
 
   // total enrolled DOES NOT include students who withdrew/incomplete
   let totalEnrolled = 0;
@@ -13,97 +20,42 @@ export async function grades(CourseControlNumber: number, Year: number, Semester
   grades.map((grade)=> {
     if (grade.EnrollmentCnt && grade.GradeNm) {
       if (grade.GradeNm !== "Withdrawn" && grade.GradeNm !== "Incomplete") {
-        possible_grades[grade.GradeNm] = grade.EnrollmentCnt;
+        if (grade.GradeNm in possible_grades) {
+          possible_grades[grade.GradeNm] += grade.EnrollmentCnt;
+        } else {
+          possible_grades[grade.GradeNm] = grade.EnrollmentCnt;
+        }
         totalEnrolled += grade.EnrollmentCnt;
       }
     }
   })
 
+  const LetterGradeZero = {
+    percent: 0,
+    numerator: 0,
+    percentile_high: 0,
+    percentile_low: 0
+  }
+
   const gradesResponse : GradeModule.Grade = {
-    APlus: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    A: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    AMinus: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    BPlus: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    B: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    BMinus: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    CPlus: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    C: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    CMinus: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    D: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    F: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    P: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    },
-    NP: {
-      percent: 0,
-      numerator: 0,
-      percentile_high: 0,
-      percentile_low: 0
-    }
+    APlus: LetterGradeZero,
+    A: LetterGradeZero,
+    AMinus: LetterGradeZero,
+    BPlus: LetterGradeZero,
+    B: LetterGradeZero,
+    BMinus: LetterGradeZero,
+    CPlus: LetterGradeZero,
+    C: LetterGradeZero,
+    CMinus: LetterGradeZero,
+    D: LetterGradeZero,
+    F: LetterGradeZero,
+    P: LetterGradeZero,
+    NP: LetterGradeZero
   }
 
   let starting_percentile = 1;
   // map over response object and add percent, numerator, percentile_high, percentile_low
   Object.keys(gradesResponse).map((grade)=> {
-
     let newGrade = grade.toString().replace("Plus", "+").replace("Minus", "-")
     if (grade === "P") {
       newGrade = "Pass";
@@ -114,12 +66,13 @@ export async function grades(CourseControlNumber: number, Year: number, Semester
 
     if (newGrade in possible_grades) {
       const percentage = Math.round((possible_grades[newGrade] / totalEnrolled) * 100) / 100;
-      gradesResponse[grade as keyof GradeModule.Grade].percent = percentage;
-      gradesResponse[grade as keyof GradeModule.Grade].numerator = possible_grades[newGrade];
-      gradesResponse[grade as keyof GradeModule.Grade].percentile_high = starting_percentile;
-
       const percentile_low = Math.round((starting_percentile - percentage) * 100) / 100;
-      gradesResponse[grade as keyof GradeModule.Grade].percentile_low = percentile_low;
+      gradesResponse[grade as keyof GradeModule.Grade] = {
+        percent: percentage,
+        numerator: possible_grades[newGrade],
+        percentile_high: starting_percentile,
+        percentile_low: percentile_low
+      }
       starting_percentile = percentile_low;
     }
   })
