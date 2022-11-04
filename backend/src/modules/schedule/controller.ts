da3b2fb5e6301835ba8b91d5bc90c728e17ca0ed
Zachary Zollman <zacharyzollman@pm.me>
2022-11-04 15:53:17 -0700

    Update controller.ts

--- backend/src/modules/schedule/controller.ts
+++ backend/src/modules/schedule/controller.ts
@@ -4,9 +4,17 @@ import { ScheduleModel } from "./model";
 export async function schedules() {
   const schedules = await ScheduleModel.find();
   return schedules
+  // return null as any as ScheduleModule.Schedule;
   // return schedules.map(formatSchedule);
 }
 
+// get the schedules for a user
 export async function getSchedulesByUser(userID:String) {
-  ScheduleModel.findById("")
+  const userSchedules = await ScheduleModel.findById(userID)
+  return userSchedules
+}
+
+// get the schedule for a user and a specific term
+export async function name(userID:String, term:String) {
+  
 }
