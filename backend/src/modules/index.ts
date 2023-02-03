d3b1c02259144da85588a570324b7e1a5fbabf87
Zachary Zollman <85812228+zacharyzollman@users.noreply.github.com>
2023-02-02 23:38:15 -0800

    Update index.ts

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -2,8 +2,9 @@ import { Resolvers } from "../generated-types/graphql";
 import User from "./user";
 import Grade from "./grade";
 import { merge } from "lodash";
+import Schedule from "./schedule";
 
-const modules = [User, Grade];
+const modules = [User, Grade, Schedule];
 
 // Important: Add all your module's resolver in this
 export const resolvers = merge(modules.map((module) => module.resolver));
