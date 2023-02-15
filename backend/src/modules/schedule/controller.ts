cef5189487bd11f056550007a7145bd27c2ca919
Zachary Zollman <85812228+zacharyzollman@users.noreply.github.com>
2023-02-15 11:46:07 -0800

    Update controller.ts

--- backend/src/modules/schedule/controller.ts
+++ backend/src/modules/schedule/controller.ts
@@ -10,13 +10,13 @@ export async function schedules(): Promise<Schedule[]> {
 }
 
 // get the schedules for a user
-export async function getSchedulesByUser(userID:String): Promise<Schedule[]> {
+export async function getSchedulesByUser(userID:string): Promise<Schedule[]> {
   const userSchedules = await ScheduleModel.find({created_by: userID})
   return userSchedules.map(formatSchedule)
 }
 
 // get the schedule for a user and a specific term
-export async function getScheduleByTermandUser(userID:String, term:String): Promise<Schedule> {
+export async function getScheduleByTermandUser(userID:string, term:string): Promise<Schedule> {
   const userTermSchedule = await ScheduleModel.findOne({created_by: userID, term: term})
   return formatSchedule(userTermSchedule as any)
 }
