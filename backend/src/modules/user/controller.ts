c29950fdf2b9b6a485f5307ba1a68435ca1bde67
maxmwang <orangemax888@gmail.com>
2023-02-15 17:48:54 -0800

    Throw error on invalid user id

--- backend/src/modules/user/controller.ts
+++ backend/src/modules/user/controller.ts
@@ -4,5 +4,10 @@ import { UserModel, UserType } from "./model";
 
 export async function getById(id: String): Promise<User> {
     const user = await UserModel.findById(id);
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
     return formatUser(user as UserType);
 }
