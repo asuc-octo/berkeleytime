e201e88aed61bdeba5ff83d8e3d76066b0e0ab73
Jaden <moorejaden5389@gmail.com>
2023-02-22 22:09:50 -0800

    re-add old pill sorting mechanism

--- frontend/src/app/Catalog/service.ts
+++ frontend/src/app/Catalog/service.ts
@@ -1,4 +1,4 @@
-import { FilterFragment, GetFiltersQuery } from 'graphql';
+import { FilterFragment, GetFiltersQuery, PlaylistType } from 'graphql';
 import {
 	CatalogCategoryKeys,
 	FilterTemplate,
@@ -109,7 +109,7 @@ const processFilterData = (data?: GetFiltersQuery) => {
 	return filters;
 };
 /**
- * 
+ *
  * @param filterItems an empty filter template
  * @param filters processed filter data
  * @description Populates the `options` field of each template item with the
@@ -180,6 +180,17 @@ const SemesterToValue = (semesterFilter: FilterFragment) => {
 	return parseInt(year, 10) + SEMESTER_VALUES[semester];
 };
 
+/**
+ * @description sorts the playlists alphabetically, and
+ * by putting the `units` category at the end of the list
+ */
+const sortPills = (playlists: PlaylistType[]) => {
+	const units = sortByName(playlists.filter((p) => p.category === 'units'));
+	const rest = sortByName(playlists.filter((p) => p.category !== 'units'));
+
+	return rest.concat(units);
+};
+
 export const sortByName = <T extends { name: string }[]>(arr: T) => {
 	return arr.sort((a, b) => a.name.localeCompare(b.name));
 };
@@ -191,5 +202,6 @@ export default {
 	processFilterData,
 	putFilterOptions,
 	sortByName,
-	sortSemestersByLatest
+	sortSemestersByLatest,
+	sortPills
 };
