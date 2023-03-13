476fde6318fa29f61137fe7fd57cfa7f327c27a8
maxmwang <orangemax888@gmail.com>
2023-03-13 02:38:14 -0700

    Use findOneAndUpdate

--- backend/src/modules/user/controller.ts
+++ backend/src/modules/user/controller.ts
@@ -2,18 +2,12 @@ import { User } from "../../generated-types/graphql";
 import { formatUser } from "./formatter";
 import { UserModel, UserType } from "./model";
 
-async function getUserModel(email: string): Promise<UserType> {
+export async function getUserByEmail(email: string): Promise<User> {
     const user = await UserModel.findOne({ email });
 
     if (!user) {
         throw new Error("User not found");
     }
-
-    return user;
-}
-
-export async function getUserByEmail(email: string): Promise<User> {
-    const user = await getUserModel(email);
 
     return formatUser(user as UserType);
 }
@@ -24,21 +18,17 @@ export async function updateUserInfo(
     first_name: string,
     last_name: string
 ) {
-    const user = await getUserModel(email);
-
-    user.username = username === undefined ? user.username : username;
-    user.first_name = first_name === undefined ? user.first_name : first_name;
-    user.last_name = last_name === undefined ? user.last_name : last_name;
-
-    await user.save();
+    await UserModel.findOneAndUpdate({ email }, {
+        username,
+        first_name,
+        last_name,
+    }, { lean: true });
 }
 
 export async function updateUserMajor(email: string, major: string[]) {
-    const user = await getUserModel(email);
-
-    user.major = major === undefined ? user.major : major;
-
-    await user.save();
+    await UserModel.findOneAndUpdate({ email }, {
+        major,
+    }, { lean: true });
 }
 
 export async function updateUserEmailPreferences(
@@ -48,18 +38,14 @@ export async function updateUserEmailPreferences(
     email_enrollment_opening: boolean,
     email_berkeleytime_update: boolean
 ) {
-    const user = await getUserModel(email);
-
-    user.email_class_update = email_class_update == undefined ? user.email_class_update : email_class_update;
-    user.email_grade_update = email_grade_update == undefined ? user.email_grade_update : email_grade_update;
-    user.email_enrollment_opening = email_enrollment_opening == undefined ? user.email_enrollment_opening : email_enrollment_opening;
-    user.email_berkeleytime_update = email_berkeleytime_update == undefined ? user.email_berkeleytime_update : email_berkeleytime_update;
-
-    await user.save();
+    await UserModel.findOneAndUpdate({ email }, {
+        email_class_update,
+        email_grade_update,
+        email_enrollment_opening,
+        email_berkeleytime_update,
+    }, { lean: true });
 }
 
 export async function deleteUser(email: string) {
-    const user = await getUserModel(email);
-
-    await user.deleteOne();
+    await UserModel.deleteOne({ email });
 }
