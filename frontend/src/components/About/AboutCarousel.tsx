77c812a073a56d35d5c1bd6da78889c7e2dba47d
QxBytes <39818795+QxBytes@users.noreply.github.com>
2023-03-15 18:35:57 -0700

    Fix linter warning

--- frontend/src/components/About/AboutCarousel.tsx
+++ frontend/src/components/About/AboutCarousel.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import doe from 'assets/img/about/group/doe.jpg';
 import michaels from 'assets/img/about/group/michaels.jpg';
@@ -16,50 +16,54 @@ enum Sliding {
 	Left = 2
 }
 
-const AboutCarousel: FC = () => {
-	const [shownImage, setShownImage] = useState<number>(2);
+const images = [
+	{ img: retreat_silly, alt: 'retreat silly' },
+	{ img: zoom, alt: 'zoom' },
+	{ img: doe, alt: 'doe' },
+	{ img: grace_janet, alt: 'grace_janet' },
+	{ img: retreat, alt: 'retreat' },
+	{ img: christina_janet, alt: 'christina_janet' },
+	{ img: michaels, alt: 'michaels' },
+	{ img: will, alt: 'will' },
+	{ img: jemma, alt: 'jemma' }
+];
 
+const wrap = (val: number) => (val + images.length) % images.length;
+
+const AboutCarousel = () => {
+	const [shownImage, setShownImage] = useState<number>(2);
 	const [queuedImage, setQueuedImage] = useState<number>(2);
 	const [sliding, setSliding] = useState<Sliding>(Sliding.Still);
-	const [intervalID, setIntervalID] = useState<number>(0);
-	const images = [
-		retreat_silly,
-		zoom,
-		doe,
-		grace_janet,
-		retreat,
-		christina_janet,
-		michaels,
-		will,
-		jemma
-	];
+	const intervalID = useRef<number | undefined>(undefined);
+
 	useEffect(() => {
-		const interval = window.setInterval(nextImage, 5000);
-		setIntervalID(interval);
-	}, []);
-	const stopInterval = () => {
-		clearInterval(intervalID);
-	};
-	const wrap = (val: number) => {
-		return (val + images.length) % images.length;
-	};
-	const previousImage = () => {
+		const intervalHandler = () => {
+			if (sliding === Sliding.Still) {
+				setSliding(Sliding.Left);
+				setQueuedImage((prev) => wrap(prev + 1));
+			}
+		};
+
+		intervalID.current = window.setInterval(intervalHandler, 5000);
+
+		return () => {
+			clearInterval(intervalID.current);
+		};
+	}, [sliding]);
+
+	const changeImage = (slide: Sliding) => {
 		if (sliding === Sliding.Still) {
-			setSliding(Sliding.Right);
-			setQueuedImage((prevShownImage) => wrap(prevShownImage - 1));
+			setSliding(slide);
+			const direction = slide === Sliding.Left ? 1 : -1;
+			setQueuedImage((prev) => wrap(prev + direction));
 		}
 	};
-	const nextImage = () => {
-		if (sliding === Sliding.Still) {
-			setSliding(Sliding.Left);
-			/* If we call from setInterval, "shownImage" will always be the init value */
-			setQueuedImage((prevShownImage) => wrap(prevShownImage + 1));
-		}
-	};
+
 	const triggerSwap = () => {
 		setShownImage(queuedImage);
 		setSliding(Sliding.Still);
 	};
+
 	const getCarouselItemClass = (idx: number) => {
 		let classes = 'about-carousel-item ';
 		if (idx === shownImage) {
@@ -75,6 +79,7 @@ const AboutCarousel: FC = () => {
 		}
 		return classes;
 	};
+
 	const getCarouselClass = () => {
 		let classes = 'about-carousel ';
 		if (sliding === Sliding.Left) {
@@ -84,34 +89,29 @@ const AboutCarousel: FC = () => {
 		}
 		return classes;
 	};
+
 	return (
 		<div className="group mb-5">
 			<div className={getCarouselClass()} onTransitionEnd={triggerSwap}>
-				{images.map((imgVal, idx) => (
-					<div key={imgVal} className={getCarouselItemClass(idx)}>
-						<img src={imgVal} alt="" />
+				{images.map((imgVal, index) => (
+					<div key={imgVal.alt} className={getCarouselItemClass(index)}>
+						<img src={imgVal.img} alt={imgVal.alt} />
 					</div>
 				))}
 			</div>
 			<div className="about-carousel-prev">
 				<span
-					onClick={() => {
-						previousImage();
-						stopInterval();
-					}}
+					onClick={() => changeImage(Sliding.Right)}
 					className="about-carousel-prev-icon"
 					aria-hidden="true"
-				></span>
+				/>
 			</div>
 			<div className="about-carousel-next">
 				<span
-					onClick={() => {
-						nextImage();
-						stopInterval();
-					}}
+					onClick={() => changeImage(Sliding.Left)}
 					className="about-carousel-next-icon"
 					aria-hidden="true"
-				></span>
+				/>
 			</div>
 		</div>
 	);
