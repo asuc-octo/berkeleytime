74c9363c4cd1c00c0973298acd6f81e082014c78
Zachary Zollman <85812228+zacharyzollman@users.noreply.github.com>
2023-03-23 14:25:53 -0700

    update schedule formatting to account for undefined custom_events

--- backend/src/modules/schedule/formatter.ts
+++ backend/src/modules/schedule/formatter.ts
@@ -11,7 +11,7 @@ export function formatSchedule(schedule: ScheduleType): ScheduleModule.Schedule 
     class_IDs: schedule.class_IDs,
     primary_section_IDs: schedule.primary_section_IDs,
     secondary_section_IDs: schedule.secondary_section_IDs,
-    custom_events: schedule.custom_events.map(formatCustomEvents)
+    custom_events: schedule.custom_events ? schedule.custom_events.map(formatCustomEvents) : undefined
     
   };
 }
