9f40c0465f7f6c6b8ced02f5609b7b64b7803175
Zachary Zollman <85812228+zacharyzollman@users.noreply.github.com>
2023-03-28 23:02:54 -0700

    try to remove explicit nulls

--- backend/src/modules/schedule/controller.ts
+++ backend/src/modules/schedule/controller.ts
@@ -1,6 +1,7 @@
 import { formatSchedule } from "./formatter";
-import { Schedule, TermInput, ScheduleInput } from "../../generated-types/graphql";
+import { Schedule, ScheduleInput, CustomEventInput } from "../../generated-types/graphql";
 import { ScheduleModel } from "../../db/schedule";
+import { omitBy } from "lodash";
 
 
 // get the schedules for a user
@@ -30,30 +31,33 @@ export async function removeSchedule(scheduleID: string): Promise<string> {
   return scheduleID
 }
 
+function removeNullEventVals(custom_event: CustomEventInput) {
+  for (const key in custom_event) {
+      if (custom_event[key as  keyof CustomEventInput] === null) {
+          delete custom_event[key as keyof CustomEventInput];
+      }
+  }
+}
 
 // create a new schedule
 //export async function createSchedule(created_by: string, term: TermInput, schedule_name: string | undefined | null, is_public: boolean | null | undefined, class_IDs: string[] | undefined | null, primary_section_IDs: string[] | undefined | null, secondary_section_IDs: string[] | undefined | null): Promise<Schedule> {
 export async function createSchedule(main_schedule: ScheduleInput): Promise<Schedule> {
-
-  // const schedulePartsToCreate: minimumViableSchedule = { created_by: created_by, term: term }
-  
-  // schedulePartsToCreate.name = schedule_name ? schedule_name : undefined
-  // schedulePartsToCreate.class_IDs = class_IDs ? class_IDs : undefined
-  // schedulePartsToCreate.primary_section_IDs = primary_section_IDs ? primary_section_IDs : undefined
-  // schedulePartsToCreate.secondary_section_IDs = secondary_section_IDs ? secondary_section_IDs : undefined
-  // schedulePartsToCreate.is_public = is_public ? is_public : undefined
-
-  const newSchedule = await ScheduleModel.create(main_schedule)
-  // const newSchedule = await ScheduleModel.create({_id: newID, name: schedule_name, created_by: created_by, date_created: current_time, last_updated: current_time, term: term, public: is_public, class_IDs: class_IDs, section_IDs: section_IDs})
+  if (main_schedule.custom_events) {
+    main_schedule.custom_events.forEach(removeNullEventVals)
+  }
+  const non_null_schedule = omitBy(main_schedule, (value) => value == null)
+  const newSchedule = await ScheduleModel.create(non_null_schedule)
   return formatSchedule(newSchedule)
 }
 
 
-
 // update an existing schedule
 export async function editSchedule(schedule_ID: string, main_schedule: ScheduleInput): Promise<Schedule> {
-
-  const updatedSchedule = await ScheduleModel.findByIdAndUpdate(schedule_ID, main_schedule, {returnDocument: 'after'})
+  if (main_schedule.custom_events) {
+    main_schedule.custom_events.forEach(removeNullEventVals)
+  }
+  const non_null_schedule = omitBy(main_schedule, (value) => value == null)
+  const updatedSchedule = await ScheduleModel.findByIdAndUpdate(schedule_ID, non_null_schedule, {returnDocument: 'after'})
   if (!updatedSchedule) {
     throw new Error("Unable to update existing schedule")
   }
