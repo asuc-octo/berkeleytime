5430efd89b8a02bb28e51c1257914441033cd325
Matthew Rowland <contact@mathhulk.com>
2023-09-24 11:48:43 -0700

    Remove fallbackElement since it didn't work; hope for the best

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,7 +4,6 @@ import Catalog from './app/Catalog';
 import Landing from './views/Landing';
 import Error from './views/Error';
 import Layout from 'components/Common/Layout';
-import BTLoader from 'components/Common/BTLoader';
 
 const Grades = () => import('./app/Grades');
 const Enrollment = () => import('./app/Enrollment');
@@ -65,14 +64,5 @@ const router = createBrowserRouter([
 ]);
 
 export default function Routes() {
-	return (
-		<RouterProvider
-			router={router}
-			fallbackElement={
-				<div className="viewport-app">
-					<BTLoader fill />
-				</div>
-			}
-		/>
-	);
+	return <RouterProvider router={router} />;
 }
