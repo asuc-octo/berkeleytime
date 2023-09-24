b5b9081e4010921f2995bd5a1190ec413de6307e
Matthew Rowland <contact@mathhulk.com>
2023-09-24 11:46:11 -0700

    Open browser automatically, convert final components to function components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,9 +11,7 @@ export default defineConfig({
 	server: {
 		host: true,
 		port: 3000,
-		watch: {
-			usePolling: true
-		}
+		open: true
 	},
 	publicDir: path.resolve(__dirname, 'public'),
 	plugins: [
