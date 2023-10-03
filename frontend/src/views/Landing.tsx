70862290600eb9410342bcae30930c9ac77be80e
xyntechx <xyntechx@gmail.com>
2023-10-02 19:12:17 -0700

    Remove application banner and redirect

--- frontend/src/views/Landing.tsx
+++ frontend/src/views/Landing.tsx
@@ -1,13 +1,11 @@
 import Explore from 'components/Landing/Explore';
 import Mission from 'components/Landing/Mission';
 import Blurbs from 'components/Landing/Blurbs';
-import LandingModal from 'components/Landing/LandingModal';
 import Jumbotron from 'components/Landing/Jumbotron';
 
 export default function Landing() {
 	return (
 		<>
-			<LandingModal />
 			<div className="landing-jumbo">
 				<Jumbotron />
 			</div>
