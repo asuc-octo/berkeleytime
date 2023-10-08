edd186a62a3a011bf1ea33d0ef1a43423f4ce82d
Kartavya Sharma <kartavya@berkeley.edu>
2023-10-08 15:02:43 -0700

    Added constants file

---
+++ backend/scripts/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * States for mongoose's connection to the local MongoDB instance or 
+ * MongoDB Atlas.
+ */
+export const DB_CONNECTION_STATES = [
+    "connecting",
+    "connected",
+    "disconnecting",
+    "disconnected"
+]
