63ac768e2f2decd2ac50c04c9338cea2c479b232
Kartavya Sharma <kartavya@berkeley.edu>
2023-10-09 16:00:14 -0700

    Added types for all SIS class API endpoints

--- backend/scripts/models/classes.ts
+++ backend/scripts/models/classes.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from "mongoose"
 import { paths } from "../common/types/api/classes-schema";
 
-type ClassSuccessResponse = paths["/v1/classes"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+type v1ClassesSuccessResponse = paths["/v1/classes"]["get"]["responses"]["200"]["schema"]["apiResponse"]
 
-const classSchema = new Schema<ClassSuccessResponse>({
+const classSchema = new Schema<v1ClassesSuccessResponse>({
 	"apiResponse": {
 		"correlationId": {
 			"type": "String"
@@ -29,6 +29,11 @@ const classSchema = new Schema<ClassSuccessResponse>({
 	}
 });
 
-const ClassModel = mongoose.model<ClassSuccessResponse>("Class", classSchema);
+export const ClassModel = mongoose.model<v1ClassesSuccessResponse>("Class", classSchema);
 
-export default ClassModel;
+type v1ClassesDescriptorsSuccessResponse = paths["/v1/classes/descriptors"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+type v1ClassesSectionsSuccessResponse = paths["/v1/classes/sections"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+type v1ClassesSectionsDescriptorsSuccessResponse = paths["/v1/classes/sections/descriptors"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+type v1ClassesEnrollmentByTermSuccessResponse = paths["/v1/classes/sections/terms/{term-id}/updated/enrollments"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+type v1ClassesSectionByIdSuccessResponse = paths["/v1/classes/sections/{class-section-id}"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+type v1ClassesEnrollmentBySectionAndTermSuccessResponse = paths["/v1/classes/sections/{class-section-id}/term/{term-id}/enrollment"]["get"]["responses"]["200"]["schema"]["apiResponse"]
