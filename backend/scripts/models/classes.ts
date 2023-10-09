9d6f097e95b79fa1b0a4a63e160939a4ad8db492
Kartavya Sharma <kartavya@berkeley.edu>
2023-10-08 21:41:15 -0700

    Added classes SIS API response model in mongoose

---
+++ backend/scripts/models/classes.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose"
+import { paths } from "../common/types/api/classes-schema";
+
+type ClassSuccessResponse = paths["/v1/classes"]["get"]["responses"]["200"]["schema"]["apiResponse"]
+
+const classSchema = new Schema<ClassSuccessResponse>(
+	{
+		"apiResponse": {
+			"correlationId": {
+				"type": "String"
+			},
+			"httpStatus": {
+				"code": {
+					"type": "Date"
+				},
+				"description": {
+					"type": "String"
+				}
+			},
+			"responseType": {
+				"type": "String"
+			},
+			"response": {
+				"classes": {
+					"type": [
+						"Mixed"
+					]
+				}
+			}
+		}
+	}
+);
+
+const ClassModel = mongoose.model<ClassSuccessResponse>("Class", classSchema);
