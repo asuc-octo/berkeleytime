cef5a38414644e9f75a69b21d81ffe36f7e34ce9
Kartavya Sharma <kartavya@berkeley.edu>
2023-10-08 21:47:26 -0700

    Fixed models/classes.ts formatting

--- backend/scripts/models/classes.ts
+++ backend/scripts/models/classes.ts
@@ -3,32 +3,30 @@ import { paths } from "../common/types/api/classes-schema";
 
 type ClassSuccessResponse = paths["/v1/classes"]["get"]["responses"]["200"]["schema"]["apiResponse"]
 
-const classSchema = new Schema<ClassSuccessResponse>(
-	{
-		"apiResponse": {
-			"correlationId": {
+const classSchema = new Schema<ClassSuccessResponse>({
+	"apiResponse": {
+		"correlationId": {
+			"type": "String"
+		},
+		"httpStatus": {
+			"code": {
+				"type": "Date"
+			},
+			"description": {
 				"type": "String"
-			},
-			"httpStatus": {
-				"code": {
-					"type": "Date"
-				},
-				"description": {
-					"type": "String"
-				}
-			},
-			"responseType": {
-				"type": "String"
-			},
-			"response": {
-				"classes": {
-					"type": [
-						"Mixed"
-					]
-				}
+			}
+		},
+		"responseType": {
+			"type": "String"
+		},
+		"response": {
+			"classes": {
+				"type": [
+					"Mixed"
+				]
 			}
 		}
 	}
-);
+});
 
 const ClassModel = mongoose.model<ClassSuccessResponse>("Class", classSchema);
