bb1e4d27e203dc761d67e19ffa247a7fc6c9d03c
JackyJerkyYt <jackywongcheukki@gmail.com>
2023-10-09 19:30:44 -0700

    added the final as dummy file

--- backend/berkeleytime/config/semesters/spring2024.py
+++ backend/berkeleytime/config/semesters/spring2024.py
@@ -1,5 +1,5 @@
 """Configurations for Spring 2024."""
-# from berkeleytime.config.finals.semesters.spring2024 import *
+from berkeleytime.config.finals.semesters.spring2024 import *
 
 import datetime
 
