1380a069c98834c90987ca51881e09eaf6953f51
kara4600 <kara178@comcast.net>
2023-10-23 13:11:37 -0700

    Add info blurbs to footer

--- frontend/src/app/Home/HomeFooter/HomeFooter.tsx
+++ frontend/src/app/Home/HomeFooter/HomeFooter.tsx
@@ -1,4 +1,4 @@
-import { Discord, Facebook, GitHub, Instagram } from 'iconoir-react';
+import { ArrowRight, Discord, Facebook, GitHub, Instagram } from 'iconoir-react';
 import asuc_logo from '../../../assets/img/landing/asuc_logo.png';
 import stf_logo from '../../../assets/img/landing/stf_logo.jpeg';
 import ocf_logo from '../../../assets/img/landing/ocf_logo.png';
@@ -7,6 +7,45 @@ import styles from './HomeFooter.module.scss';
 const HomeFooter = () => {
 	return (
 		<div className={styles.root}>
+			<div className={styles.info}>
+				<div className={styles.infoBlock}>
+					<h2>What We Do</h2>
+					<p>
+						We're a small group of student volunteers at UC Berkeley that actively builds, improves,
+						and maintains Berkeleytime.
+					</p>
+					<p>
+						<a>
+							Read More <ArrowRight width={'1.15em'} height={'1.15em'} />
+						</a>
+					</p>
+				</div>
+				<div className={styles.infoBlock}>
+					<h2>Get Involved</h2>
+					<p>
+						Berkeleytime is maintained by students just like you! If you're looking to make a
+						difference – we're looking for you.
+					</p>
+					<p>
+						<a>
+							Learn More <ArrowRight width={'1.15em'} height={'1.15em'} />
+						</a>
+					</p>
+				</div>
+				<div className={styles.infoBlock}>
+					<h2>Have Feedback?</h2>
+					<p>
+						We'd love to hear it! User feedback helps us continually improve Berkeleytime. We want
+						to know what you think.
+					</p>
+					<p>
+						<a>
+							Reach Out <ArrowRight width={'1.15em'} height={'1.15em'} />
+						</a>
+					</p>
+				</div>
+			</div>
+
 			<div className={styles.blurb}>
 				<h4>In Memory Of Courtney Brousseau</h4>
 				<p>
