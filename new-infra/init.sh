2d01af037d0b6acea993f376b4d9fcab23dd6419
maxmwang <orangemax888@gmail.com>
2024-01-27 01:27:07 -0800

    feat: mongodb

--- new-infra/init.sh
+++ new-infra/init.sh
@@ -3,14 +3,18 @@
 # install ingress-nginx with helm
 helm upgrade --install ingress-nginx ingress-nginx \
     --repo https://kubernetes.github.io/ingress-nginx \
-    --namespace ingress-nginx --create-namespace
+    --namespace ingress-nginx --create-namespace        # using namespace=bt-base requires changing chart values (through --set or --values)
 
 # install app with helm. run in /new-infra
-helm install app \
+helm install bt \
     --name-template=bt \
-    --namespace=bt --create-namespace \
+    --namespace=bt --create-namespace       # namespace=bt-{env}
 
 # uninstall app with helm on config changes. run in /new-infra
-helm uninstall app \
-    --namespace=bt
+helm uninstall bt \
+    --namespace=bt                          # namespace=bt-{env}
 
+# install mongodb with helm
+helm upgrade --install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb \
+    --namespace=bt --create-namespace \     # namespace=bt-{env}
+    --values=./mongodb/values.yaml
