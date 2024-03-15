00f959839047456e2cdd4fc342253ed5ded26901
Matthew Rowland <contact@mathhulk.com>
2024-03-14 23:46:54 -0700

    feat: Init dynamic calendar

--- frontend/src/app/Schedules/Calendar/index.tsx
+++ frontend/src/app/Schedules/Calendar/index.tsx
@@ -1,32 +1,138 @@
 import { MouseEvent, useMemo, useRef, useState } from "react";
 
+import { Event } from "../types";
 import styles from "./Calendar.module.scss";
 
-export default function Calendar() {
+const adjustAttachedEvents = (
+  relevantEvents: Event[],
+  attachedEvents: number[],
+  minutes: number[][],
+  positions: Record<number, [number, number]>
+) => {
+  const adjustedEvents: number[] = [];
+
+  const adjustEvent = (eventId: number) => {
+    if (adjustedEvents.includes(eventId)) return;
+
+    adjustedEvents.push(eventId);
+
+    positions[eventId][1]++;
+
+    const event = relevantEvents.find(({ id }) => id === eventId);
+    if (!event) return;
+
+    const top = getY(event.startTime);
+    const height = getY(event.endTime) - top;
+
+    for (let i = top; i < top + height; i++) {
+      for (const eventId of minutes[i]) {
+        adjustEvent(eventId);
+      }
+    }
+  };
+
+  for (const eventId of attachedEvents) {
+    adjustEvent(eventId);
+  }
+};
+
+const getY = (time: string) => {
+  const [hour, minute] = time.split(":");
+  return (parseInt(hour) - 6) * 60 + parseInt(minute);
+};
+
+interface CalendarProps {
+  events: Event[];
+}
+
+export default function Calendar({ events }: CalendarProps) {
   const ref = useRef<HTMLDivElement>(null);
   const [y, setY] = useState<number | null>(null);
+
+  const days = useMemo(
+    () =>
+      [...Array(7)].map((_, day) => {
+        const positions: Record<number, [number, number]> = {};
+        const minutes: number[][] = [...Array(60 * 18)].map(() => []);
+
+        const relevantEvents = events
+          // Filter events for the current day
+          .filter((event) => event.days[day])
+          // Sort events by start time
+          .sort((a, b) => getY(a.startTime) - getY(b.startTime));
+
+        for (const event of relevantEvents) {
+          const top = getY(event.startTime);
+          const height = getY(event.endTime) - top;
+
+          const attachedEvents = minutes[top];
+
+          let position = 0;
+
+          while (
+            attachedEvents.findIndex(
+              (eventId) => positions[eventId][0] === position
+            ) !== -1
+          ) {
+            position++;
+          }
+
+          if (
+            attachedEvents.length > 0 &&
+            Math.max(
+              position,
+              ...attachedEvents.map((eventId) => positions[eventId][0])
+            ) === position
+          ) {
+            adjustAttachedEvents(
+              relevantEvents,
+              attachedEvents,
+              minutes,
+              positions
+            );
+          }
+
+          positions[event.id] = [
+            position,
+            attachedEvents.length === 0 ? 1 : positions[attachedEvents[0]][1],
+          ];
+
+          for (let i = top; i < top + height; i++) {
+            minutes[i].push(event.id);
+            console.log(minutes[i]);
+          }
+        }
+
+        return relevantEvents.map((event) => {
+          const [position, columns] = positions[event.id];
+
+          return {
+            ...event,
+            position,
+            columns,
+          };
+        });
+      }),
+    [events]
+  );
 
   const currentTime = useMemo(() => {
     if (!ref.current || !y) return;
 
-    const percent = y / ref.current.getBoundingClientRect().height;
+    const hour = (Math.floor(y / 60) + 6) % 12 || 12;
+    const minute = Math.floor(y % 60);
 
-    const hour = (Math.floor(percent * 18) + 6) % 12 || 12;
-
-    const minute = Math.floor((percent * 18 * 60) % 60);
-    const formattedMinute = minute < 10 ? `0${minute}` : minute;
-
-    return `${hour}:${formattedMinute}`;
+    return `${hour}:${minute < 10 ? `0${minute}` : minute}`;
   }, [y]);
 
   const handleMouseMove = (event: MouseEvent<HTMLDivElement>) => {
     if (!ref.current) return;
 
     const y = Math.max(
-      13,
+      15,
       Math.min(
         event.clientY - ref.current.getBoundingClientRect().top,
-        ref.current.clientHeight - 11
+        ref.current.clientHeight - 15
       )
     );
 
@@ -59,7 +165,7 @@ export default function Calendar() {
       >
         <div className={styles.sideBar}>
           {currentTime && y && (
-            <div className={styles.time} style={{ top: `${y - 13}px` }}>
+            <div className={styles.time} style={{ top: `${y - 11.5}px` }}>
               {currentTime}
             </div>
           )}
@@ -72,22 +178,47 @@ export default function Calendar() {
           ))}
         </div>
         <div className={styles.week}>
-          {[...Array(7)].map((_, day) => (
+          {days.map((events, day) => (
             <div key={day} className={styles.day}>
-              <div className={styles.event}>
-                <p className={styles.heading}>AFRICAM 294</p>
-                <p className={styles.description}>Lecture</p>
-              </div>
-              <div className={styles.event}>
-                <p className={styles.heading}>Soccer practice</p>
-                <p className={styles.description}>Custom event</p>
-              </div>
-              {y && (
-                <div className={styles.line} style={{ top: `${y - 1}px` }} />
-              )}
               {[...Array(18)].map((_, hour) => (
                 <div key={hour} className={styles.hour}></div>
               ))}
+              {events.map(
+                (
+                  {
+                    kind,
+                    startTime,
+                    columns,
+                    position,
+                    endTime,
+                    color,
+                    placeholder,
+                  },
+                  index
+                ) => {
+                  const top = getY(startTime);
+                  const height = getY(endTime) - top;
+
+                  return (
+                    <div
+                      key={index}
+                      className={styles.event}
+                      style={{
+                        top: `${top}px`,
+                        backgroundColor: color,
+                        opacity: placeholder ? 0.25 : 1,
+                        height: `${height}px`,
+                        width: `calc((100% - 8px) / ${columns})`,
+                        left: `calc(4px + (((100% - 8px) / ${columns}) * ${position})`,
+                      }}
+                    >
+                      <div className={styles.heading}>AFRICAM 294</div>
+                      <div className={styles.description}>{kind}</div>
+                    </div>
+                  );
+                }
+              )}
+              {y && <div className={styles.line} style={{ top: `${y}px` }} />}
             </div>
           ))}
         </div>
---
+++ frontend/src/app/Schedules/types.d.ts
@@ -0,0 +1,9 @@
+export interface Event {
+  days: boolean[];
+  startTime: string;
+  endTime: string;
+  kind: string;
+  color: string;
+  id: number;
+  placeholder?: boolean;
+}
