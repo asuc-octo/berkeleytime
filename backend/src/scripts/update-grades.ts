2eebeade9b4d6058e3698767dc0b74dea4ef5172
ARtheboss <advayratan@yahoo.com>
2024-04-23 23:30:52 -0700

    grades bug fixes

--- backend/src/scripts/update-grades.ts
+++ backend/src/scripts/update-grades.ts
@@ -8,6 +8,19 @@ const getAllGradeFiles = async () => {
     return Fs.readdirSync(resourcesPath)
 }
 
+const capitalizeFirstLetter = (s : string) : string => {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+let accessed = false;
+
+const getOrDefault = (values : string[], m : Map<string, number>, key : string) : string | null => {
+    if (!m.has(key)) return null;
+    const ind = m.get(key)
+    if (ind === undefined) return null;
+    return values[m.get(key)!];
+}
+
 const fileToMongo = async (file : string) => {
     const matches = /(fall|spring|summer)_([0-9]{4})/.exec(file)
 
@@ -21,31 +34,32 @@ const fileToMongo = async (file : string) => {
     
     const data = Fs.readFileSync(`${resourcesPath}/${file}`)
 
-    console.log("Starting: " + file, data.length)
+    console.log("Starting: " + file, "Lines: " + data.length)
     let lines = data.toString().split("\n")
     const header = lines[0].split(",")
     lines = lines.slice(1)
 
     header.forEach((name, index) => {
-        headerMap.set(name, index)
+        headerMap.set(name.replace(/^[0-9\s]*|[+*\r\n]/g, ""), index)
     })
     lines.forEach((line) => {
         try {
+            if (!line) return
             const values = line.split(",")
             const obj:any = {
-                CourseControlNbr: parseInt(values[headerMap.get("Course Control Nbr")!]),
-                CourseNumber: values[headerMap.get("Course Number")!],
-                CourseSubjectShortNm: values[headerMap.get("Course Subject Short Nm")!],
-                CourseTitleName: values[headerMap.get("Course Title Nm")!],
-                EnrollmentCnt: parseInt(values[headerMap.get("EnrollmentCnt")!]),
-                GradeNm: values[headerMap.get("Grade Nm")!],
-                GradeSortNbr: values[headerMap.get("Grade Sort Nbr")!],
-                GradeSubtypeDesc: values[headerMap.get("Grade Subtype Desc")!],
-                GradeTypeDesc: values[headerMap.get("Grade Type Desc")!],
-                InstructorName: values[headerMap.get("Instructor Name")!].split("; "),
-                SectionNbr: values[headerMap.get("Section Nbr")!],
+                CourseControlNbr: parseInt(getOrDefault(values, headerMap, "Course Control Nbr") ?? ""),
+                CourseNumber: getOrDefault(values, headerMap, "Course Number"),
+                CourseSubjectShortNm: getOrDefault(values, headerMap, "Course Subject Short Nm"),
+                CourseTitleName: getOrDefault(values, headerMap, "Course Title Nm"),
+                EnrollmentCnt: parseInt(getOrDefault(values, headerMap, "Enrollment Cnt") ?? ""),
+                GradeNm: getOrDefault(values, headerMap, "Grade Nm"),
+                GradeSortNbr: getOrDefault(values, headerMap, "Grade Sort Nbr"),
+                GradeSubtypeDesc: getOrDefault(values, headerMap, "Grade Subtype Desc"),
+                GradeTypeDesc: getOrDefault(values, headerMap, "Grade Type Desc"),
+                InstructorName: (getOrDefault(values, headerMap, "Instructor Name") ?? "").split("; "),
+                SectionNbr: getOrDefault(values, headerMap, "Section Nbr"), 
                 term: {
-                    semester: semester,
+                    semester: capitalizeFirstLetter(semester),
                     year: year
                 }
             }
@@ -74,7 +88,7 @@ const fileToMongo = async (file : string) => {
             await fileToMongo(file)
         }
 
-        console.log((await GradeModel.find()).length)
+        console.log(`Loaded ${(await GradeModel.find()).length} entries`)
 
         console.log("SUCCESS")
 
