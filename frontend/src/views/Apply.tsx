6a0354a1a3aa3f27afe10372d199c0222b500ce7
maxmwang <orangemax888@gmail.com>
2024-09-03 12:25:38 -0700

    update link

--- frontend/src/views/Apply.tsx
+++ frontend/src/views/Apply.tsx
@@ -1,4 +1,4 @@
 export function Component() {
-	window.location.replace('https://airtable.com/appllQy6crmpquCOQ/shrnGwxvaCi7onxaC');
+	window.location.replace('https://airtable.com/appllQy6crmpquCOQ/pag5ngU9hgKJt5eAs/form');
 	return null;
 }
