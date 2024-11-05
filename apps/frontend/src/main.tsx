a3842a61baed1427c2cffda1d720b21563b6bc5a
Leo <klhftco@berkeley.edu>
2024-11-05 02:30:11 -0800

    frontend break

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./main.scss";
 
-// const t: Number[] = [];
-// console.log(t[0]);
+const t: Number[] = [];
+console.log(t[0]);
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
