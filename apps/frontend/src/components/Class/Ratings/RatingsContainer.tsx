f497ef47996c2abec81cc00df0462c7b5ba6fbfc
Nicole Lee <nicolelee7887@gmail.com>
2024-11-21 17:19:47 -0800

    changed color

--- apps/frontend/src/components/Class/Ratings/RatingsContainer.tsx
+++ apps/frontend/src/components/Class/Ratings/RatingsContainer.tsx
@@ -125,8 +125,8 @@ function RatingDetail({
               <Tooltip.Trigger asChild>
                 <span className={styles.info}>
                   <svg
-                    width="12"
-                    height="12"
+                    width="14"
+                    height="14"
                     viewBox="0 0 12 12"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
