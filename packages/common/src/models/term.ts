b301658a6ca8678c27c5aa0271299fe8b5beb439
ARtheboss <advayratan@yahoo.com>
2024-11-26 11:16:44 -0800

    ratings bubble changes

--- packages/common/src/models/term.ts
+++ packages/common/src/models/term.ts
@@ -5,7 +5,7 @@ import { descriptor } from "../lib/sis";
 
 const temporalPosition = {
   type: String,
-  enum: ["Previous", "Current", "Next"],
+  enum: ["Past", "Current", "Future"],
 };
 
 export const semester = {
