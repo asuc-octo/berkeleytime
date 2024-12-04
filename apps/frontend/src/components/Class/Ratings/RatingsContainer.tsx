21bbbd5e97afb8431b52dda3a8220e28fc014f16
PineND <pinenguyen@berkeley.edu>
2024-12-03 18:33:15 -0800

    lint

--- apps/frontend/src/components/Class/Ratings/RatingsContainer.tsx
+++ apps/frontend/src/components/Class/Ratings/RatingsContainer.tsx
@@ -177,8 +177,11 @@ export function RatingsContainer() {
         ? termRatings.metrics
         : aggregatedRatings?.course?.aggregatedRatings?.metrics;
 
-    if (!metrics || !metrics.some(
-      (metric : any) => isMetricRating(metric.metricName) && metric.count !== 0)
+    if (
+      !metrics ||
+      !metrics.some(
+        (metric: any) => isMetricRating(metric.metricName) && metric.count !== 0
+      )
     ) {
       return null;
     }
