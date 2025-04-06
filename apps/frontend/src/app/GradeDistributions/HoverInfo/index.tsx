4dbae9af6fc7528bf50f45658e524557cb9c23c1
ARtheboss <30683624+ARtheboss@users.noreply.github.com>
2025-04-06 14:29:09 -0700

    lint

--- apps/frontend/src/app/GradeDistributions/HoverInfo/index.tsx
+++ apps/frontend/src/app/GradeDistributions/HoverInfo/index.tsx
@@ -153,7 +153,9 @@ export default function HoverInfo({
             tooltip="for this instructor/semester combination"
           />
         )}
-        {gradeDistribution ? `(${gradeDistribution.average?.toFixed(3)}` : "No data"}
+        {gradeDistribution
+          ? `(${gradeDistribution.average?.toFixed(3)}`
+          : "No data"}
       </div>
       {hoveredLetter && (
         <div>
