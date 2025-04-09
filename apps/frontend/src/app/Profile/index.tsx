8f68161f81f34056979ebc80c2aa786c633941ff
Eric Xu <41406930+Youngster-Joey@users.noreply.github.com>
2025-04-09 00:35:06 -0700

    schedules added to user page

--- apps/frontend/src/app/Profile/index.tsx
+++ apps/frontend/src/app/Profile/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import classNames from "classnames";
 import { HelpCircle, LogOut, ProfileCircle, Star } from "iconoir-react";
@@ -13,14 +13,36 @@ import Carousel from "@/components/Carousel";
 import ClassCard from "@/components/ClassCard";
 import ClassDrawer from "@/components/ClassDrawer";
 
-import { useReadUser } from "@/hooks/api/users/useReadUser";
+import { useReadSchedules, useReadUser } from "@/hooks/api";
+import { ISchedule } from "@/lib/api";
+
 import { signOut } from "@/lib/api";
 import { IUser } from "@/lib/api";
 
 import styles from "./Profile.module.scss";
 
+const SEMESTER_ORDER = ["Spring", "Summer", "Fall"];
+
 function YourAccount({ user }: { user: IUser | undefined }) {
   if (!user) return <></>;
+  const { data: schedules, loading: schedulesLoading } = useReadSchedules({
+    skip: !user,
+  });
+  
+  const schedulesBySemester = useMemo(() => {
+    return schedules
+      ? schedules.reduce(
+          (acc, schedule) => {
+            const term = `${schedule.semester} ${schedule.year}`;
+            if (!acc[term]) acc[term] = [];
+            acc[term].push(schedule);
+            return acc;
+          },
+          {} as { [key: string]: ISchedule[] }
+        )
+      : ({} as { [key: string]: ISchedule[] });
+  }, [schedules]);
+
   return (
     <div>
       <h2>Personal Information</h2>
@@ -45,30 +67,63 @@ function YourAccount({ user }: { user: IUser | undefined }) {
       <h2>Bookmarked Classes</h2>  
       <div>
         <Carousel.CarouselNoTitle>
-            {/* TODO: Better placeholder states */}
-            {user.bookmarkedClasses.length === 0 ? (
-              <Carousel.Item>
-                <Flex
-                  align="center"
-                  justify="center"
-                  className={styles.placeholder}
-                >
-                  <Text>No bookmarked classes</Text>
-                </Flex>
+          {/* TODO: Better placeholder states */}
+          {user.bookmarkedClasses.length === 0 ? (
+            <Carousel.Item>
+              <Flex
+                align="center"
+                justify="center"
+                className={styles.placeholder}
+              >
+                <Text>No bookmarked classes</Text>
+              </Flex>
+            </Carousel.Item>
+          ) : (
+            user.bookmarkedClasses.map((bookmarkedClass, index) => (
+              <Carousel.Item key={index}>
+                <ClassDrawer {...bookmarkedClass}>
+                  <ClassCard class={bookmarkedClass} />
+                </ClassDrawer>
               </Carousel.Item>
-            ) : (
-              user.bookmarkedClasses.map((bookmarkedClass, index) => (
-                <Carousel.Item key={index}>
-                  <ClassDrawer {...bookmarkedClass}>
-                    <ClassCard class={bookmarkedClass} />
-                  </ClassDrawer>
-                </Carousel.Item>
-              ))
-            )}
-          </Carousel.CarouselNoTitle>
-          <Link to={"/catalog"} className={styles.afterCarouselLink}>
-            See all classes
-          </Link>
+            ))
+          )}
+        </Carousel.CarouselNoTitle>
+        <Link to={"/catalog"} className={styles.afterCarouselLink}>
+          See all classes
+        </Link>
+        { schedulesLoading ? <></> : (
+          <div>
+            <h2>Saved Schedules</h2>
+            {Object.keys(schedulesBySemester)
+              .sort((a, b) => {
+                return schedulesBySemester[a][0].year ==
+                  schedulesBySemester[b][0].year
+                  ? SEMESTER_ORDER.indexOf(schedulesBySemester[b][0].semester) -
+                      SEMESTER_ORDER.indexOf(schedulesBySemester[a][0].semester)
+                  : schedulesBySemester[b][0].year -
+                      schedulesBySemester[a][0].year;
+              })
+              .map((sem) => {
+                return (
+                  <Carousel.CarouselNoTitle>
+                    {schedulesBySemester[sem].map(({ _id, name, classes }, i) => {
+                      return (
+                        <Carousel.Schedule
+                          key={i}
+                          _id={_id}
+                          name={name}
+                          classes={classes}
+                        />
+                      );
+                    })}
+                  </Carousel.CarouselNoTitle>
+                );
+              })}
+            <Link to={"/schedules"} className={styles.afterCarouselLink}>
+              See all schedules
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
