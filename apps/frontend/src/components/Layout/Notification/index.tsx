07aba981b7ea82b3018f6d58fbb11bba336331a9
Aayush Shah <aayush30shah@gmail.com>
2025-04-09 19:54:43 -0700

    add dummy API call for banner info and update styling

--- apps/frontend/src/components/Layout/Notification/index.tsx
+++ apps/frontend/src/components/Layout/Notification/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Xmark } from "iconoir-react";
 import { Link } from "react-router-dom";
@@ -7,30 +7,61 @@ import { Button } from "@repo/theme";
 
 import styles from "./Notification.module.scss";
 
+interface NotifacationProps {
+  mainText: string;
+  buttonText: string;
+  link: string;
+}
+
 export default function Notification() {
   const [open, setOpen] = useState(
     () => localStorage.getItem("showNotification") !== "1"
   );
+  const [notificationData, setNotificationData] = useState<NotifacationProps>({
+    mainText: "",
+    buttonText: "",
+    link: "",
+  });
+
+  useEffect(() => {
+    const fetchNotificationData = async () => {
+      try {
+        const response = await fetch("https://localhost:8080"); // Dummy API
+        const data = await response.json();
+        setNotificationData({
+          mainText: data.mainText,
+          buttonText: data.buttonText,
+          link: data.link,
+        });
+      } catch (error) {
+        console.error("Error fetching notification data:", error);
+        setNotificationData({
+          mainText: "",
+          buttonText: "",
+          link: "",
+        });
+      }
+    };
+
+    fetchNotificationData();
+  }, []);
 
   const handleClose = () => {
     localStorage.setItem("showNotification", "1");
     setOpen(false);
   };
 
-  if (!open) return null;
-
-  // TODO: pull text and link from an API
-  const text =
-    "We are looking to improve our features to redesign the enrollment experience!";
-  const link = "https://bit.ly/berkeleytime-enrollment";
+  if (!open || !notificationData.mainText) return null;
 
   return (
     <a className={styles.root}>
       <div className={styles.group}>
-        <p className={styles.text}>{text}</p>
-        <Link to={link}>
-          <Button variant="solid">Fill out our survey</Button>
-        </Link>
+        <p className={styles.text}>{notificationData.mainText}</p>
+        {notificationData.buttonText && notificationData.link && (
+          <Link to={notificationData.link}>
+            <Button variant="solid">{notificationData.buttonText}</Button>
+          </Link>
+        )}
       </div>
       <Xmark
         className={styles.close}
