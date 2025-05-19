065243164943d54b5dbba8999475a85a0e013bd2
PineND <pinenguyen@berkeley.edu>
2025-05-19 13:39:34 -0700

    initialize

---
+++ apps/frontend/src/app/Profile/Ratings/index.tsx
@@ -0,0 +1,12 @@
+export default function Ratings() {
+  return (
+    <div>
+      <h1>Ratings</h1>
+      Thank you for beta testing our website!
+      <br />
+      To leave us feedback, either use the blue button on the bottom right or
+        email us at octo.berkeleytime@asuc.org.
+      </div>
+    );
+  }
+  
