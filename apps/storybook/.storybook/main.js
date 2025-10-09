f2e9a1b0a5e02bfffd4e6aacff3ef10e35b972b7
vaclis.mbp <nckusic2022@gmail.com>
2025-10-08 18:23:28 -0700

    fix: storybook ES module compatibility
    Fix storybook build error by properly importing createRequire
    for ES module context. This resolves the 'require is not defined'
    error when building storybook.

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,4 +1,8 @@
 import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
