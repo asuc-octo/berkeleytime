9b1fcacee545e59507465ea7bce20b282b99df21
Nathan Dai <nathandai2000@gmail.com>
2025-10-15 11:20:28 -0700

    fix gpa to letter grade lookup logic

--- apps/frontend/src/lib/grades.ts
+++ apps/frontend/src/lib/grades.ts
@@ -18,19 +18,22 @@ export const PASS_FAIL = ["P", "NP"] as const;
 
 export const GRADES = [...LETTER_GRADES, ...PASS_FAIL] as const;
 
+// gpa scale based on
+// https://reslife.berkeley.edu/gpa-calculator/
 export function getLetterGradeFromGPA(gpa: number): string {
-  if (!gpa) return "F";
-  if (gpa > 4) return "A+";
+  if (gpa >= 4.0) return "A+";
   if (gpa > 3.7) return "A";
-  if (gpa > 3.5) return "A-";
-  if (gpa > 3) return "B+";
+  if (gpa > 3.3) return "A-";
+  if (gpa > 3.0) return "B+";
   if (gpa > 2.7) return "B";
-  if (gpa > 2.5) return "B-";
-  if (gpa > 2) return "C+";
+  if (gpa > 2.3) return "B-";
+  if (gpa > 2.0) return "C+";
   if (gpa > 1.7) return "C";
-  if (gpa > 1.5) return "C-";
-  if (gpa > 1) return "D+";
+  if (gpa > 1.3) return "C-";
+  if (gpa > 1.0) return "D+";
   if (gpa > 0.7) return "D";
+  if (gpa > 0) return "D-";
+  if (gpa === 0) return "F";
 
-  return gpa ? "D-" : "F";
+  return "";
 }
