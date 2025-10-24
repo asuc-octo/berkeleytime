aef6fd7edde28e64b29101cc9c92a9aecd2ac688
PineND <pinenguyen@berkeley.edu>
2025-10-24 11:57:58 -0700

    removed remaining catalog cache hashing

--- apps/backend/src/modules/catalog/typedefs/catalog.ts
+++ apps/backend/src/modules/catalog/typedefs/catalog.ts
@@ -2,11 +2,6 @@ import { gql } from "graphql-tag";
 
 export default gql`
   type Query {
-    catalog(
-      year: Int!
-      semester: Semester!
-      query: String
-      refresh: Boolean = false
-    ): [Class!]!
+    catalog(year: Int!, semester: Semester!, query: String): [Class!]!
   }
 `;
