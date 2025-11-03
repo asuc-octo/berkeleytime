48480097e9c1f18b2398a5bc13fd92f8f1049017
PineND <pinenguyen@berkeley.edu>
2025-11-02 16:49:41 -0800

    unit slider

--- apps/frontend/src/components/ClassBrowser/browserContext.ts
+++ apps/frontend/src/components/ClassBrowser/browserContext.ts
@@ -2,7 +2,14 @@ import { Dispatch, SetStateAction, createContext } from "react";
 
 import { Component, IClass, ITerm, Semester } from "@/lib/api";
 
-import { Breadth, Day, Level, SortBy, Unit, UniversityRequirement } from "./browser";
+import {
+  Breadth,
+  Day,
+  Level,
+  SortBy,
+  UnitRange,
+  UniversityRequirement,
+} from "./browser";
 
 export interface BrowserContextType {
   responsive: boolean;
@@ -16,7 +23,7 @@ export interface BrowserContextType {
   terms?: ITerm[];
   query: string;
   components: Component[];
-  units: Unit[];
+  units: UnitRange;
   levels: Level[];
   days: Day[];
   breadths: Breadth[];
@@ -27,7 +34,7 @@ export interface BrowserContextType {
   reverse: boolean;
   effectiveOrder: "asc" | "desc";
   updateComponents: Dispatch<Component[]>;
-  updateUnits: Dispatch<Unit[]>;
+  updateUnits: Dispatch<UnitRange>;
   updateLevels: Dispatch<Level[]>;
   updateDays: Dispatch<Day[]>;
   updateBreadths: Dispatch<Breadth[]>;
