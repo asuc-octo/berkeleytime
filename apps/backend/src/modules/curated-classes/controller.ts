e1fe97bf780ff17ff04d189ec9ba79de0cbda0b7
Matthew Rowland <contact@mathhulk.com>
2025-11-04 15:33:22 -0800

    chore: Force slow resolution of sections to fetch accurate enrollment data

--- apps/backend/src/modules/curated-classes/controller.ts
+++ apps/backend/src/modules/curated-classes/controller.ts
@@ -3,12 +3,8 @@ import { GraphQLResolveInfo } from "graphql";
 import {
   CourseModel,
   CuratedClassModel,
-  GradeDistributionModel,
   IClassItem,
   ICourseItem,
-  IGradeDistributionItem,
-  ISectionItem,
-  SectionModel,
   UserType,
 } from "@repo/common";
 
@@ -16,19 +12,13 @@ import {
   CreateCuratedClassInput,
   UpdateCuratedClassInput,
 } from "../../generated-types/graphql";
-import { getFields } from "../../utils/graphql";
 import { getClass } from "../class/controller";
-import { formatClass, formatSection } from "../class/formatter";
+import { formatClass } from "../class/formatter";
 import { ClassModule } from "../class/generated-types/module-types";
 import { formatCourse } from "../course/formatter";
-import {
-  getAverageGrade,
-  getDistribution,
-} from "../grade-distribution/controller";
-import { GradeDistributionModule } from "../grade-distribution/generated-types/module-types";
 import { CuratedClassesModule } from "./generated-types/module-types";
 
-export const getCuratedClasses = async (info: GraphQLResolveInfo) => {
+export const getCuratedClasses = async (_: GraphQLResolveInfo) => {
   const curatedClasses = await CuratedClassModel.find().lean();
 
   const uniqueClasses = curatedClasses.reduce(
@@ -66,55 +56,55 @@ export const getCuratedClasses = async (info: GraphQLResolveInfo) => {
   }).lean();
 
   // Fetch available sections for the term
-  const sections = await SectionModel.find({
-    courseId: { $in: courseIds },
-    printInScheduleOfClasses: true,
-  }).lean();
+  // const sections = await SectionModel.find({
+  //   courseId: { $in: courseIds },
+  //   printInScheduleOfClasses: true,
+  // }).lean();
 
-  const parsedGradeDistributions = {} as Record<
-    string,
-    GradeDistributionModule.GradeDistribution
-  >;
+  // const parsedGradeDistributions = {} as Record<
+  //   string,
+  //   GradeDistributionModule.GradeDistribution
+  // >;
 
-  const children = getFields(info.fieldNodes);
-  const includesGradeDistribution = children.includes("gradeDistribution");
+  // const children = getFields(info.fieldNodes);
+  // const includesGradeDistribution = children.includes("gradeDistribution");
 
-  if (includesGradeDistribution) {
-    const sectionIds = sections.map((section) => section.sectionId);
+  // if (includesGradeDistribution) {
+  //   const sectionIds = sections.map((section) => section.sectionId);
 
-    const gradeDistributions = await GradeDistributionModel.find({
-      // The bottleneck seems to be the amount of data we are fetching and not the query itself
-      sectionId: { $in: sectionIds },
-    }).lean();
+  //   const gradeDistributions = await GradeDistributionModel.find({
+  //     // The bottleneck seems to be the amount of data we are fetching and not the query itself
+  //     sectionId: { $in: sectionIds },
+  //   }).lean();
 
-    const reducedGradeDistributions = gradeDistributions.reduce(
-      (accumulator, gradeDistribution) => {
-        const courseSubjectNumber = `${gradeDistribution.subject}-${gradeDistribution.courseNumber}`;
-        const sectionId = gradeDistribution.sectionId;
+  //   const reducedGradeDistributions = gradeDistributions.reduce(
+  //     (accumulator, gradeDistribution) => {
+  //       const courseSubjectNumber = `${gradeDistribution.subject}-${gradeDistribution.courseNumber}`;
+  //       const sectionId = gradeDistribution.sectionId;
 
-        accumulator[courseSubjectNumber] = accumulator[courseSubjectNumber]
-          ? [...accumulator[courseSubjectNumber], gradeDistribution]
-          : [gradeDistribution];
-        accumulator[sectionId] = accumulator[sectionId]
-          ? [...accumulator[sectionId], gradeDistribution]
-          : [gradeDistribution];
+  //       accumulator[courseSubjectNumber] = accumulator[courseSubjectNumber]
+  //         ? [...accumulator[courseSubjectNumber], gradeDistribution]
+  //         : [gradeDistribution];
+  //       accumulator[sectionId] = accumulator[sectionId]
+  //         ? [...accumulator[sectionId], gradeDistribution]
+  //         : [gradeDistribution];
 
-        return accumulator;
-      },
-      {} as Record<string, IGradeDistributionItem[]>
-    );
+  //       return accumulator;
+  //     },
+  //     {} as Record<string, IGradeDistributionItem[]>
+  //   );
 
-    const entries = Object.entries(reducedGradeDistributions);
+  //   const entries = Object.entries(reducedGradeDistributions);
 
-    for (const [key, value] of entries) {
-      const distribution = getDistribution(value);
+  //   for (const [key, value] of entries) {
+  //     const distribution = getDistribution(value);
 
-      parsedGradeDistributions[key] = {
-        average: getAverageGrade(distribution),
-        distribution,
-      } as GradeDistributionModule.GradeDistribution;
-    }
-  }
+  //     parsedGradeDistributions[key] = {
+  //       average: getAverageGrade(distribution),
+  //       distribution,
+  //     } as GradeDistributionModule.GradeDistribution;
+  //   }
+  // }
 
   // Turn courses into a map to decrease time complexity for filtering
   const reducedCourses = courses.reduce(
@@ -126,21 +116,21 @@ export const getCuratedClasses = async (info: GraphQLResolveInfo) => {
   );
 
   // Turn sections into a map to decrease time complexity for filtering
-  const reducedSections = sections.reduce(
-    (accumulator, section) => {
-      const courseId = section.courseId;
-      const classNumber = section.classNumber;
+  // const reducedSections = sections.reduce(
+  //   (accumulator, section) => {
+  //     const courseId = section.courseId;
+  //     const classNumber = section.classNumber;
 
-      const id = `${courseId}-${classNumber}`;
+  //     const id = `${courseId}-${classNumber}`;
 
-      accumulator[id] = (
-        accumulator[id] ? [...accumulator[id], section] : [section]
-      ) as ISectionItem[];
+  //     accumulator[id] = (
+  //       accumulator[id] ? [...accumulator[id], section] : [section]
+  //     ) as ISectionItem[];
 
-      return accumulator;
-    },
-    {} as Record<string, ISectionItem[]>
-  );
+  //     return accumulator;
+  //   },
+  //   {} as Record<string, ISectionItem[]>
+  // );
 
   const reducedClasses = curatedClasses.reduce((accumulator, curatedClass) => {
     const _class = classes.find(
@@ -159,50 +149,50 @@ export const getCuratedClasses = async (info: GraphQLResolveInfo) => {
     const course = reducedCourses[courseId];
     if (!course) return accumulator;
 
-    const sections = reducedSections[`${courseId}-${_class.number}`];
-    if (!sections) return accumulator;
+    // const sections = reducedSections[`${courseId}-${_class.number}`];
+    // if (!sections) return accumulator;
 
-    const index = sections.findIndex((section) => section.primary);
-    if (index === -1) return accumulator;
+    // const index = sections.findIndex((section) => section.primary);
+    // if (index === -1) return accumulator;
 
-    const formattedPrimarySection = formatSection(sections.splice(index, 1)[0]);
-    const formattedSections = sections.map(formatSection);
+    // const formattedPrimarySection = formatSection(sections.splice(index, 1)[0]);
+    // const formattedSections = sections.map(formatSection);
 
     const formattedCourse = formatCourse(
       course
     ) as unknown as ClassModule.Course;
 
     // Add grade distribution to course
-    if (includesGradeDistribution) {
-      const key = `${course.subject}-${course.number}`;
-      const gradeDistribution = parsedGradeDistributions[key];
+    // if (includesGradeDistribution) {
+    //   const key = `${course.subject}-${course.number}`;
+    //   const gradeDistribution = parsedGradeDistributions[key];
 
-      // Fall back to an empty grade distribution to prevent resolving the field again
-      formattedCourse.gradeDistribution = gradeDistribution ?? {
-        average: null,
-        distribution: [],
-      };
-    }
+    //   // Fall back to an empty grade distribution to prevent resolving the field again
+    //   formattedCourse.gradeDistribution = gradeDistribution ?? {
+    //     average: null,
+    //     distribution: [],
+    //   };
+    // }
 
     const formattedClass = {
       ...formatClass(_class as IClassItem),
-      primarySection: formattedPrimarySection,
-      sections: formattedSections,
+      // primarySection: formattedPrimarySection,
+      // sections: formattedSections,
       course: formattedCourse,
     } as unknown as ClassModule.Class;
 
     // Add grade distribution to class
-    if (includesGradeDistribution) {
-      const sectionId = formattedPrimarySection.sectionId;
+    // if (includesGradeDistribution) {
+    //   const sectionId = formattedPrimarySection.sectionId;
 
-      // Fall back to an empty grade distribution to prevent resolving the field again
-      const gradeDistribution = parsedGradeDistributions[sectionId] ?? {
-        average: null,
-        distribution: [],
-      };
+    //   // Fall back to an empty grade distribution to prevent resolving the field again
+    //   const gradeDistribution = parsedGradeDistributions[sectionId] ?? {
+    //     average: null,
+    //     distribution: [],
+    //   };
 
-      formattedClass.gradeDistribution = gradeDistribution;
-    }
+    //   formattedClass.gradeDistribution = gradeDistribution;
+    // }
 
     accumulator.push({
       ...curatedClass,
