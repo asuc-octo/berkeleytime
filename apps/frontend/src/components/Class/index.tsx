e2dad6dfade9c6fbddb456623ea3e862a2843724
Priya Venkatesan <priyavenkatesan@berkeley.edu>
2025-11-12 18:53:47 -0800

    buggy styling for notif button on catalog

--- apps/frontend/src/components/Class/index.tsx
+++ apps/frontend/src/components/Class/index.tsx
@@ -1,4 +1,12 @@
-import { ReactNode, lazy, useCallback, useEffect, useMemo } from "react";
+import {
+  ReactNode,
+  lazy,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+  useRef,
+} from "react";
 
 import classNames from "classnames";
 import {
@@ -7,7 +15,10 @@ import {
   CalendarPlus,
   OpenNewWindow,
   Xmark,
+  NavArrowDown,
+  Check,
 } from "iconoir-react";
+import { Checkbox } from "radix-ui";
 import { Tabs } from "radix-ui";
 import {
   Link,
@@ -102,7 +113,6 @@ interface DialogClassProps {
   onClose?: never;
 }
 
-// TODO: Determine whether a controlled input is even necessary
 type ClassProps = (CatalogClassProps | DialogClassProps) &
   (ControlledProps | UncontrolledProps);
 
@@ -117,13 +127,15 @@ export default function Class({
   onClose,
   dialog,
 }: ClassProps) {
-  // const { pins, addPin, removePin } = usePins();
   const location = useLocation();
   const navigate = useNavigate();
-
   const { user, loading: userLoading } = useUser();
-
   const [updateUser] = useUpdateUser();
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [tempThresholds, setTempThresholds] = useState<number[]>([]);
+  const popupRef = useRef<HTMLDivElement>(null);
+  const bellIconRef = useRef<HTMLDivElement>(null);
 
   const { data: course, loading: courseLoading } = useReadCourseForClass(
     providedClass?.subject ?? (subject as string),
@@ -140,13 +152,11 @@ export default function Class({
     courseNumber as string,
     number as string,
     {
-      // Allow class to be provided
       skip: !!providedClass,
     }
   );
 
   const _class = useMemo(() => providedClass ?? data, [data, providedClass]);
-
   const _course = useMemo(
     () => providedCourse ?? course,
     [course, providedCourse]
@@ -164,6 +174,122 @@ export default function Class({
       ),
     [user, _class]
   );
+
+  const trackedClassData = useMemo(() => {
+    return user?.trackedClasses?.find(
+      (trackedClass) =>
+        trackedClass.subject === _class.subject &&
+        trackedClass.courseNumber === _class.courseNumber &&
+        trackedClass.number === _class.number &&
+        trackedClass.year === _class.year &&
+        trackedClass.semester === _class.semester
+    );
+  }, [user, _class]);
+
+  const isActive = useMemo(
+    () => (trackedClassData?.thresholds.length ?? 0) > 0,
+    [trackedClassData]
+  );
+
+  useEffect(() => {
+    if (showPopup) {
+      setTempThresholds(trackedClassData?.thresholds ?? []);
+    }
+  }, [showPopup, trackedClassData]);
+
+  const handleTempThresholdChange = (threshold: number, checked: boolean) => {
+    setTempThresholds((prev) => {
+      if (checked) {
+        return [...prev, threshold].sort((a, b) => a - b);
+      } else {
+        return prev.filter((t) => t !== threshold);
+      }
+    });
+  };
+
+  const applyThresholdChanges = useCallback(async () => {
+    if (!user || !_class) return;
+
+    const originalThresholds = trackedClassData?.thresholds ?? [];
+    const sameLength = originalThresholds.length === tempThresholds.length;
+    const sameValues = originalThresholds.every((t) =>
+      tempThresholds.includes(t)
+    );
+
+    if (sameLength && sameValues) {
+      return;
+    }
+
+    const otherTrackedClasses = user.trackedClasses.filter(
+      (trackedClass) =>
+        !(
+          trackedClass.subject === _class.subject &&
+          trackedClass.courseNumber === _class.courseNumber &&
+          trackedClass.number === _class.number &&
+          trackedClass.year === _class.year &&
+          trackedClass.semester === _class.semester
+        )
+    );
+
+    let newTrackedClasses;
+    if (tempThresholds.length > 0) {
+      newTrackedClasses = [
+        ...otherTrackedClasses,
+        {
+          ..._class,
+          thresholds: tempThresholds,
+        },
+      ];
+    } else {
+      newTrackedClasses = otherTrackedClasses;
+    }
+
+    await updateUser(
+      {
+        trackedClasses: newTrackedClasses.map((tc) => ({
+          subject: tc.subject,
+          number: tc.number,
+          courseNumber: tc.courseNumber,
+          year: tc.year,
+          semester: tc.semester,
+          sessionId: tc.sessionId,
+          thresholds: tc.thresholds,
+        })),
+      },
+      {
+        optimisticResponse: {
+          updateUser: {
+            ...user,
+            trackedClasses: newTrackedClasses,
+          },
+        },
+      }
+    );
+  }, [_class, tempThresholds, trackedClassData, updateUser, user]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popupRef.current &&
+        bellIconRef.current &&
+        !popupRef.current.contains(event.target as Node) &&
+        !bellIconRef.current.contains(event.target as Node)
+      ) {
+        if (showPopup) {
+          applyThresholdChanges();
+          setShowPopup(false);
+        }
+      }
+    };
+
+    if (showPopup) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showPopup, applyThresholdChanges]);
 
   const pin = useMemo(() => {
     if (!_class) return;
@@ -184,8 +310,6 @@ export default function Class({
       },
     } as ClassPin;
   }, [_class]);
-
-  // const pinned = useMemo(() => pins.some((p) => p.id === pin?.id), [pins, pin]);
 
   const bookmark = useCallback(async () => {
     if (!user || !_class) return;
@@ -247,32 +371,6 @@ export default function Class({
     );
   }, [_course]);
 
-  // seat reservation logic pending design + consideration for performance.
-  // const seatReservationTypeMap = useMemo(() => {
-  //   const reservationTypes =
-  //     _class?.primarySection.enrollment?.seatReservationTypes ?? [];
-
-  //   const reservationMap = new Map<number, string>();
-  //   for (const type of reservationTypes) {
-  //     reservationMap.set(type.number, type.requirementGroup);
-  //   }
-  //   return reservationMap;
-  // }, [_class]);
-
-  // const seatReservationMaxEnroll = useMemo(() => {
-  //   const maxEnroll =
-  //     _class?.primarySection.enrollment?.history[0].seatReservationCount ?? [];
-  //   const maxEnrollMap = new Map<number, number>();
-
-  //   for (const type of maxEnroll) {
-  //     maxEnrollMap.set(type.number, type.maxEnroll);
-  //   }
-  //   return maxEnrollMap;
-  // }, [_class]);
-
-  // const seatReservationCount =
-  //   _class?.primarySection.enrollment?.latest?.seatReservationCount ?? [];
-
   const courseGradeDistribution = _class?.course.gradeDistribution;
 
   const hasCourseGradeSummary = useMemo(() => {
@@ -304,10 +402,11 @@ export default function Class({
     return <></>;
   }
 
-  // TODO: Error state
   if (!_course || !_class || !pin) {
     return <></>;
   }
+
+  const uniqueId = `${_class.subject}-${_class.number}`;
 
   return (
     <Root dialog={dialog}>
@@ -316,8 +415,7 @@ export default function Class({
           <Container size="3">
             <Flex direction="column" gap="5">
               <Flex justify="between" align="start">
-                <Flex gap="3">
-                  {/* TODO: Reusable bookmark button */}
+                <Flex gap="3" align="center" style={{ position: "relative" }}>
                   <Tooltip
                     content={bookmarked ? "Remove bookmark" : "Bookmark"}
                   >
@@ -331,23 +429,95 @@ export default function Class({
                       {bookmarked ? <BookmarkSolid /> : <Bookmark />}
                     </IconButton>
                   </Tooltip>
-                  {/* TODO: Reusable pin button
-              <Tooltip content={pinned ? "Remove pin" : "Pin"}>
-                <IconButton
-                  className={classNames(styles.bookmark, {
-                    [styles.active]: pinned,
-                  })}
-                  onClick={() => (pinned ? removePin(pin) : addPin(pin))}
-                >
-                  {pinned ? <PinSolid /> : <Pin />}
-                </IconButton>
-                  </Tooltip> */}
+
                   <Tooltip content="Add to schedule">
                     <IconButton>
                       <CalendarPlus />
                     </IconButton>
                   </Tooltip>
+
+                  <div
+                    ref={bellIconRef}
+                    className={`${styles.bellWrapper} ${
+                      isActive ? styles.active : ""
+                    }`}
+                    onClick={() => setShowPopup(!showPopup)}
+                  >
+                    {isActive ? (
+                      <>
+                        <CustomBellNotificationIcon width={16} height={16} />
+                        <NavArrowDown
+                          width={14}
+                          height={14}
+                          strokeWidth={2.5}
+                          style={{ color: "var(--blue-500)" }}
+                          className={showPopup ? styles.arrowRotated : ""}
+                        />
+                      </>
+                    ) : (
+                      <CustomBellIcon width={16} height={16} />
+                    )}
+                  </div>
+
+                  {showPopup && (
+                    <div ref={popupRef} className={styles.popup}>
+                      <div className={styles.checkboxOption}>
+                        <Checkbox.Root
+                          id={`${uniqueId}-50`}
+                          className={styles.checkbox}
+                          checked={tempThresholds.includes(50)}
+                          onCheckedChange={(checked) =>
+                            handleTempThresholdChange(50, checked as boolean)
+                          }
+                        >
+                          <Checkbox.Indicator asChild>
+                            <Check width={12} height={12} />
+                          </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <label htmlFor={`${uniqueId}-50`}>
+                          <span>When 50% full</span>
+                        </label>
+                      </div>
+
+                      <div className={styles.checkboxOption}>
+                        <Checkbox.Root
+                          id={`${uniqueId}-75`}
+                          className={styles.checkbox}
+                          checked={tempThresholds.includes(75)}
+                          onCheckedChange={(checked) =>
+                            handleTempThresholdChange(75, checked as boolean)
+                          }
+                        >
+                          <Checkbox.Indicator asChild>
+                            <Check width={12} height={12} />
+                          </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <label htmlFor={`${uniqueId}-75`}>
+                          <span>When 75% full</span>
+                        </label>
+                      </div>
+
+                      <div className={styles.checkboxOption}>
+                        <Checkbox.Root
+                          id={`${uniqueId}-90`}
+                          className={styles.checkbox}
+                          checked={tempThresholds.includes(90)}
+                          onCheckedChange={(checked) =>
+                            handleTempThresholdChange(90, checked as boolean)
+                          }
+                        >
+                          <Checkbox.Indicator asChild>
+                            <Check width={12} height={12} />
+                          </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <label htmlFor={`${uniqueId}-90`}>
+                          <span>When 90% full</span>
+                        </label>
+                      </div>
+                    </div>
+                  )}
                 </Flex>
+
                 <Flex gap="3">
                   <Tooltip content="Open in Berkeley Catalog">
                     <IconButton
@@ -375,6 +545,7 @@ export default function Class({
                   )}
                 </Flex>
               </Flex>
+
               <Flex direction="column" gap="4">
                 <Flex direction="column" gap="1">
                   <h1 className={styles.heading}>
@@ -475,11 +646,6 @@ export default function Class({
                       <MenuItem active={isActive}>Grades</MenuItem>
                     )}
                   </NavLink>
-                  {/* <NavLink to={{ ...location, pathname: "enrollment" }}>
-                    {({ isActive }) => (
-                      <MenuItem active={isActive}>Enrollment</MenuItem>
-                    )}
-                  </NavLink> */}
                   <NavLink to={{ ...location, pathname: "ratings" }}>
                     {({ isActive }) => (
                       <MenuItem active={isActive}>
@@ -539,3 +705,61 @@ export default function Class({
     </Root>
   );
 }
+
+const CustomBellNotificationIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14.752 1.91434C14.1218 2.7805 13.75 3.84683 13.75 5C13.75 7.83971 16.0046 10.1528 18.8214 10.247C18.8216 10.2508 18.8219 10.2546 18.8222 10.2583C18.8369 10.4557 18.852 10.6591 18.8726 10.855C19.1087 13.1025 19.6495 14.6443 20.1679 15.6582C20.5132 16.3334 20.8519 16.781 21.0922 17.0516C21.2125 17.1871 21.3088 17.2788 21.3696 17.3328C21.4 17.3599 21.4216 17.3775 21.4329 17.3865L21.4416 17.3933C21.7027 17.5833 21.8131 17.9196 21.7147 18.2278C21.6154 18.5386 21.3265 18.7496 21.0002 18.7496L3.00005 18.75C2.67373 18.75 2.38485 18.539 2.28559 18.2281C2.18718 17.9199 2.29755 17.5837 2.55863 17.3937L2.56735 17.3869C2.57869 17.3779 2.60028 17.3602 2.63069 17.3332C2.69148 17.2792 2.7877 17.1875 2.90804 17.052C3.14835 16.7814 3.48701 16.3338 3.8323 15.6585C4.52142 14.3109 5.25005 12.0306 5.25005 8.4C5.25005 6.51876 5.95021 4.70561 7.21026 3.36156C8.47184 2.01587 10.1937 1.25 12.0001 1.25C12.3823 1.25 12.7613 1.28434 13.1331 1.35139C13.3707 1.39421 14.1514 1.63742 14.752 1.91434Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M15.25 5C15.25 2.92893 16.9289 1.25 19 1.25C21.0711 1.25 22.75 2.92893 22.75 5C22.75 7.07107 21.0711 8.75 19 8.75C16.9289 8.75 15.25 7.07107 15.25 5Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.89369 20.3514C10.252 20.1435 10.7109 20.2655 10.9188 20.6238C11.0287 20.8132 11.1864 20.9705 11.3761 21.0798C11.5659 21.1891 11.781 21.2466 12 21.2466C12.219 21.2466 12.4342 21.1891 12.6239 21.0798C12.8137 20.9705 12.9714 20.8132 13.0813 20.6238C13.2891 20.2655 13.7481 20.1435 14.1063 20.3514C14.4646 20.5592 14.5866 21.0182 14.3788 21.3765C14.137 21.7932 13.7901 22.1391 13.3726 22.3796C12.9551 22.62 12.4818 22.7466 12 22.7466C11.5183 22.7466 11.0449 22.62 10.6275 22.3796C10.21 22.1391 9.86301 21.7932 9.62127 21.3765C9.41343 21.0182 9.5354 20.5592 9.89369 20.3514Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+const CustomBellIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="currentColor"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14.752 1.91434C14.1218 2.7805 13.75 3.84683 13.75 5C13.75 7.83971 16.0046 10.1528 18.8214 10.247C18.8216 10.2508 18.8219 10.2546 18.8222 10.2583C18.8369 10.4557 18.852 10.6591 18.8726 10.855C19.1087 13.1025 19.6495 14.6443 20.1679 15.6582C20.5132 16.3334 20.8519 16.781 21.0922 17.0516C21.2125 17.1871 21.3088 17.2788 21.3696 17.3328C21.4 17.3599 21.4216 17.3775 21.4329 17.3865L21.4416 17.3933C21.7027 17.5833 21.8131 17.9196 21.7147 18.2278C21.6154 18.5386 21.3265 18.7496 21.0002 18.7496L3.00005 18.75C2.67373 18.75 2.38485 18.539 2.28559 18.2281C2.18718 17.9199 2.29755 17.5837 2.55863 17.3937L2.56735 17.3869C2.57869 17.3779 2.60028 17.3602 2.63069 17.3332C2.69148 17.2792 2.7877 17.1875 2.90804 17.052C3.14835 16.7814 3.48701 16.3338 3.8323 15.6585C4.52142 14.3109 5.25005 12.0306 5.25005 8.4C5.25005 6.51876 5.95021 4.70561 7.21026 3.36156C8.47184 2.01587 10.1937 1.25 12.0001 1.25C12.3823 1.25 12.7613 1.28434 13.1331 1.35139C13.3707 1.39421 14.1514 1.63742 14.752 1.91434Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.89369 20.3514C10.252 20.1435 10.7109 20.2655 10.9188 20.6238C11.0287 20.8132 11.1864 20.9705 11.3761 21.0798C11.5659 21.1891 11.781 21.2466 12 21.2466C12.219 21.2466 12.4342 21.1891 12.6239 21.0798C12.8137 20.9705 12.9714 20.8132 13.0813 20.6238C13.2891 20.2655 13.7481 20.1435 14.1063 20.3514C14.4646 20.5592 14.5866 21.0182 14.3788 21.3765C14.137 21.7932 13.7901 22.1391 13.3726 22.3796C12.9551 22.62 12.4818 22.7466 12 22.7466C11.5183 22.7466 11.0449 22.62 10.6275 22.3796C10.21 22.1391 9.86301 21.7932 9.62127 21.3765C9.41343 21.0182 9.5354 20.5592 9.89369 20.3514Z"
+      fill="currentColor"
+    />
+  </svg>
+);
