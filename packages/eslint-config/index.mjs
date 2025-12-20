969eae4c34cbda3532b2018626210cd4e8cdfc4c
maxmwang <orangemax888@gmail.com>
2025-12-19 22:43:42 -0800

    add backend linter

--- packages/eslint-config/index.mjs
+++ packages/eslint-config/index.mjs
@@ -6,7 +6,21 @@ import reactRefreshPlugin from "eslint-plugin-react-refresh";
 import ts from "typescript-eslint";
 
 /** @type { import("eslint").Linter.Config[] } */
-export default [
+export const backendConfig = [
+  { ignores: ["**/generated-types/**", "**/generated-typedefs/**"] },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.ts"],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+];
+
+/** @type { import("eslint").Linter.Config[] } */
+export const frontendConfig = [
   js.configs.recommended,
   ...ts.configs.recommended,
   prettier,
