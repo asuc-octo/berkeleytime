ea071c5ee36888547b675a3efe442997742d2fa9
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2026-03-31 00:59:45 -0700

    tracking events job

--- apps/backend/src/bootstrap/loaders/index.ts
+++ apps/backend/src/bootstrap/loaders/index.ts
@@ -6,6 +6,7 @@ import { startBannerViewCountFlushJob } from "../../modules/banner/jobs/flush-vi
 import { startViewCountFlushJob } from "../../modules/class/jobs/flush-view-counts";
 import { startClickEventsFlushJob } from "../../modules/click-tracking/jobs/flush-click-events";
 import { startActivityScoreUpdateJob } from "../../modules/user/jobs/update-activity-scores";
+import { startTrackingEventsFlushJob } from "../../modules/tracking/jobs/flush-tracking-events";
 // loaders
 import apolloLoader from "./apollo";
 import expressLoader from "./express";
@@ -32,6 +33,7 @@ export default async (root: Application): Promise<void> => {
   startBannerViewCountFlushJob(apolloRedis);
   startClickEventsFlushJob(apolloRedis);
   startActivityScoreUpdateJob();
+  startTrackingEventsFlushJob(apolloRedis);
 
   // append backend path to all routes
   root.use(config.backendPath, app);
