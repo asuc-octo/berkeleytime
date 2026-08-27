70f05c63c82f6dbffa13971f16f1cd8d557d44e7
Nathan Dai <nathandai2000@gmail.com>
2026-08-27 13:39:33 -0700

    Revert "Hide site banner on apply page"
    This reverts commit a944c6117c040ed77bbcf9b1aca18238ceeccd00.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -174,6 +174,14 @@ const router = createBrowserRouter([
             ),
           },
           {
+            path: "apply",
+            element: (
+              <SuspenseBoundary key="apply">
+                <Apply />
+              </SuspenseBoundary>
+            ),
+          },
+          {
             path: "legal/privacy",
             element: (
               <SuspenseBoundary key="privacy">
@@ -186,19 +194,6 @@ const router = createBrowserRouter([
             element: (
               <SuspenseBoundary key="terms">
                 <Terms />
-              </SuspenseBoundary>
-            ),
-          },
-        ],
-      },
-      {
-        element: <Layout banner={false} />,
-        children: [
-          {
-            path: "apply",
-            element: (
-              <SuspenseBoundary key="apply">
-                <Apply />
               </SuspenseBoundary>
             ),
           },
